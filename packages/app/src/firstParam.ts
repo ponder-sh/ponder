5d671486e3cdfb400b74bc005f0e5f72133f7349
Olias <0xolias@gmail.com>
2022-08-12 15:07:16 -0700

    feat: rm app package

--- packages/app/src/firstParam.ts
+++
@@ -1,3 +0,0 @@
-export const firstParam = (param: string | string[] | undefined) => {
-  return Array.isArray(param) ? param[0] : param;
-};
--- packages/app/src/formatAddress.ts
+++
@@ -1,9 +0,0 @@
-import { getAddress } from "@ethersproject/address";
-
-export const formatAddress = (address: string) => {
-  try {
-    return getAddress(address);
-  } catch (error) {
-    return address;
-  }
-};
