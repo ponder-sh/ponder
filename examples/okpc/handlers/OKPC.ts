d8723f87b198e0f2972f24d7475a3c0a24c475f5
Olias <0xolias@gmail.com>
2022-08-17 12:27:22 -0700

    demo: done

--- examples/okpc/handlers/OKPC.ts
+++ examples/okpc/handlers/OKPC.ts
@@ -1,13 +1,22 @@
 import { TransferHandler } from "../generated/OKPC";
 
 const handleTransfer: TransferHandler = async (event, context) => {
-  const { OkpcToken } = context.entities;
-  const { to, tokenId } = event.params;
+  const { OkpcToken, OkpcOwner } = context.entities;
+  const { to, from, tokenId } = event.params;
 
   await OkpcToken()
     .insert({
       id: tokenId.toNumber().toString(),
       owner: to,
+      lastOwner: from,
+    })
+    .onConflict("id")
+    .merge();
+
+  await OkpcOwner()
+    .insert({
+      id: tokenId.toNumber().toString(),
+      address: to,
     })
     .onConflict("id")
     .merge();
