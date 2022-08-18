775943ab8a528fe23a19f2fe0bf6202e555ea864
Olias <0xolias@gmail.com>
2022-08-18 16:41:21 -0700

    examples: add enum to okpc example

--- examples/okpc/handlers/OKPC.ts
+++ examples/okpc/handlers/OKPC.ts
@@ -1,4 +1,5 @@
 import { TransferHandler } from "../generated/OKPC";
+import { OkpcOwnerKind } from "../generated/schema";
 
 const handleTransfer: TransferHandler = async (event, context) => {
   const { OkpcToken, OkpcOwner } = context.entities;
@@ -17,6 +18,7 @@ const handleTransfer: TransferHandler = async (event, context) => {
     .insert({
       id: tokenId.toNumber().toString(),
       address: to,
+      kind: Math.random() > 0.5 ? OkpcOwnerKind.Good : OkpcOwnerKind.Bad,
     })
     .onConflict("id")
     .merge();
