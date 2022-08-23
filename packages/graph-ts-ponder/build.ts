86e8848cb8eab6b25ac9aa7abe0befc412c3682a
Olias <0xolias@gmail.com>
2022-08-23 13:42:59 -0700

    fix: module export

--- packages/graph-ts-ponder/build.ts
+++ packages/graph-ts-ponder/build.ts
@@ -4,7 +4,7 @@ import path from 'node:path'
 const build = async () => {
   const raw = await readFile(path.join(__dirname, './index.js'), 'utf-8')
   const escaped = raw.replaceAll('`', '\\`').replaceAll('$', '\\$')
-  const final = `\nexports.graphTsStaticString = \`${escaped}\``
+  const final = `\nmodule.exports.graphTsStaticString = \`${escaped}\``
 
   await appendFile(path.join(__dirname, './index.js'), final)
 }
