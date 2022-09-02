cc79cff99d2e4899eb8a0e9ec58ba05a7000556e
Olias <0xolias@gmail.com>
2022-09-01 17:49:15 -0700

    chore: remove graph compat stuff from config

--- packages/ponder/src/config.ts
+++ packages/ponder/src/config.ts
@@ -4,10 +4,7 @@ const dir = process.cwd();
 const getAbsolutePath = (targetPath: string) => path.join(dir, targetPath);
 
 // TODO: Parse these from CLI flags.
-// const USER_OPTIONS = {};
-const USER_OPTIONS = {
-  GRAPH_COMPAT_ENABLED: true,
-};
+const USER_OPTIONS = {};
 
 const DEFAULT_OPTIONS = {
   // File path options
@@ -18,13 +15,7 @@ const DEFAULT_OPTIONS = {
   PONDER_DIR_PATH: ".ponder",
 
   // General options
-  LOG_LEVEL: 4,
-
-  // Graph Protocol compatibility options
-  GRAPH_COMPAT_ENABLED: false,
-  GRAPH_COMPAT_SUBGRAPH_YAML_PATH: "subgraph.yaml",
-  GRAPH_COMPAT_SUBGRAPH_SCHEMA_PATH: "schema.graphql",
-  GRAPH_COMPAT_HANDLERS_DIR_PATH: "src",
+  LOG_LEVEL: 2,
 };
 
 const OPTIONS = {
@@ -34,21 +25,12 @@ const OPTIONS = {
 
 const CONFIG = {
   ...OPTIONS,
-  // Resolve absolute paths for _PATH options
+  // Resolve absolute paths
   PONDER_CONFIG_FILE_PATH: getAbsolutePath(OPTIONS.PONDER_CONFIG_FILE_PATH),
   SCHEMA_FILE_PATH: getAbsolutePath(OPTIONS.SCHEMA_FILE_PATH),
   HANDLERS_DIR_PATH: getAbsolutePath(OPTIONS.HANDLERS_DIR_PATH),
   GENERATED_DIR_PATH: getAbsolutePath(OPTIONS.GENERATED_DIR_PATH),
   PONDER_DIR_PATH: getAbsolutePath(OPTIONS.PONDER_DIR_PATH),
-  GRAPH_COMPAT_SUBGRAPH_YAML_PATH: getAbsolutePath(
-    OPTIONS.GRAPH_COMPAT_SUBGRAPH_YAML_PATH
-  ),
-  GRAPH_COMPAT_SUBGRAPH_SCHEMA_PATH: getAbsolutePath(
-    OPTIONS.GRAPH_COMPAT_SUBGRAPH_SCHEMA_PATH
-  ),
-  GRAPH_COMPAT_HANDLERS_DIR_PATH: getAbsolutePath(
-    OPTIONS.GRAPH_COMPAT_HANDLERS_DIR_PATH
-  ),
 };
 
 export { CONFIG };
