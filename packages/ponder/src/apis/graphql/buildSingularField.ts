cc4159e969846c78c23a24e2388a9687567f5d5d
Olias <0xolias@gmail.com>
2022-09-05 17:29:37 -0400

    wip: dynamic resolvers

--- packages/ponder/src/apis/graphql/buildSingularField.ts
+++ packages/ponder/src/apis/graphql/buildSingularField.ts
@@ -24,6 +24,22 @@ const buildSingularField = (
     if (!id) return null;
 
     return await store.getEntity(entity.name, id);
+
+    // const entityInstance = (await store.getEntity(entity.name, id)) as any;
+
+    // // Build resolvers for any relationship fields on the entity.
+    // entity.fields
+    //   .filter(
+    //     (field): field is RelationshipField =>
+    //       field.kind === FieldKind.RELATIONSHIP
+    //   )
+    //   .forEach((field) => {
+    //     const relatedEntityId = entityInstance[field.name];
+
+    //     entityInstance[field.name] = async () => {
+    //       return await store.getEntity(field.baseGqlType.name, relatedEntityId);
+    //     };
+    //   });
   };
 
   return {
