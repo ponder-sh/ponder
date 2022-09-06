89326abdc726045677536263de104c7fd1a62873
Olias <0xolias@gmail.com>
2022-09-06 17:21:58 -0400

    example: add example to test create-ponder-app

---
+++ examples/ethplays/src/registryReceiverV0.ts
@@ -0,0 +1,53 @@
+import { Player } from "../generated/schema";
+import {
+  buildGameStateEntity,
+  buildPlayerEntity,
+  buildFeedItemEntity,
+} from "./utils";
+
+import {
+  NewRegistration,
+  UpdatedRegistration,
+} from "../generated/RegistryReceiverV0/RegistryReceiverV0";
+
+export function handleNewRegistration(event: NewRegistration): void {
+  // hack, calling this like a setup script lol
+  const gameStateEntity = buildGameStateEntity();
+  gameStateEntity.save();
+
+  const playerEntity = buildPlayerEntity(
+    event.params.burnerAccount.toHexString()
+  );
+  playerEntity.account = event.params.account;
+  playerEntity.save();
+
+  const feedItemEntity = buildFeedItemEntity(event);
+  feedItemEntity.type = "NewRegistration";
+  feedItemEntity.from = event.params.burnerAccount;
+  feedItemEntity.account = event.params.account;
+  feedItemEntity.save();
+}
+
+export function handleUpdatedRegistration(event: UpdatedRegistration): void {
+  // Replace / invalidate previous Player entity
+  const previousPlayerEntity = Player.load(
+    event.params.previousBurnerAccount.toHexString()
+  );
+  if (previousPlayerEntity) {
+    previousPlayerEntity.replacedAt = event.block.timestamp.toU32();
+    previousPlayerEntity.save();
+  }
+
+  // Create new Player entity
+  const playerEntity = buildPlayerEntity(
+    event.params.burnerAccount.toHexString()
+  );
+  playerEntity.account = event.params.account;
+  playerEntity.save();
+
+  const feedItemEntity = buildFeedItemEntity(event);
+  feedItemEntity.type = "UpdatedRegistration";
+  feedItemEntity.from = event.params.burnerAccount;
+  feedItemEntity.account = event.params.account;
+  feedItemEntity.save();
+}
