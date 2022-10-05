dcfd54b852d981aaee4e4046147ed87a52c1fd5f
Olias <0xolias@gmail.com>
2022-10-04 20:07:41 -0400

    example: update weth database file path config

--- examples/weth/ponder.config.js
+++ examples/weth/ponder.config.js
@@ -2,6 +2,7 @@ module.exports = {
   database: [
     {
       kind: "sqlite",
+      filename: process.env.DATABASE_FILE_PATH,
     },
   ],
   sources: [
@@ -12,7 +13,7 @@ module.exports = {
       rpcUrl: process.env.PONDER_RPC_URL_1,
       address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
       abi: "./abis/WETH.json",
-      startBlock: 15673000,
+      startBlock: 15678220,
     },
   ],
   apis: [{ kind: "graphql", default: true, port: 42069 }],
