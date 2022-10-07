1063d2c7be4f06e1f4465568344f2515fee0782f
Olias <0xolias@gmail.com>
2022-10-07 00:57:24 -0400

    example: env var name

--- examples/okpc/ponder.config.js
+++ examples/okpc/ponder.config.js
@@ -7,7 +7,7 @@ module.exports = {
       kind: "evm",
       name: "OKPC",
       chainId: 1,
-      rpcUrl: process.env.MAINNET_RPC_URL,
+      rpcUrl: process.env.PONDER_RPC_URL_1,
       abi: "./abis/OKPC.json",
       address: "0x7183209867489e1047f3a7c23ea1aed9c4e236e8",
       startBlock: 15500000,
