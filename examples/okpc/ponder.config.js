bf07e86ee3771eee5ae418e49683796848829526
Olias <0xolias@gmail.com>
2022-10-13 22:40:20 -0400

    example: add another contract to okpc example for testing purposes

--- examples/okpc/ponder.config.js
+++ examples/okpc/ponder.config.js
@@ -15,5 +15,14 @@ module.exports = {
       address: "0x7183209867489e1047f3a7c23ea1aed9c4e236e8",
       startBlock: 15500000,
     },
+    {
+      kind: "evm",
+      name: "CanonicalTransactionChain",
+      chainId: 1,
+      rpcUrl: process.env.PONDER_RPC_URL_1,
+      abi: "./abis/CanonicalTransactionChain.json",
+      address: "0x5E4e65926BA27467555EB562121fac00D24E9dD2",
+      startBlock: 15743300,
+    },
   ],
 };
