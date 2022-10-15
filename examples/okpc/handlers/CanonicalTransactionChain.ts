2e2a187e9385e9b6fbb2ea87d95550d007f091dc
Olias <0xolias@gmail.com>
2022-10-15 16:06:47 -0400

    example: add entity update to ctc handler

--- examples/okpc/handlers/CanonicalTransactionChain.ts
+++ examples/okpc/handlers/CanonicalTransactionChain.ts
@@ -1,14 +1,22 @@
 import { SequencerBatchAppendedHandler } from "../generated/CanonicalTransactionChain";
+import { OkpcOwnerTrait } from "../generated/schema";
 
 const handleSequencerBatchAppended: SequencerBatchAppendedHandler = async (
   event,
   context
 ) => {
-  // const { block, transaction } = event;
+  const { block, transaction } = event;
   const { OKPC } = context.contracts;
+  const { OkpcOwner } = context.entities;
 
   await OKPC.tokenURI(444, {
     blockTag: event.block.number,
+  });
+
+  await OkpcOwner.insert({
+    id: transaction.hash!,
+    traits: [OkpcOwnerTrait.Good, OkpcOwnerTrait.Bad],
+    receivedCount: 5,
   });
 };
 
