78fe941d0d5c6b2002da0d65436b7cad9ae09d87
Olias <0xolias@gmail.com>
2022-10-16 18:42:54 -0400

    chore: add export and types

---
+++ packages/ponder/src/index.ts
@@ -0,0 +1,4 @@
+export { generateContextTypes } from "./codegen/generateContextTypes";
+export { generateHandlerTypes } from "./codegen/generateHandlerTypes";
+export { generateSchema } from "./codegen/generateSchema";
+export { generateSchemaTypes } from "./codegen/generateSchemaTypes";
