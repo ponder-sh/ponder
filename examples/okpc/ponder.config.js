a2d08fd0a09be6c0187a64ec04f094e37da74637
Olias <0xolias@gmail.com>
2022-10-16 15:54:55 -0400

    example: update examples to use networks in config

--- examples/okpc/ponder.config.js
+++ examples/okpc/ponder.config.js
@@ -2,27 +2,30 @@ module.exports = {
   database: {
     kind: "sqlite",
   },
-  graphql: {
-    port: 42069,
-  },
+  networks: [
+    {
+      kind: "evm",
+      name: "mainnet",
+      chainId: 1,
+      rpcUrl: process.env.PONDER_RPC_URL_1,
+    },
+  ],
   sources: [
     {
       kind: "evm",
       name: "OKPC",
-      chainId: 1,
-      rpcUrl: process.env.PONDER_RPC_URL_1,
+      network: "mainnet",
       abi: "./abis/OKPC.json",
       address: "0x7183209867489e1047f3a7c23ea1aed9c4e236e8",
-      startBlock: 15755250,
+      startBlock: 15762500,
     },
     {
       kind: "evm",
       name: "CanonicalTransactionChain",
-      chainId: 1,
-      rpcUrl: process.env.PONDER_RPC_URL_1,
+      network: "mainnet",
       abi: "./abis/CanonicalTransactionChain.json",
       address: "0x5E4e65926BA27467555EB562121fac00D24E9dD2",
-      startBlock: 15755250,
+      startBlock: 15762300,
     },
   ],
 };
--- examples/weth/ponder.config.js
+++ examples/weth/ponder.config.js
@@ -3,15 +3,19 @@ module.exports = {
     kind: "sqlite",
     filename: process.env.DATABASE_FILE_PATH,
   },
-  graphql: {
-    port: 42069,
-  },
+  networks: [
+    {
+      kind: "evm",
+      name: "mainnet",
+      chainId: 1,
+      rpcUrl: process.env.PONDER_RPC_URL_1,
+    },
+  ],
   sources: [
     {
       kind: "evm",
       name: "WETH",
-      chainId: 1,
-      rpcUrl: process.env.PONDER_RPC_URL_1,
+      network: "mainnet",
       address: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
       abi: "./abis/WETH.json",
       startBlock: Number(process.env.WETH_START_BLOCK),
