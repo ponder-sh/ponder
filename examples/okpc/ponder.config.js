1a2fe5baf5ca363bf55917814a3f3b27375a409f
Olias <0xolias@gmail.com>
2022-10-20 16:19:19 -0400

    example: use env var for okpc start block

--- examples/okpc/ponder.config.js
+++ examples/okpc/ponder.config.js
@@ -17,7 +17,7 @@ module.exports = {
       network: "mainnet",
       abi: "./abis/OKPC.json",
       address: "0x7183209867489e1047f3a7c23ea1aed9c4e236e8",
-      startBlock: 15770250,
+      startBlock: Number(process.env.START_BLOCK),
     },
     {
       kind: "evm",
@@ -25,7 +25,7 @@ module.exports = {
       network: "mainnet",
       abi: "./abis/CanonicalTransactionChain.json",
       address: "0x5E4e65926BA27467555EB562121fac00D24E9dD2",
-      startBlock: 15770250,
+      startBlock: Number(process.env.START_BLOCK),
     },
   ],
 };
