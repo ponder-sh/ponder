4503cd57fc385a02d325a7dafa530bc94e36bd6c
Olias <0xolias@gmail.com>
2022-10-19 23:21:35 -0400

    fix: export more types

--- packages/ponder/src/index.ts
+++ packages/ponder/src/index.ts
@@ -1,4 +1,7 @@
 export { generateContextTypes } from "./codegen/generateContextTypes";
 export { generateHandlerTypes } from "./codegen/generateHandlerTypes";
+export type { PonderLogger } from "./common/logger";
+export type { PonderOptions } from "./common/options";
+export type { PonderDatabase } from "./db/db";
 export type { PonderPlugin } from "./plugin";
 export type { Block, EventLog, Transaction } from "./types";
