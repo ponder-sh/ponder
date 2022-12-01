2d04b02138c20552d2c8f5637972d212406871e8
Olias <0xolias@gmail.com>
2022-12-01 01:47:40 -0500

    docs: add nextra:

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withNextra = require("nextra")({
+  theme: "nextra-theme-docs",
+  themeConfig: "./theme.config.js",
+  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+});
 
-module.exports = nextConfig
+module.exports = withNextra();
--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import "nextra-theme-docs/style.css";
 
 import type { AppProps } from "next/app";
 
