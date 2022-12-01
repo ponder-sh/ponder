b6a25e83998a19166109749e9395d6bd94e30ec5
Olias <0xolias@gmail.com>
2022-12-01 02:31:15 -0500

    docs: fix nextra theme, add getting started page

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,7 +1,12 @@
 const withNextra = require("nextra")({
   theme: "nextra-theme-docs",
   themeConfig: "./theme.config.js",
-  // optional: add `unstable_staticImage: true` to enable Nextra's auto image import
+  unstable_contentDump: true,
+  unstable_defaultShowCopyCode: true,
+  unstable_flexsearch: {
+    codeblocks: true,
+  },
+  unstable_staticImage: true,
 });
 
 module.exports = withNextra();
