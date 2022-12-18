0adea2bac9c3eb6c88bffc699b57bb100f5982e6
Olias <0xolias@gmail.com>
2022-12-18 14:45:28 -0500

    chore(docs): add analytics script

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,7 +1,21 @@
 import "nextra-theme-docs/style.css";
 
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <noscript>
+        {/* eslint-disable @next/next/no-img-element */}
+        <img
+          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          alt=""
+          referrerPolicy="no-referrer-when-downgrade"
+        />
+      </noscript>
+    </>
+  );
 }
