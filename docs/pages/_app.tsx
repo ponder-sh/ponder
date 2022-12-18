ca631868787581380f0e8bbf7e17265464a2f6fd
Olias <0xolias@gmail.com>
2022-12-18 15:18:01 -0500

    chore(docs): use sa subdomain

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -7,11 +7,11 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Script src="https://scripts.simpleanalyticscdn.com/latest.js" />
+      <Script src="https://sa-api.ponder.sh/latest.js" />
       <noscript>
         {/* eslint-disable @next/next/no-img-element */}
         <img
-          src="https://queue.simpleanalyticscdn.com/noscript.gif"
+          src="https://sa-api.ponder.sh/noscript.gif"
           alt=""
           referrerPolicy="no-referrer-when-downgrade"
         />
