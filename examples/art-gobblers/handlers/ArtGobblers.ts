734987efdca7c7854a6b747a16728a31a6ec59b2
Olias <0xolias@gmail.com>
2022-12-27 19:57:44 -0500

    chore(examples): update handler

--- examples/art-gobblers/handlers/ArtGobblers.ts
+++ examples/art-gobblers/handlers/ArtGobblers.ts
@@ -1,14 +1,12 @@
 import { ArtGobbledHandler } from "../generated/handlers";
 
 const handleArtGobbled: ArtGobbledHandler = async (event, context) => {
-  console.log("Art Gobbled!");
-
-  context.entities.GobbledArt.insert(`${event.params.nft}-${event.params.id}`, {
-    id: `${event.params.nft}-${event.params.id}`,
-    user: event.params.user,
-  });
-
-  return;
+  await context.entities.GobbledArt.insert(
+    `${event.params.nft}-${event.params.id}`,
+    {
+      user: event.params.user,
+    }
+  );
 };
 
 export const ArtGobblers = {
