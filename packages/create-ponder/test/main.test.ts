a48209be15c472320c52403dbfc47f5eab0fcc5d
Olias <0xolias@gmail.com>
2023-01-05 15:30:51 -0500

    test(create-ponder): use workspace packages

--- packages/create-ponder/test/main.test.ts
+++ packages/create-ponder/test/main.test.ts
@@ -1,30 +1,29 @@
 import fs from "node:fs";
-import os from "node:os";
 import path from "node:path";
 
 import { run } from "../src/index";
 
 describe("create-ponder", () => {
-  let rootDir: string;
-
-  beforeEach(() => {
-    rootDir = path.join(os.tmpdir(), "create-ponder-tests");
-  });
+  const tmpDir = "../../tmp";
+  const rootDir = path.join(tmpDir, "create-ponder-tests");
 
   afterEach(() => {
-    fs.rmSync(rootDir, { recursive: true, force: true });
+    fs.rmSync(tmpDir, { recursive: true, force: true });
   });
 
   describe("run", () => {
-    it.skip("works", async () => {
-      await run({
-        ponderRootDir: rootDir,
-        etherscanApiKey: process.env.ETHERSCAN_API_KEY,
-        fromEtherscan:
-          "https://etherscan.io/address/0x9746fD0A77829E12F8A9DBe70D7a322412325B91",
-      });
-
-      console.log({ rootDir });
+    it("works", async () => {
+      await run(
+        {
+          ponderRootDir: rootDir,
+          fromEtherscan:
+            "https://etherscan.io/address/0x9746fD0A77829E12F8A9DBe70D7a322412325B91",
+        },
+        {
+          installCommand:
+            'export npm_config_LOCKFILE=false ; pnpm --silent --filter "." install',
+        }
+      );
 
       expect(rootDir).toBe(rootDir);
     });
