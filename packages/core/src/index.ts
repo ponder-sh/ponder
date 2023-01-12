943252a8dc1ca5fdeee67c2ab3a30db9661bbdc9
Olias <0xolias@gmail.com>
2023-01-12 15:31:38 -0500

    feat(core): use esbuild to build ponder config file type export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,9 +1,9 @@
+export type { PonderConfig } from "@/buildPonderConfig";
 export type { ReadOnlyContract } from "@/codegen/ethers-abitype";
 export type { PonderLogger } from "@/common/logger";
 export type { PonderOptions } from "@/common/options";
 export type { EntityStore } from "@/db/entity/entityStore";
 export { Ponder } from "@/Ponder";
-export type { PonderConfig } from "@/readPonderConfig";
 export type {
   Entity,
   Field,
