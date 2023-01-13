ce18b8e3132f5726faeea1483445cda5d47e9fb6
Olias <0xolias@gmail.com>
2023-01-12 19:43:24 -0500

    chore: remove weth and okpc examples

--- examples/okpc/handlers/index.ts
+++
@@ -1,9 +0,0 @@
-import { CanonicalTransactionChain } from "./dir/CanonicalTransactionChain";
-import { OKPC } from "./OKPC";
-
-export default {
-  OKPC,
-  CanonicalTransactionChain,
-};
-
-// haha
