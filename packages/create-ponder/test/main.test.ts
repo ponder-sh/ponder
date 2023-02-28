b07c815a84daf2f65ccd9ac90bc83a3128adf9a3
Olias <0xolias@gmail.com>
2023-02-28 00:51:50 -0500

    test: add create-ponder subgraph id test

--- packages/create-ponder/test/main.test.ts
+++ packages/create-ponder/test/main.test.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from "node:crypto";
 import fs from "node:fs";
 import path from "node:path";
 
@@ -7,9 +8,13 @@ import { run } from "@/index";
 const tmpDir = "../../tmp";
 
 describe("create-ponder", () => {
+  afterAll(() => {
+    fs.rmSync(tmpDir, { recursive: true, force: true });
+  });
+
   describe("from etherscan", () => {
     describe("mainnet (ethfs)", () => {
-      const rootDir = path.join(tmpDir, "create-ponder-tests-mainnet");
+      const rootDir = path.join(tmpDir, randomUUID());
 
       beforeAll(async () => {
         await run(
@@ -26,10 +31,6 @@ describe("create-ponder", () => {
               'export npm_config_LOCKFILE=false ; pnpm --silent --filter "." install',
           }
         );
-      });
-
-      afterAll(() => {
-        fs.rmSync(tmpDir, { recursive: true, force: true });
       });
 
       it("creates project files and directories", async () => {
@@ -79,7 +80,7 @@ describe("create-ponder", () => {
     });
 
     describe("goerli (collector)", () => {
-      const rootDir = path.join(tmpDir, "create-ponder-tests-goerli");
+      const rootDir = path.join(tmpDir, randomUUID());
 
       beforeAll(async () => {
         await run(
@@ -96,10 +97,6 @@ describe("create-ponder", () => {
               'export npm_config_LOCKFILE=false ; pnpm --silent --filter "." install',
           }
         );
-      });
-
-      afterAll(() => {
-        fs.rmSync(tmpDir, { recursive: true, force: true });
       });
 
       it("creates project files and directories", async () => {
@@ -145,6 +142,78 @@ describe("create-ponder", () => {
       it("creates src files", async () => {
         const src = fs.readdirSync(path.join(rootDir, "src"));
         expect(src.sort()).toEqual(["Collector.ts"].sort());
+      });
+    });
+  });
+
+  describe("from subgraph id", () => {
+    describe("arbitrum (sentiment)", () => {
+      const rootDir = path.join(tmpDir, randomUUID());
+
+      beforeAll(async () => {
+        await run(
+          {
+            projectName: "sentiment",
+            rootDir,
+            template: {
+              kind: TemplateKind.SUBGRAPH_ID,
+              id: "Qmd4tEQqAgLUV5SVBp2D92436N2PL8tD4svUz2XYcucKfM",
+            },
+          },
+          {
+            installCommand:
+              'export npm_config_LOCKFILE=false ; pnpm --silent --filter "." install',
+          }
+        );
+      });
+
+      it("creates project files and directories", async () => {
+        const root = fs.readdirSync(rootDir);
+        expect(root.sort()).toEqual(
+          [
+            ".env.local",
+            ".gitignore",
+            ".ponder",
+            "abis",
+            "generated",
+            "src",
+            "node_modules",
+            "package.json",
+            "ponder.config.ts",
+            "schema.graphql",
+            "tsconfig.json",
+          ].sort()
+        );
+      });
+
+      it("downloads abi", async () => {
+        const abiString = fs.readFileSync(
+          path.join(rootDir, `abis/LToken.json`),
+          { encoding: "utf8" }
+        );
+        const abi = JSON.parse(abiString);
+
+        expect(abi.length).toBeGreaterThan(0);
+      });
+
+      it("creates codegen files", async () => {
+        const generated = fs.readdirSync(path.join(rootDir, "generated"));
+        expect(generated.sort()).toEqual(
+          ["index.ts", "app.ts", "contracts", "schema.graphql"].sort()
+        );
+        const contracts = fs.readdirSync(
+          path.join(rootDir, "generated/contracts")
+        );
+        expect(contracts.sort()).toEqual(
+          ["LETH.ts", "LFRAX.ts", "LUSDC.ts", "LUSDT.ts"].sort()
+        );
+      });
+
+      it("creates src files", async () => {
+        const src = fs.readdirSync(path.join(rootDir, "src"));
+        expect(src.sort()).toEqual(
+          ["LETH.ts", "LFRAX.ts", "LUSDC.ts", "LUSDT.ts"].sort()
+        );
       });
     });
   });
