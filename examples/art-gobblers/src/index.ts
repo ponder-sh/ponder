b06cc1a0d7739532ca736526e0cd706c6e9ed619
Olias <0xolias@gmail.com>
2023-03-12 15:50:23 -0400

    chore: remove error

--- examples/art-gobblers/src/index.ts
+++ examples/art-gobblers/src/index.ts
@@ -1,8 +1,6 @@
 import { ponder } from "@/generated";
 
 ponder.on("ArtGobblers:ArtGobbled", async ({ event, context }) => {
-  throw new Error("kek");
-
   await context.entities.GobbledArt.insert(
     `${event.params.nft}-${event.params.id}`,
     {
