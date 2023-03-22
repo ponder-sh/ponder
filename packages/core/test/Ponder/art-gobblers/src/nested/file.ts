bba74b304ac48056d75fdd42ea29279d94aaacf9
Olias <0xolias@gmail.com>
2023-03-22 02:36:22 -0400

    test: update tests for new entity api

--- packages/core/test/Ponder/art-gobblers/src/nested/file.ts
+++ packages/core/test/Ponder/art-gobblers/src/nested/file.ts
@@ -1,10 +1,36 @@
 import { ponder } from "@/generated";
 
 ponder.on("ArtGobblers:GobblerClaimed", async ({ event, context }) => {
-  await context.entities.Account.upsert(event.params.user, {});
+  const { Account, Token } = context.entities;
 
-  await context.entities.Token.upsert(event.params.gobblerId, {
-    owner: event.params.user,
-    claimedBy: event.params.user,
+  await Account.upsert({ id: event.params.user, create: {}, update: {} });
+
+  await Token.upsert({
+    id: event.params.gobblerId,
+    create: {
+      owner: event.params.user,
+      claimedBy: event.params.user,
+    },
+    update: {},
+  });
+
+  await Token.update({
+    id: event.params.gobblerId,
+    data: { owner: event.params.user },
+  });
+
+  const token = await Token.findUnique({ id: event.params.gobblerId });
+  if (!token) throw new Error(`Token not found!`);
+
+  await Token.delete({
+    id: token.id,
+  });
+
+  await Token.create({
+    id: token.id,
+    data: {
+      owner: event.params.user,
+      claimedBy: event.params.user,
+    },
   });
 });
--- packages/core/test/Ponder/ens/src/index.ts
+++ packages/core/test/Ponder/ens/src/index.ts
@@ -3,20 +3,42 @@ import { ponder } from "@/generated";
 ponder.on(
   "BaseRegistrarImplementation:Transfer",
   async ({ event, context }) => {
-    await context.entities.EnsNft.upsert(event.params.tokenId.toString(), {
-      owner: event.params.to,
-      labelHash: "0x" + event.params.tokenId.toString(16),
-      transferredAt: Number(event.block.timestamp),
-      stringArray: ["123", "abc"],
-      intArray: [123, 456],
+    const { EnsNft, Account } = context.entities;
+
+    await EnsNft.upsert({
+      id: event.params.tokenId.toString(),
+      create: {
+        owner: event.params.to,
+        labelHash: "0x" + event.params.tokenId.toString(16),
+        transferredAt: Number(event.block.timestamp),
+        stringArray: ["123", "abc"],
+        intArray: [123, 456],
+      },
+      update: {
+        owner: event.params.to,
+        labelHash: "0x" + event.params.tokenId.toString(16),
+        transferredAt: Number(event.block.timestamp),
+      },
     });
 
-    await context.entities.Account.upsert(event.params.from, {
-      lastActive: Number(event.block.timestamp),
+    await Account.upsert({
+      id: event.params.from,
+      create: {
+        lastActive: Number(event.block.timestamp),
+      },
+      update: {
+        lastActive: Number(event.block.timestamp),
+      },
     });
 
-    await context.entities.Account.upsert(event.params.to, {
-      lastActive: Number(event.block.timestamp),
+    await Account.upsert({
+      id: event.params.to,
+      create: {
+        lastActive: Number(event.block.timestamp),
+      },
+      update: {
+        lastActive: Number(event.block.timestamp),
+      },
     });
   }
 );
