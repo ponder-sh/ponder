47269e583c57d240b17a16c686421d2ab24c4ba5
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-05-25 21:48:28 -0400

    add log filter name and decode in aggregator service

--- packages/core/src/event-aggregator/service.ts
+++ packages/core/src/event-aggregator/service.ts
@@ -1,8 +1,22 @@
 import Emittery from "emittery";
+import { decodeEventLog } from "viem";
 
 import { LogFilter } from "@/config/logFilters";
 import type { Network } from "@/config/networks";
 import type { EventStore } from "@/event-store/store";
+import { isLogMatchedByFilter } from "@/realtime-sync/filter";
+import { Block } from "@/types/block";
+import { Log } from "@/types/log";
+import { Transaction } from "@/types/transaction";
+
+export type LogEvent = {
+  logFilterName: string;
+  eventName: string;
+  params: Record<string, unknown>;
+  log: Log;
+  block: Block;
+  transaction: Transaction;
+};
 
 type EventAggregatorEvents = {
   eventsAvailable: { toTimestamp: number };
@@ -73,7 +87,6 @@ export class EventAggregatorService extends Emittery<EventAggregatorEvents> {
     fromTimestamp: number;
     toTimestamp: number;
   }) => {
-    // Get local events.
     const events = await this.store.getLogEvents({
       fromTimestamp,
       toTimestamp,
@@ -84,9 +97,41 @@ export class EventAggregatorService extends Emittery<EventAggregatorEvents> {
       })),
     });
 
-    // TODO: Get remote events and merge with local events.
+    // Ugly step. We need to associate events with a log filter. There may be duplicates.
+    const hydratedEvents = events.reduce<LogEvent[]>((acc, event) => {
+      const matchedLogFilters = this.logFilters.filter((logFilter) =>
+        isLogMatchedByFilter({
+          log: event.log,
+          address: logFilter.filter.address,
+          topics: logFilter.filter.topics,
+        })
+      );
 
-    return events;
+      matchedLogFilters.forEach((logFilter) => {
+        try {
+          const decodedLog = decodeEventLog({
+            abi: logFilter.abi,
+            data: event.log.data,
+            topics: event.log.topics,
+          });
+
+          acc.push({
+            logFilterName: logFilter.name,
+            eventName: decodedLog.eventName,
+            params: decodedLog.args || {},
+            log: event.log,
+            block: event.block,
+            transaction: event.transaction,
+          });
+        } catch (err) {
+          // pass
+        }
+      });
+
+      return acc;
+    }, []);
+
+    return hydratedEvents;
   };
 
   handleNewHistoricalCheckpoint = ({
