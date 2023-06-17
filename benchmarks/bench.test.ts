fd0423e68277055dc604bb3a5f1e12ee2796b1a5
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-06-17 12:32:08 -0400

    wip subgraph action

---
+++ benchmarks/bench.test.ts
@@ -0,0 +1,14 @@
+import execa from "execa";
+import { beforeAll, test } from "vitest";
+
+beforeAll(async () => {
+  console.log("In beforeAll");
+
+  // Need to exec the `graph build` and `graph deploy` commands here
+  // This setup should not be part of the benchmark.
+  await execa("graph", ["build"]);
+}, 5_000);
+
+test("test", async () => {
+  console.log("In bench.ts!");
+});
