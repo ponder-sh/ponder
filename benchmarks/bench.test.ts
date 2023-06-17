c8eb6b0f3f9c969af3f3760ac2ae00b5ba08d8c0
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-06-17 12:57:29 -0400

    tweaks

--- benchmarks/bench.test.ts
+++ benchmarks/bench.test.ts
@@ -6,7 +6,11 @@ beforeAll(async () => {
 
   // Need to exec the `graph build` and `graph deploy` commands here
   // This setup should not be part of the benchmark.
-  await execa("graph", ["build"]);
+  await execa("graph", [
+    "build",
+    "./subgraph/subgraph.yaml",
+    "--output-dir=./subgraph/build",
+  ]);
 }, 5_000);
 
 test("test", async () => {
