834ea87c4e3c6e5df82f4a1225acafa6dcaaa69d
k-xo <alkassimk@gmail.com>
2023-07-02 23:24:07 +0100

    chore: remove unused imports, move blocks to constants

--- packages/core/src/event-store/store.test.ts
+++ packages/core/src/event-store/store.test.ts
@@ -1,7 +1,15 @@
-import { hexToNumber, RpcBlock, RpcLog, RpcTransaction } from "viem";
+import { hexToNumber } from "viem";
 import { beforeEach, expect, test } from "vitest";
 
-import { usdcContractConfig } from "@/_test/constants";
+import {
+  blockOne,
+  blockOneLogs,
+  blockOneTransactions,
+  blockTwo,
+  blockTwoLogs,
+  blockTwoTransactions,
+  usdcContractConfig,
+} from "@/_test/constants";
 import { setupEventStore } from "@/_test/setup";
 import { blobToBigInt } from "@/utils/decode";
 
@@ -23,174 +31,6 @@ test("setup creates tables", async (context) => {
   expect(tableNames).toContain("logs");
   expect(tableNames).toContain("transactions");
 });
-
-const blockOne: RpcBlock = {
-  baseFeePerGas: "0x0",
-  difficulty: "0x2d3a678cddba9b",
-  extraData: "0x",
-  gasLimit: "0x1c9c347",
-  gasUsed: "0x0",
-  hash: "0xebc3644804e4040c0a74c5a5bbbc6b46a71a5d4010fe0c92ebb2fdf4a43ea5dd",
-  logsBloom:
-    "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-  miner: "0x0000000000000000000000000000000000000000",
-  mixHash: "0x0000000000000000000000000000000000000000000000000000000000000000",
-  nonce: "0x0000000000000000",
-  number: "0xec6fc6",
-  parentHash:
-    "0xe55516ad8029e53cd32087f14653d851401b05245abb1b2d6ed4ddcc597ac5a6",
-  receiptsRoot:
-    "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-  sealFields: [
-    "0x0000000000000000000000000000000000000000000000000000000000000000",
-    "0x0000000000000000",
-  ],
-  sha3Uncles:
-    "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-  size: "0x208",
-  stateRoot:
-    "0x0000000000000000000000000000000000000000000000000000000000000000",
-  timestamp: "0x63198f6f",
-  totalDifficulty: "0xc70d815d562d3cfa955",
-  transactions: [],
-  transactionsRoot:
-    "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-  uncles: [],
-};
-
-const blockOneTransactions: RpcTransaction[] = [
-  // Legacy transaction.
-  {
-    accessList: undefined,
-    blockHash:
-      "0xebc3644804e4040c0a74c5a5bbbc6b46a71a5d4010fe0c92ebb2fdf4a43ea5dd",
-    blockNumber: "0x10f2c",
-    chainId: "0x1",
-    from: "0x1",
-    gas: "0x4234584",
-    gasPrice: "0x45",
-    hash: "0xa4b1f606b66105fa45cb5db23d2f6597075701e7f0e2367f4e6a39d17a8cf98b",
-    input: "0x1",
-    nonce: "0x1",
-    r: "0x1",
-    s: "0x1",
-    to: "0x1",
-    transactionIndex: "0x1",
-    type: "0x0",
-    v: "0x1",
-    value: "0x1",
-  },
-  // EIP-2930 transaction.
-  {
-    accessList: [
-      {
-        address: "0x1",
-        storageKeys: ["0x1"],
-      },
-    ],
-    blockHash:
-      "0xebc3644804e4040c0a74c5a5bbbc6b46a71a5d4010fe0c92ebb2fdf4a43ea5dd",
-    blockNumber: "0x10f2c",
-    chainId: "0x1",
-    from: "0x1",
-    gas: "0x4234584",
-    gasPrice: "0x45",
-    hash: "0xc3f1f606b66105fa45cb5db23d2f6597075701e7f0e2367f4e6a39d17a8cf98b",
-    input: "0x1",
-    maxFeePerGas: undefined,
-    maxPriorityFeePerGas: undefined,
-    nonce: "0x1",
-    r: "0x1",
-    s: "0x1",
-    to: "0x1",
-    transactionIndex: "0x1",
-    type: "0x1",
-    v: "0x1",
-    value: "0x1",
-  },
-];
-
-const blockOneLogs: RpcLog[] = [
-  {
-    address: "0x15d4c048f83bd7e37d49ea4c83a07267ec4203da",
-    blockHash:
-      "0xebc3644804e4040c0a74c5a5bbbc6b46a71a5d4010fe0c92ebb2fdf4a43ea5dd",
-    blockNumber: "0xe6e55f",
-    data: "0x0000000000000000000000000000000000000000000000000000002b3b6fb3d0",
-    logIndex: "0x6c",
-    removed: false,
-    topics: [
-      "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-      "0x000000000000000000000000a00f99bc38b1ecda1fd70eaa1cd31d576a9f46b0",
-      "0x000000000000000000000000f16e9b0d03470827a95cdfd0cb8a8a3b46969b91",
-    ],
-    transactionHash:
-      "0xa4b1f606b66105fa45cb5db23d2f6597075701e7f0e2367f4e6a39d17a8cf98b",
-    transactionIndex: "0x45",
-  },
-  {
-    address: "0x72d4c048f83bd7e37d49ea4c83a07267ec4203da",
-    blockHash:
-      "0xebc3644804e4040c0a74c5a5bbbc6b46a71a5d4010fe0c92ebb2fdf4a43ea5dd",
-    blockNumber: "0xe6e55f",
-    data: "0x0000000000000000000000000000000000000000000000000000002b3b6fb3d0",
-    logIndex: "0x6d",
-    removed: false,
-    topics: [],
-    transactionHash:
-      "0xc3f1f606b66105fa45cb5db23d2f6597075701e7f0e2367f4e6a39d17a8cf98b",
-    transactionIndex: "0x46",
-  },
-];
-
-const blockTwo: RpcBlock = {
-  ...blockOne,
-  number: "0xec6fc7",
-  hash: "0xf123644804e4040c0a74c5a5bbbc6b46a71a5d4010fe0c92ebb2fdf4a43ea5dd",
-  timestamp: "0x63198f70",
-  transactions: [],
-};
-
-const blockTwoTransactions: RpcTransaction[] = [
-  {
-    accessList: undefined,
-    blockHash:
-      "0xf123644804e4040c0a74c5a5bbbc6b46a71a5d4010fe0c92ebb2fdf4a43ea5dd",
-    blockNumber: "0x10f2c",
-    chainId: "0x1",
-    from: "0x1",
-    gas: "0x4234584",
-    gasPrice: "0x45",
-    hash: "0xb5f1f606b66105fa45cb5db23d2f6597075701e7f0e2367f4e6a39d17a8cf98b",
-    input: "0x1",
-    nonce: "0x1",
-    r: "0x1",
-    s: "0x1",
-    to: "0x1",
-    transactionIndex: "0x1",
-    type: "0x0",
-    v: "0x1",
-    value: "0x1",
-  },
-];
-
-const blockTwoLogs: RpcLog[] = [
-  {
-    address: "0x93d4c048f83bd7e37d49ea4c83a07267ec4203da",
-    blockHash:
-      "0xf123644804e4040c0a74c5a5bbbc6b46a71a5d4010fe0c92ebb2fdf4a43ea5dd",
-    blockNumber: "0xec6fc7",
-    data: "0x0000000000000000000000000000000000000000000000000000002b3b6fb3d0",
-    logIndex: "0x6e",
-    removed: false,
-    topics: [
-      "0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef",
-    ],
-    transactionHash:
-      "0xb5f1f606b66105fa45cb5db23d2f6597075701e7f0e2367f4e6a39d17a8cf98b",
-    transactionIndex: "0x1",
-  },
-];
 
 test("insertUnfinalizedBlock inserts block", async (context) => {
   const { eventStore } = context;
