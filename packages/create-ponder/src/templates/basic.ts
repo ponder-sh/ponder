7f9ef719ab2ceddab4e32a5b0a56560815e05d4f
Salief <salieflewis@gmail.com>
2023-07-20 14:51:32 +0200

    add links to schema design tips to generated schema

--- packages/create-ponder/src/templates/basic.ts
+++ packages/create-ponder/src/templates/basic.ts
@@ -1,17 +1,20 @@
-import { writeFileSync } from "node:fs";
-import path from "node:path";
-import prettier from "prettier";
+import { writeFileSync } from 'node:fs';
+import path from 'node:path';
+import prettier from 'prettier';
 
-import type { PartialConfig } from "@/index";
+import type { PartialConfig } from '@/index';
 
 export const fromBasic = ({ rootDir }: { rootDir: string }) => {
   const abiFileContents = `[]`;
 
-  const abiRelativePath = "./abis/ExampleContract.json";
+  const abiRelativePath = './abis/ExampleContract.json';
   const abiAbsolutePath = path.join(rootDir, abiRelativePath);
   writeFileSync(abiAbsolutePath, abiFileContents);
 
   const schemaGraphqlFileContents = `
+  # The entity types defined below map to database tables.
+  # The functions you write as event handlers inside the \`src/\` directory are responsible for creating and updating records in those tables.
+  # Visit the [documentation](https://ponder.sh/guides/design-your-schema) or view the [\`examples/\`](https://github.com/0xOlias/ponder/tree/main/examples) directory for further guidance on designing your schema.
     type ExampleToken @entity {
       id: String!
       tokenId: Int!
@@ -24,26 +27,26 @@ export const fromBasic = ({ rootDir }: { rootDir: string }) => {
   `;
 
   // Generate the schema.graphql file.
-  const ponderSchemaFilePath = path.join(rootDir, "schema.graphql");
+  const ponderSchemaFilePath = path.join(rootDir, 'schema.graphql');
   writeFileSync(
     ponderSchemaFilePath,
-    prettier.format(schemaGraphqlFileContents, { parser: "graphql" })
+    prettier.format(schemaGraphqlFileContents, { parser: 'graphql' })
   );
 
   // Build the partial ponder config.
   const config: PartialConfig = {
     networks: [
       {
-        name: "mainnet",
+        name: 'mainnet',
         chainId: 1,
         rpcUrl: `process.env.PONDER_RPC_URL_1`,
       },
     ],
     contracts: [
       {
-        name: "ExampleContract",
-        network: "mainnet",
-        address: "0x0",
+        name: 'ExampleContract',
+        network: 'mainnet',
+        address: '0x0',
         abi: abiRelativePath,
         startBlock: 1234567,
       },
