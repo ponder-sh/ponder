e0932dee3a6920c5e0c1de5bc4d0867b6dbbb753
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-07-25 15:07:05 -0400

    also remove Options

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,5 @@
 export { PonderApp } from "@/build/handlers";
 export type { Config, ResolvedConfig } from "@/config/config";
-export type { Options } from "@/config/options";
 export type { Block } from "@/types/block";
 export type { ReadOnlyContract } from "@/types/contract";
 export type { Log } from "@/types/log";
