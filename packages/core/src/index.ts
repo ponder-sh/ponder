9819706e8f0e2440fa7d8a1ac675eeed1ec87ddc
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-07-25 15:05:49 -0400

    stop exporting Ponder

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,6 @@
 export { PonderApp } from "@/build/handlers";
 export type { Config, ResolvedConfig } from "@/config/config";
 export type { Options } from "@/config/options";
-export { Ponder } from "@/Ponder";
 export type { Block } from "@/types/block";
 export type { ReadOnlyContract } from "@/types/contract";
 export type { Log } from "@/types/log";
