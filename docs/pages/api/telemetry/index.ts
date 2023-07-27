d609767d38b59bd9d5638116da1a43081384de1d
Elio Briceño <eliobricenov@gmail.com>
2023-07-27 15:24:08 -0400

    chore(docs-telemetry): use segment analytics sdk

--- docs/pages/api/telemetry/index.ts
+++ docs/pages/api/telemetry/index.ts
@@ -1,49 +1,38 @@
+import { Analytics } from "@segment/analytics-node";
 import { NextApiRequest, NextApiResponse } from "next";
 
-async function forwardTelemetry(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === "POST") {
-    try {
-      if (!process.env.SEGMENT_WRITE_KEY) {
-        throw new Error("No Segment write key found.");
-      }
-
-      const telemetryPayload = req.body;
-      const segmentWriteKey = process.env.SEGMENT_WRITE_KEY;
-      const segmentEndpoint = "https://api.segment.io/v1/track";
-      const authorization = `${segmentWriteKey}:`;
-
-      const response = await fetch(segmentEndpoint, {
-        method: "POST",
-        headers: {
-          Authorization: `Basic ${Buffer.from(authorization).toString(
-            "base64"
-          )}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(telemetryPayload),
-      });
-
-      if (response.ok) {
-        res.status(200).json({
-          message: "Telemetry data processed successfully.",
-        });
-      } else {
-        const errorMessage = response.statusText;
-        console.error("Error processing telemetry data", errorMessage);
-        res.status(response.status).json({
-          error: "An error occurred while processing telemetry data.",
-        });
-      }
-    } catch (error) {
-      console.error("Error forwarding telemetry data to Segment API:", error);
-      res
-        .status(500)
-        .json({ error: "An error occurred while processing telemetry data." });
-    }
-  } else {
+export default async function forwardTelemetry(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
     res.setHeader("Allow", ["POST"]);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  if (!process.env.SEGMENT_WRITE_KEY) {
+    console.error('Missing required environment variable "SEGMENT_WRITE_KEY".');
+    return res
+      .status(500)
+      .json({ error: "An error occurred while processing telemetry data." });
+  }
+
+  try {
+    const analytics = new Analytics({
+      writeKey: process.env.SEGMENT_WRITE_KEY,
+    });
+
+    await analytics.track({
+      ...req.body,
+    });
+
+    res.status(200).json({
+      message: "Telemetry data processed successfully.",
+    });
+  } catch (error) {
+    console.error("Error forwarding telemetry data to Segment API:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while processing telemetry data." });
   }
 }
-
-export default forwardTelemetry;
