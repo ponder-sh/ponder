df12bdbaa14187d4af3e9a2bedcc88e8b1b78ae9
Elio Briceño <eliobricenov@gmail.com>
2023-08-03 11:49:50 -0400

    chore(telemetry): move instantiation outside of function because we're now using nodejs environment

--- docs/pages/api/telemetry/index.ts
+++ docs/pages/api/telemetry/index.ts
@@ -1,26 +1,23 @@
 import { Analytics } from "@segment/analytics-node";
 import { NextApiRequest, NextApiResponse } from "next";
 
+if (!process.env.SEGMENT_WRITE_KEY) {
+  throw new Error('Missing required environment variable "SEGMENT_WRITE_KEY".');
+}
+
+const analytics = new Analytics({
+  writeKey: process.env.SEGMENT_WRITE_KEY,
+});
+
 export default async function forwardTelemetry(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (!process.env.SEGMENT_WRITE_KEY) {
-    console.error('Missing required environment variable "SEGMENT_WRITE_KEY".');
-    return res.status(500).json({
-      error: `Server error`,
-    });
-  }
-
   if (req.method !== "POST") {
     return res.status(405).json({
       error: `Method ${req.method} Not Allowed`,
     });
   }
-
-  const analytics = new Analytics({
-    writeKey: process.env.SEGMENT_WRITE_KEY,
-  });
 
   res.status(200).json({
     message: "Telemetry data processed successfully.",
