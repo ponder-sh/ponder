49b35a8eb22a4440d9fe30e535573f26fc6a54b2
Elio Briceño <eliobricenov@gmail.com>
2023-08-02 22:41:41 -0400

    chore(telemetry-api): remove variable because it is only read once

--- docs/pages/api/telemetry/index.ts
+++ docs/pages/api/telemetry/index.ts
@@ -22,13 +22,11 @@ export default async function forwardTelemetry(
     writeKey: process.env.SEGMENT_WRITE_KEY,
   });
 
-  const data = req.body;
-
   res.status(200).json({
     message: "Telemetry data processed successfully.",
   });
 
   await analytics.track({
-    ...data,
+    ...req.body,
   });
 }
