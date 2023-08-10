3abbd046b88b46f1c1823cc55c0f71c7014d5a59
Elio Briceño <eliobricenov@gmail.com>
2023-08-10 09:05:37 -0400

    chore(eslint-rule): add comments

--- packages/eslint-plugin-indexing/src/rule.ts
+++ packages/eslint-plugin-indexing/src/rule.ts
@@ -15,10 +15,14 @@ const rule: TSESLint.RuleModule<MessageIds> = {
   create: (context) => ({
     CallExpression: (node) => {
       const { callee } = node;
+
+      // Check if the callee is a member expression aka a function call
+      // e.g. context.upsert({})
       if (callee.type !== "MemberExpression") return;
       if (callee.property.type !== "Identifier") return;
       if (callee.property.name !== "upsert") return;
 
+      // check if the call is preceded by an await expression
       const ancestor = context
         .getAncestors()
         .find((ancestor) => ancestor.type === "AwaitExpression");
