c96894f9ad1017fdb7e42fc34a9c16271e4d9c9c
Elio Briceño <eliobricenov@gmail.com>
2023-08-13 22:51:05 -0400

    feat(eslint-config): add extensible eslint-config

---
+++ packages/eslint-config/src/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: ["plugin:@typescript-eslint/recommended-type-checked"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: "./",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-floating-promises": "error",
+  },
+};
--- packages/eslint-plugin-indexing/src/index.ts
+++
@@ -1,5 +0,0 @@
-import rule from "./rule";
-
-export const rules = {
-  "await-upsert": rule,
-};
