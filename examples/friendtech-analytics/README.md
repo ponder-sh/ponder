b781643e0076d1265c27640ca4470919bf6c39d0
Slokh <slokhx@gmail.com>
2023-08-15 22:53:05 -0400

    refactor: use upsert fn api

--- examples/friendtech-analytics/README.md
+++
@@ -1,1 +0,0 @@
-# friendtech-analytics
--- examples/friendtech-analytics/src/FriendtechSharesV1.ts
+++ examples/friendtech-analytics/src/FriendtechSharesV1.ts
@@ -1,7 +1,7 @@
 import { ponder } from "@/generated";
 
 ponder.on("FriendtechSharesV1:Trade", async ({ event, context }) => {
-  const { Protocol, Share, Subject, TradeEvent, Trader } = context.entities;
+  const { Share, Subject, TradeEvent, Trader } = context.entities;
 
   const subjectId = event.params.subject;
   const traderId = event.params.trader;
@@ -16,167 +16,106 @@ ponder.on("FriendtechSharesV1:Trade", async ({ event, context }) => {
     ? event.params.ethAmount + feeAmount
     : event.params.ethAmount - feeAmount;
 
-  let [tradeEvent, subject, trader] = await Promise.all([
-    TradeEvent.create({
-      id: tradeEventId,
-      data: {
+  const tradeEvent = await TradeEvent.create({
+    id: tradeEventId,
+    data: {
+      subject: subjectId,
+      trader: traderId,
+      shareAmount: event.params.shareAmount,
+      isBuy: event.params.isBuy,
+      ethAmount: event.params.ethAmount,
+      protocolEthAmount: event.params.protocolEthAmount,
+      subjectEthAmount: event.params.subjectEthAmount,
+      supply: event.params.supply,
+      timestamp: Number(event.block.timestamp),
+      traderAmount: traderAmount,
+    },
+  });
+
+  await Subject.upsert({
+    id: subjectId,
+    create: {
+      totalTrades: 0n,
+      totalShares: 0n,
+      lastPrice: 0n,
+      earnings: 0n,
+      traderVolume: 0n,
+      protocolFeesGenerated: 0n,
+    },
+    update: ({ current }) => {
+      const shareDelta = tradeEvent.isBuy
+        ? tradeEvent.shareAmount
+        : -tradeEvent.shareAmount;
+
+      const traderSpend = tradeEvent.isBuy
+        ? traderAmount
+        : tradeEvent.ethAmount;
+
+      return {
+        totalTrades: current.totalTrades + 1n,
+        totalShares: current.totalShares + shareDelta,
+        lastPrice:
+          tradeEvent.shareAmount > 0
+            ? traderSpend / tradeEvent.shareAmount
+            : 0n,
+        earnings: current.earnings + tradeEvent.subjectEthAmount,
+        traderVolume: current.traderVolume + traderSpend,
+        protocolFeesGenerated:
+          current.protocolFeesGenerated + tradeEvent.protocolEthAmount,
+      };
+    },
+  });
+
+  await Trader.upsert({
+    id: traderId,
+    create: {
+      totalTrades: 0n,
+      spend: 0n,
+      earnings: 0n,
+      profit: 0n,
+      subjectFeesPaid: 0n,
+      protocolFeesPaid: 0n,
+    },
+    update: ({ current }) => {
+      const spendDelta = tradeEvent.isBuy ? traderAmount : 0n;
+      const earningsDelta = tradeEvent.isBuy ? 0n : traderAmount;
+      const profitDelta = tradeEvent.isBuy ? -traderAmount : traderAmount;
+      return {
+        totalTrades: current.totalTrades + 1n,
+        spend: current.spend + spendDelta,
+        earnings: current.earnings + earningsDelta,
+        profit: current.profit + profitDelta,
+        subjectFeesPaid: current.subjectFeesPaid + tradeEvent.subjectEthAmount,
+        protocolFeesPaid:
+          current.protocolFeesPaid + tradeEvent.protocolEthAmount,
+      };
+    },
+  });
+
+  if (tradeEvent.isBuy) {
+    await Share.upsert({
+      id: shareId,
+      create: {
         subject: subjectId,
         trader: traderId,
-        shareAmount: event.params.shareAmount,
-        isBuy: event.params.isBuy,
-        ethAmount: event.params.ethAmount,
-        protocolEthAmount: event.params.protocolEthAmount,
-        subjectEthAmount: event.params.subjectEthAmount,
-        supply: event.params.supply,
-        timestamp: Number(event.block.timestamp),
-        traderAmount: traderAmount,
+        shareAmount: tradeEvent.shareAmount,
       },
-    }),
-    Subject.findUnique({ id: subjectId }),
-    Trader.findUnique({ id: traderId }),
-  ]);
-
-  if (subject || trader) {
-    throw Error("test");
-  }
-
-  // Get or create Subject
-  if (!subject) {
-    subject = await Subject.create({
-      id: subjectId,
-      data: {
-        totalTrades: 0n,
-        totalShares: 0n,
-        lastPrice: 0n,
-        earnings: 0n,
-        traderVolume: 0n,
-        protocolFeesGenerated: 0n,
-      },
-    });
-  }
-
-  // Get or create Trader
-  if (!trader) {
-    trader = await Trader.create({
-      id: traderId,
-      data: {
-        totalTrades: 0n,
-        spend: 0n,
-        earnings: 0n,
-        profit: 0n,
-        subjectFeesPaid: 0n,
-        protocolFeesPaid: 0n,
-      },
-    });
-  }
-
-  const share = await Share.findUnique({ id: shareId });
-
-  // Buy
-  if (tradeEvent.isBuy) {
-    let sharePromise;
-
-    // Share exists before buy
-    if (share) {
-      sharePromise = Share.update({
-        id: shareId,
-        data: {
-          shareAmount: share.shareAmount + tradeEvent.shareAmount,
-        },
-      });
-
-      // Share does not exist before buy
-    } else {
-      sharePromise = Share.create({
-        id: shareId,
-        data: {
-          subject: subjectId,
-          trader: traderId,
-          shareAmount: tradeEvent.shareAmount,
-        },
-      });
-    }
-
-    await Promise.all([
-      Subject.update({
-        id: subjectId,
-        data: {
-          totalTrades: subject.totalTrades + 1n,
-          totalShares: subject.totalShares + tradeEvent.shareAmount,
-          lastPrice:
-            tradeEvent.shareAmount > 0
-              ? tradeEvent.ethAmount / tradeEvent.shareAmount
-              : 0n,
-          earnings: subject.earnings + tradeEvent.subjectEthAmount,
-          traderVolume: subject.traderVolume + traderAmount,
-          protocolFeesGenerated:
-            subject.protocolFeesGenerated + tradeEvent.protocolEthAmount,
-        },
+      update: ({ current }) => ({
+        shareAmount: current.shareAmount + tradeEvent.shareAmount,
       }),
-      Trader.update({
-        id: traderId,
-        data: {
-          totalTrades: subject.totalTrades + 1n,
-          spend: trader.spend + traderAmount,
-          profit: trader.profit - traderAmount,
-          subjectFeesPaid: trader.subjectFeesPaid + tradeEvent.subjectEthAmount,
-          protocolFeesPaid:
-            trader.protocolFeesPaid + tradeEvent.protocolEthAmount,
-        },
+    });
+  } else {
+    const share = await Share.update({
+      id: shareId,
+      data: ({ current }) => ({
+        shareAmount: current.shareAmount - tradeEvent.shareAmount,
       }),
-      sharePromise,
-    ]);
+    });
 
-    // Sell
-  } else if (share) {
-    const shareAmountAfter = share.shareAmount - tradeEvent.shareAmount;
-
-    let sharePromise;
-
-    // Share exists after sell
-    if (shareAmountAfter > 0) {
-      sharePromise = Share.update({
-        id: shareId,
-        data: {
-          shareAmount: share.shareAmount - tradeEvent.shareAmount,
-        },
-      });
-
-      // Share does not exist after sell
-    } else {
-      sharePromise = Share.delete({
+    if (share.shareAmount === 0n) {
+      await Share.delete({
         id: shareId,
       });
     }
-
-    await Promise.all([
-      Subject.update({
-        id: subjectId,
-        data: {
-          totalTrades: subject.totalTrades + 1n,
-          totalShares: subject.totalShares - tradeEvent.shareAmount,
-          lastPrice:
-            tradeEvent.shareAmount > 0
-              ? tradeEvent.ethAmount / tradeEvent.shareAmount
-              : 0n,
-          earnings: subject.earnings + tradeEvent.subjectEthAmount,
-          traderVolume: subject.traderVolume + tradeEvent.ethAmount,
-          protocolFeesGenerated:
-            subject.protocolFeesGenerated + tradeEvent.protocolEthAmount,
-        },
-      }),
-      Trader.update({
-        id: traderId,
-        data: {
-          totalTrades: subject.totalTrades + 1n,
-          earnings: trader.spend + traderAmount,
-          profit: trader.profit + traderAmount,
-          subjectFeesPaid: trader.subjectFeesPaid + tradeEvent.subjectEthAmount,
-          protocolFeesPaid:
-            trader.protocolFeesPaid + tradeEvent.protocolEthAmount,
-        },
-      }),
-      sharePromise,
-    ]);
   }
 });
