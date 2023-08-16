0e0954d2a15c9b18a422dbf2ab1d8abc0fb2013e
Slokh <slokhx@gmail.com>
2023-08-15 22:56:33 -0400

    fix: add max block range

--- examples/friendtech/ponder.config.ts
+++ examples/friendtech/ponder.config.ts
@@ -11,6 +11,7 @@ export const config: Config = {
       abi: "./abis/FriendtechSharesV1.json",
       address: "0xcf205808ed36593aa40a44f10c7f7c2f67d4a4d4",
       startBlock: 2430440,
+      maxBlockRange: 100,
     },
   ],
 };
