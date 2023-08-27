17bccf6a0c997578d6fd02f2168332e92780d3da
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-08-26 23:43:04 -0400

    refresh intervals code

--- packages/core/src/historical-sync/intervals.test.ts
+++ packages/core/src/historical-sync/intervals.test.ts
@@ -1,5 +1,39 @@
-import { test } from "vitest";
+import { expect, test } from "vitest";
 
-test.todo("findMissingIntervals", () => {
-  // TODO
+import { removeIntervalsFromIntervals } from "./intervals";
+
+test("removeIntervalsFromIntervals handles overlap", () => {
+  expect(removeIntervalsFromIntervals([[1, 5]], [[0, 3]])).toMatchObject([
+    [4, 5],
+  ]);
+});
+
+test("removeIntervalsFromIntervals handles double overlap", () => {
+  expect(
+    removeIntervalsFromIntervals(
+      [
+        [1, 5],
+        [10, 15],
+      ],
+      [[3, 12]]
+    )
+  ).toMatchObject([
+    [1, 2],
+    [13, 15],
+  ]);
+});
+
+test("removeIntervalsFromIntervals handles unaffected interval", () => {
+  expect(
+    removeIntervalsFromIntervals(
+      [
+        [1, 5],
+        [9, 12],
+      ],
+      [[0, 3]]
+    )
+  ).toMatchObject([
+    [4, 5],
+    [9, 12],
+  ]);
 });
--- packages/core/src/historical-sync/intervals.ts
+++ packages/core/src/historical-sync/intervals.ts
@@ -1,10 +1,10 @@
 // These methods are used in the cached interval calculations
 // From https://stackoverflow.com/a/33857786/12841788
 
-/* This method is just a small helper method that takes an array
- * and returns a new array with duplicates removed
+/**
+ * Removes duplicate elements from an array.
  */
-function remove_duplicates(arr: number[][]) {
+function removeDuplicates(arr: [number, number][]) {
   const lookup: Record<string, number> = {};
   const results = [];
   for (let i = 0; i < arr.length; i++) {
@@ -17,49 +17,96 @@ function remove_duplicates(arr: number[][]) {
   return results;
 }
 
-/* This method takes 2 points p1 and p2 and returns an array of
- * points with the range of p2 removed, i.e. p1 = [1,7]
- * p2 = [4,5] returned = [[1,3],[6,7]]
+/**
+ * Given an interval and an interval to exclude, return an array of
+ * remaining intervals.
+ *
+ * ```ts
+ * removeIntervalFromInterval([1,7], [4,5])
+ * // [[1,3],[6,7]]
+ * ```
  */
-function p1_excluding_p2(p1: number[], p2: number[]) {
-  if (p1[1] < p2[0]) return [p1]; // line p1 finishes before the exclusion line p2
-  if (p1[0] > p2[1]) return [p1]; // line p1 starts after exclusion line p1
-  const lines = [];
-  // calculate p1 before p2 starts
-  const line1 = [p1[0], Math.min(p1[1], p2[0] - 1)];
-  if (line1[0] <= line1[1]) lines.push(line1);
-  // calculate p1 after p2 ends
-  const line2 = [p2[1] + 1, p1[1]];
-  if (line2[0] <= line2[1]) lines.push(line2);
-  // these contain the lines we calculated above
-  return lines;
+function removeIntervalFromInterval(
+  interval: [number, number],
+  intervalToExclude: [number, number]
+) {
+  if (interval[1] < intervalToExclude[0]) return [interval]; // interval finishes before intervalToExclude starts
+  if (interval[0] > intervalToExclude[1]) return [interval]; // interval starts after intervalToExclude ends
+  const intervals = [];
+  // calculate interval before intervalToExclude starts
+  const line1 = [interval[0], Math.min(interval[1], intervalToExclude[0] - 1)];
+  if (line1[0] <= line1[1]) intervals.push(line1);
+  // calculate interval after intervalToExclude ends
+  const line2 = [intervalToExclude[1] + 1, interval[1]];
+  if (line2[0] <= line2[1]) intervals.push(line2);
+  return intervals as [number, number][];
 }
 
-/* this performs the exact same operation as above, only it allows you to pass
- * multiple points (but still just 1 exclusion point) and returns results
- * in an identical format as above, i.e. points = [[1,7],[0,1]]
- *  p2 = [4,5] returned = [[0,1],[1,3],[6,7]]
+/**
+ * Given a list of intervals and one exclusion interval,
+ * return the remaining intervals.
+ *
+ * ```ts
+ * removeIntervalFromIntervals([[1,7],[0,1]], [4,5])
+ * // [[0,1],[1,3],[6,7]]
+ * ```
  */
-function points_excluding_p2(points: number[][], p2: number[]) {
-  const results: number[][] = [];
-  for (let i = 0; i < points.length; i++) {
-    const lines = p1_excluding_p2(points[i], p2);
-    results.push(...lines);
+function removeIntervalFromIntervals(
+  intervals: [number, number][],
+  intervalToExclude: [number, number]
+) {
+  const results: [number, number][] = [];
+  for (let i = 0; i < intervals.length; i++) {
+    results.push(
+      ...removeIntervalFromInterval(intervals[i], intervalToExclude)
+    );
   }
   return results;
 }
 
 /**
- * Previously called p1_excluding_all.
- * this method performs the same operation only this time it takes one point
- * and multiple exclusion points and returns an array of the results.
- * this is the important method of: given 1 point and many
- * exclusion points, return the remaining new ranges
+ * Given one interval and a list of exclusion intervals,
+ * return the remaining intervals.
  */
-export function findMissingIntervals(p1: number[], exclude: number[][]) {
-  let checking = [p1];
-  for (let i = 0; i < exclude.length; i++) {
-    checking = points_excluding_p2(checking, exclude[i]);
+export function removeIntervalsFromInterval(
+  interval: [number, number],
+  intervalsToExclude: [number, number][]
+) {
+  let checking = [interval];
+  for (let i = 0; i < intervalsToExclude.length; i++) {
+    checking = removeIntervalFromIntervals(checking, intervalsToExclude[i]);
   }
-  return remove_duplicates(checking);
+  return removeDuplicates(checking);
+}
+
+/**
+ * Given a list of intervals and a list of exclusion intervals,
+ * return the remaining intervals.
+ *
+ * ```ts
+ * removeIntervalsFromIntervals([[3,9],[0,2]], [[4,5],[0,1]])
+ * // [[1,2],[3,4],[6,9]]
+ * ```
+ */
+export function removeIntervalsFromIntervals(
+  intervals: [number, number][],
+  intervalsToExclude: [number, number][]
+) {
+  let remainingIntervals: [number, number][] = intervals;
+
+  for (let i = 0; i < intervalsToExclude.length; i++) {
+    const intervalToExclude = intervalsToExclude[i];
+    const newRemainingIntervals: [number, number][] = [];
+
+    for (let j = 0; j < remainingIntervals.length; j++) {
+      const interval = remainingIntervals[j];
+      newRemainingIntervals.push(
+        ...removeIntervalFromInterval(interval, intervalToExclude)
+      );
+    }
+
+    remainingIntervals = newRemainingIntervals;
+  }
+
+  return removeDuplicates(remainingIntervals);
 }
