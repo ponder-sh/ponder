fb83b651094f5e305179b622ed0d3559aedffa6c
Omar <github@omaraziz.dev>
2023-10-03 10:59:42 -0700

    upgrade ESLint, ESLint plugins & prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,35 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint", "simple-import-sort", "import", "prettier"],
+  env: {
+    node: true,
+  },
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {},
+      {
+        usePrettierrc: true,
+        fileInfoOptions: {
+          withNodeModules: true,
+        },
+      },
+    ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/ban-types": "off",
+  },
+  ignorePatterns: ["dist", "node_modules", "generated"],
+};
---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  tabWidth: 2,
+  printWidth: 80,
+  endOfLine: "auto",
+  trailingComma: "es5",
+};
