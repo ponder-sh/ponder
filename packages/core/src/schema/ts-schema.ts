92b8f3eda87e4c1267161d3a4f57f3a723f7c667
Kyle Scott <kyscott@umich.edu>
2023-10-02 22:00:46 -0400

    start to infer entity types

--- packages/core/src/schema/ts-schema.ts
+++ packages/core/src/schema/ts-schema.ts
@@ -7,6 +7,8 @@ export const boolean = { optional: false, data: "boolean" } as const;
 export const bytes = { optional: false, data: "bytes" } as const;
 export const bigint = { optional: false, data: "bigint" } as const;
 
+type bytes = `0x${string}`;
+
 type Scalar = (
   | typeof string
   | typeof number
@@ -34,7 +36,7 @@ type List<TData extends Scalar | unknown = unknown> = {
  * SQL Schema types
  */
 type Column<
-  TData extends Scalar | List<Scalar> | unknown = unknown,
+  TData extends Scalar | List | unknown = unknown,
   TOptional extends boolean | unknown = unknown
 > = {
   optional: TOptional;
@@ -91,3 +93,55 @@ export const createTable = <
   name: TName,
   columns: TColumns
 ): Table<TName, TColumns> => ({ name, columns });
+
+export type RecoverScalarType<TScalar extends Scalar> = TScalar extends "string"
+  ? string
+  : TScalar extends "number"
+  ? number
+  : TScalar extends "boolean"
+  ? boolean
+  : TScalar extends "bytes"
+  ? bytes
+  : TScalar extends "bigint"
+  ? bigint
+  : never;
+
+export type TableNames<TTables extends readonly Table[]> = {
+  [key in keyof TTables]: TTables[key]["name"];
+};
+
+export type TNamedEntity<TTable extends Table> = TTable extends {
+  name: infer TName extends string;
+}
+  ? Record<TName, {}>
+  : never;
+
+export type RecoverIDType<TTable extends Table> = TTable extends {
+  columns: { id: infer ID extends { data: Scalar } };
+}
+  ? RecoverScalarType<ID["data"]>
+  : never;
+
+export type RecoverOptionalType<
+  TColumn extends Column,
+  TKey extends string,
+  TData
+> = TColumn["optional"] extends false
+  ? Record<TKey, TData>
+  : Partial<Record<TKey, TData>>;
+
+export type RecoverListType<TList extends List> = TList extends {
+  data: infer Data extends Scalar;
+}
+  ? RecoverScalarType<Data>[]
+  : never;
+
+export type RecoverColumnType<TColumn extends Column> = TColumn extends {
+  data: infer _list extends List;
+}
+  ? RecoverListType<_list>
+  : TColumn extends {
+      data: infer _scalar extends Scalar;
+    }
+  ? RecoverScalarType<_scalar>
+  : never;
