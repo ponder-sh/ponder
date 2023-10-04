8673f74d070fb1e5d4b6701e6682257701a31379
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-10-04 18:18:31 -0400

    add factory example

--- examples/factory-llama/ponder.config.ts
+++ examples/factory-llama/ponder.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "@ponder/core";
+import { parseAbiItem } from "abitype";
 
 import LlamaCoreAbi from "./abis/LlamaCore.json";
 import LlamaFactoryAbi from "./abis/LlamaFactory.json";
@@ -7,9 +8,25 @@ export const config: Config = {
   networks: [
     {
       name: "sepolia",
-      chainId: 1,
-      rpcUrl: process.env.PONDER_RPC_URL_1,
+      chainId: 11155111,
+      rpcUrl: process.env.PONDER_RPC_URL_11155111,
     },
   ],
-  factories: [{}],
+  factories: [
+    {
+      name: "LlamaFactory",
+      network: "sepolia",
+      address: "0xFf5d4E226D9A3496EECE31083a8F493edd79AbEB",
+      startBlock: 4121269,
+      abi: LlamaFactoryAbi,
+      factoryEvent: parseAbiItem(
+        "event LlamaInstanceCreated(address indexed deployer, string indexed name, address llamaCore, address llamaExecutor, address llamaPolicy, uint256 chainId)"
+      ),
+      factoryEventAddressArgument: "llamaCore",
+      child: {
+        name: "LlamaCore",
+        abi: LlamaCoreAbi,
+      },
+    },
+  ],
 };
--- examples/factory-llama/src/LlamaCore.ts
+++ examples/factory-llama/src/LlamaCore.ts
@@ -1,1 +1,7 @@
 import { ponder } from "@/generated";
+
+ponder.on("LlamaCore:ActionCreated", async ({ event }) => {
+  console.log(
+    `Handling ActionCreated event from LlamaCore @ ${event.log.address}`
+  );
+});
