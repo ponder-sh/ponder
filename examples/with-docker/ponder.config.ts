80b1813fc29d6d1cc277f3435983417687589123
Kyle Scott <kyscott@umich.edu>
2023-11-04 15:41:20 -0400

    docker example

--- examples/with-docker/ponder.config.ts
+++ examples/with-docker/ponder.config.ts
@@ -1,11 +1,13 @@
-import type { Config } from "@ponder/core";
+import { createConfig } from "@ponder/core";
 import { http } from "viem";
+
+import { AdventureGoldAbi } from "./abis/AdventureGold.abi";
 
 if (!process.env.PONDER_RPC_URL_1) {
   throw new Error("Missing PONDER_RPC_URL_1 environment variable");
 }
 
-export const config: Config = {
+export const config = createConfig({
   networks: [
     {
       name: "mainnet",
@@ -16,11 +18,11 @@ export const config: Config = {
   contracts: [
     {
       name: "AdventureGold",
-      network: "mainnet",
-      abi: "./abis/AdventureGold.json",
+      network: [{ name: "mainnet" }],
+      abi: AdventureGoldAbi,
       address: "0x32353A6C91143bfd6C7d363B546e62a9A2489A20",
       startBlock: 13142655,
       endBlock: 13150000,
     },
   ],
-};
+});
