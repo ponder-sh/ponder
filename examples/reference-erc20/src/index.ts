79a05aea50ccfa5168058b3cde6f93a3d07680a5
Kyle Scott <kyscott@umich.edu>
2023-11-14 15:07:37 -0500

    erc20 example

---
+++ examples/reference-erc20/src/index.ts
@@ -0,0 +1,71 @@
+import { ponder } from "@/generated";
+
+ponder.on("ERC20:Transfer", async ({ event, context }) => {
+  const { Account, TransferEvent } = context.models;
+
+  // Create an Account for the sender, or update the balance if it already exists.
+  await Account.upsert({
+    id: event.params.from,
+    create: {
+      balance: BigInt(0),
+      isOwner: false,
+    },
+    update: ({ current }) => ({
+      balance: current.balance - event.params.value,
+    }),
+  });
+
+  // Create an Account for the recipient, or update the balance if it already exists.
+  await Account.upsert({
+    id: event.params.to,
+    create: {
+      balance: event.params.value,
+      isOwner: false,
+    },
+    update: ({ current }) => ({
+      balance: current.balance + event.params.value,
+    }),
+  });
+
+  // Create a TransferEvent.
+  await TransferEvent.create({
+    id: event.log.id,
+    data: {
+      fromId: event.params.from,
+      toId: event.params.to,
+      amount: event.params.value,
+      timestamp: Number(event.block.timestamp),
+    },
+  });
+});
+
+ponder.on("ERC20:Approval", async ({ event, context }) => {
+  const { Approval, ApprovalEvent } = context.models;
+
+  const approvalId =
+    `${event.params.owner}-${event.params.spender}` as `0x${string}`;
+
+  // Create or update the Approval.
+  await Approval.upsert({
+    id: approvalId,
+    create: {
+      ownerId: event.params.owner,
+      spenderId: event.params.spender,
+      amount: event.params.value,
+    },
+    update: {
+      amount: event.params.value,
+    },
+  });
+
+  // Create an ApprovalEvent.
+  await ApprovalEvent.create({
+    id: event.log.id,
+    data: {
+      ownerId: event.params.owner,
+      spenderId: event.params.spender,
+      amount: event.params.value,
+      timestamp: Number(event.block.timestamp),
+    },
+  });
+});
