9541a5e267dd3a38070c3c09a238623f439a68f2
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-11-17 12:35:23 -0500

    custom landing page

---
+++ docs/components/LandingPage.tsx
@@ -0,0 +1,12 @@
+export function LandingPage() {
+  return (
+    <>
+      <main className="">
+        <h1 className="mt-12">Ponder</h1>
+        <p className="">
+          Ponder is a TypeScript framework for crypto application backends.
+        </p>
+      </main>
+    </>
+  );
+}
---
+++ docs/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./components/**/*.{md,mdx,tsx}", "./pages/**/*.{md,mdx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
