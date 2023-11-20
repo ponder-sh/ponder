315bc507809e05d1cebb6f966786fc17972da0ec
Kyle Scott <kyscott@umich.edu>
2023-11-20 18:40:17 -0500

    added updated ponder-env to templates

--- packages/create-ponder/templates/default/ponder-env.d.ts
+++ packages/create-ponder/templates/default/ponder-env.d.ts
@@ -2,7 +2,16 @@ declare module "@/generated" {
   import type { PonderApp } from "@ponder/core";
 
   export const ponder: PonderApp<
-    typeof import("./ponder.config.js").default,
-    typeof import("./ponder.schema.js").default
+    typeof import("./ponder.config.ts").default,
+    typeof import("./ponder.schema.ts").default
   >;
+
+  export type Context = Parameters<
+    Parameters<
+      PonderApp<
+        typeof import("./ponder.config.ts").default,
+        typeof import("./ponder.schema.ts").default
+      >["on"]
+    >[1]
+  >[0]["context"];
 }
