0614a22068479427598e31bfae065e5317e084d8
Kyle Scott <kyscott@umich.edu>
2023-11-26 17:08:13 -0500

    update examples

--- benchmarks/ponder/ponder.schema.ts
+++ benchmarks/ponder/ponder.schema.ts
@@ -4,32 +4,44 @@ export default p.createSchema({
   Account: p.createTable({
     id: p.string(),
     balance: p.bigint(),
-    approvals: p.virtual("Approval.ownerId"),
     isOwner: p.boolean(),
 
-    approvalOwnerEvents: p.virtual("ApprovalEvent.ownerId"),
-    approvalSpenderEvents: p.virtual("ApprovalEvent.spenderId"),
-    transferFromEvents: p.virtual("TransferEvent.fromId"),
-    transferToEvents: p.virtual("TransferEvent.toId"),
+    approvals: p.many("Approval.ownerId"),
+    approvalOwnerEvents: p.many("ApprovalEvent.ownerId"),
+    approvalSpenderEvents: p.many("ApprovalEvent.spenderId"),
+    transferFromEvents: p.many("TransferEvent.fromId"),
+    transferToEvents: p.many("TransferEvent.toId"),
   }),
   Approval: p.createTable({
     id: p.bytes(),
     amount: p.bigint(),
+
     ownerId: p.string().references("Account.id"),
-    spender: p.string(),
+    spenderId: p.string().references("Account.id"),
+
+    owner: p.one("ownerId"),
+    spender: p.one("spenderId"),
   }),
   TransferEvent: p.createTable({
     id: p.string(),
     amount: p.bigint(),
+    timestamp: p.int(),
+
     fromId: p.string().references("Account.id"),
     toId: p.string().references("Account.id"),
-    timestamp: p.int(),
+
+    from: p.one("fromId"),
+    to: p.one("toId"),
   }),
   ApprovalEvent: p.createTable({
     id: p.string(),
     amount: p.bigint(),
+    timestamp: p.int(),
+
     ownerId: p.string().references("Account.id"),
     spenderId: p.string().references("Account.id"),
-    timestamp: p.int(),
+
+    owner: p.one("ownerId"),
+    spender: p.one("spenderId"),
   }),
 });
