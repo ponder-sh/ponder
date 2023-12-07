e7e921be26c07b9223e814764140df56c27c861d
bankisan <hello@bankisan.xyz>
2023-12-07 19:10:12 +0000

    use correct create schema

--- examples/with-foundry/ponder/ponder.schema.ts
+++ examples/with-foundry/ponder/ponder.schema.ts
@@ -1,9 +1,9 @@
-import { p } from "@ponder/core";
+import { createSchema } from "@ponder/core";
 
-export default p.createSchema({
+export default createSchema((p) => ({
   Counter: p.createTable({
     id: p.int(),
     value: p.int(),
     block: p.int(),
   }),
-});
+}));
