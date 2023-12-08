5ae0f55fa903bbc2e436f761100cde19b956e7b6
Kyle Scott <kyscott@umich.edu>
2023-12-08 16:07:51 -0500

    change validation pipeline

---
+++ packages/core/src/config/validate.ts
@@ -0,0 +1,41 @@
+import type { Config } from "./config.js";
+
+export const validateConfig = ({ config }: { config: Config }): void => {
+  Object.values(config.contracts).forEach((contract) => {
+    if (typeof contract.network === "string") {
+      // shortcut
+      const network = config.networks[contract.network];
+      if (!network)
+        throw Error('Contract network does not match a network in "networks"');
+
+      // Validate the address / factory data
+      const resolvedFactory = "factory" in contract && contract.factory;
+      const resolvedAddress = "address" in contract && contract.address;
+      if (resolvedFactory && resolvedAddress)
+        throw Error("Factory and address cannot both be defined");
+    } else {
+      Object.entries(contract.network).forEach(
+        ([networkName, contractOverride]) => {
+          if (!contractOverride) return;
+
+          // Make sure network matches an element in config.networks
+          const network = config.networks[networkName];
+          if (!network)
+            throw Error(
+              'Contract network does not match a network in "networks"',
+            );
+
+          // Validate the address / factory data
+          const resolvedFactory =
+            ("factory" in contractOverride && contractOverride.factory) ||
+            ("factory" in contract && contract.factory);
+          const resolvedAddress =
+            ("address" in contractOverride && contractOverride.address) ||
+            ("address" in contract && contract.address);
+          if (resolvedFactory && resolvedAddress)
+            throw Error("Factory and address cannot both be defined");
+        },
+      );
+    }
+  });
+};
