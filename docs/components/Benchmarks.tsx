9cd85a9569c96556738a4abf3a546179401f6d4c
typedarray <90073088+0xOlias@users.noreply.github.com>
2023-12-14 10:57:22 -0500

    tweaks

--- docs/components/Benchmarks.tsx
+++ docs/components/Benchmarks.tsx
@@ -29,15 +29,15 @@ export function Benchmarks({ flat = false }: { flat?: boolean }) {
             </div>
           </div>
 
-          <div className="grid grid-cols-5">
+          <div className="grid grid-cols-4 md:grid-cols-5">
             <div className="col-span-1 py-2 pl-4 md:pl-8 text-sm border-neutral-200 dark:border-neutral-50/20 border-b text-neutral-500"></div>
             <div className="col-span-1 py-2 pl-3 text-sm border-neutral-200 dark:border-neutral-50/20 border-b">
-              Sync (No Cache)
+              Sync (Cold)
             </div>
             <div className="col-span-1 py-2 pl-3 text-sm border-neutral-200 dark:border-neutral-50/20 border-b">
               Sync (Cache)
             </div>
-            <div className="col-span-1 py-2 pl-3 text-sm border-neutral-200 dark:border-neutral-50/20 border-b">
+            <div className="col-span-1 py-2 pl-3 text-sm border-neutral-200 dark:border-neutral-50/20 border-b hidden md:block">
               Database Size
             </div>
             <div className="col-span-1 py-2 pl-3 text-sm border-neutral-200 dark:border-neutral-50/20 border-b">
@@ -53,7 +53,7 @@ export function Benchmarks({ flat = false }: { flat?: boolean }) {
             <div className="col-span-1 py-2 pl-3 border-neutral-200 dark:border-neutral-50/20 border-b border-r text-ponder bg-ponder/10">
               5s
             </div>
-            <div className="col-span-1 py-2 pl-3 border-neutral-200 dark:border-neutral-50/20 border-b border-r text-ponder bg-ponder/10">
+            <div className="col-span-1 py-2 pl-3 border-neutral-200 dark:border-neutral-50/20 border-b border-r text-ponder bg-ponder/10 hidden md:block">
               31 MB
             </div>
             <div className="col-span-1 py-2 pl-3 border-neutral-200 dark:border-neutral-50/20 border-b text-ponder bg-ponder/10">
@@ -69,7 +69,7 @@ export function Benchmarks({ flat = false }: { flat?: boolean }) {
             <div className="col-span-1 py-2 pl-3 pb-3 lg:pb-6 border-neutral-200 dark:border-neutral-50/20 border-r max-lg:border-b">
               1m 15s
             </div>
-            <div className="col-span-1 py-2 pl-3 pb-3 lg:pb-6 border-neutral-200 dark:border-neutral-50/20 border-r max-lg:border-b">
+            <div className="col-span-1 py-2 pl-3 pb-3 lg:pb-6 border-neutral-200 dark:border-neutral-50/20 border-r max-lg:border-b hidden md:block">
               1.1 GB
             </div>
             <div className="col-span-1 py-2 pl-3 pb-3 lg:pb-6 border-neutral-200 dark:border-neutral-50/20 max-lg:border-b">
@@ -80,7 +80,7 @@ export function Benchmarks({ flat = false }: { flat?: boolean }) {
 
         <div className="flex flex-col lg:max-w-[22ch] p-4 md:p-6">
           <p className="text-sm mb-4 text-neutral-700 dark:text-neutral-200">
-            Benchmarks index the Rocket Pool ERC20 token contract on mainnet
+            Results of indexing the Rocket Pool ERC20 token contract on mainnet
             from block 18,600,000 to 18,718,056 (latest) on an M1 MacBook Pro (8
             core, 16GB RAM) against an Alchemy node on the Growth plan using a
             950MB/s network connection.
@@ -90,7 +90,7 @@ export function Benchmarks({ flat = false }: { flat?: boolean }) {
             target="_blank"
             className="text-sm text-ponder hover:text-ponder-50/90"
           >
-            Try it yourself →
+            Run it yourself →
           </Link>
         </div>
       </div>
