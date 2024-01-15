8ef0b111687b3b5556292c32ad89c805a3cd6a5f
Kyle Scott <kyscott@umich.edu>
2024-01-15 13:08:45 -0500

    basic typescript docs

--- docs/pages/docs/_meta.ts
+++ docs/pages/docs/_meta.ts
@@ -2,6 +2,7 @@ export default {
   "getting-started": "Getting started",
   "migration-guide": "Migration guide",
   "compared-to-subgraphs": "Compared to subgraphs",
+  typescript: "TypeScript",
   guides: "Guides",
   "api-reference": "API reference",
   advanced: "Advanced",
