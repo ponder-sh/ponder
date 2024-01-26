015a9005a751e78363eeadc818d8dbbdb1322146
Kyle Scott <kyscott@umich.edu>
2024-01-26 12:03:05 -0500

    change server filtering of p.hex to be numeric

--- packages/core/src/server/graphql/plural.ts
+++ packages/core/src/server/graphql/plural.ts
@@ -95,7 +95,7 @@ export const buildPluralField = ({
         };
       });
 
-      if (["int", "bigint", "float"].includes(column.type)) {
+      if (["int", "bigint", "float", "hex"].includes(column.type)) {
         operators.numeric.forEach((suffix) => {
           filterFields[`${columnName}${suffix}`] = {
             type: type,
@@ -103,7 +103,7 @@ export const buildPluralField = ({
         });
       }
 
-      if (["string", "hex"].includes(column.type)) {
+      if ("string" === column.type) {
         operators.string.forEach((suffix) => {
           filterFields[`${columnName}${suffix}`] = {
             type: type,
