70e700d1d189ecf9153fdd6910d775615ccc7202
Kyle Scott <kyscott@umich.edu>
2024-01-30 15:41:41 -0500

    fix benchmarks

--- benchmarks/apps/ponder-reth/src/index.ts
+++ benchmarks/apps/ponder-reth/src/index.ts
@@ -10,83 +10,69 @@ declare const ponder: import("@ponder/core").PonderApp<
 ponder.on("RocketTokenRETH:Transfer", async ({ event, context }) => {
   const { Account, TransferEvent } = context.db;
 
-  // Create an Account for the sender, or update the balance if it already exists.
-  const sender = await Account.findUnique({ id: event.args.from });
-  if (!sender) {
-    await Account.create({
+  await Promise.all([
+    Account.upsert({
       id: event.args.from,
-      data: {
-        balance: BigInt(0),
+      create: {
+        balance: 0n,
         isOwner: false,
       },
-    });
-  } else {
-    await Account.update({
-      id: event.args.from,
-      data: {
-        balance: sender.balance - event.args.value,
-      },
-    });
-  }
+      update: ({ current }) => ({
+        balance: current.balance - event.args.value,
+      }),
+    }),
 
-  // Create an Account for the recipient, or update the balance if it already exists.
-  const recipient = await Account.findUnique({ id: event.args.to });
-  if (!recipient) {
-    await Account.create({
-      id: event.args.to,
-      data: {
-        balance: event.args.value,
-        isOwner: false,
-      },
-    });
-  } else {
-    await Account.update({
-      id: event.args.to,
-      data: {
-        balance: recipient.balance + event.args.value,
-      },
-    });
-  }
+    event.args.from !== event.args.to
+      ? Account.upsert({
+          id: event.args.to,
+          create: {
+            balance: 0n,
+            isOwner: false,
+          },
+          update: ({ current }) => ({
+            balance: current.balance + event.args.value,
+          }),
+        })
+      : undefined,
 
-  // Create a TransferEvent.
-  await TransferEvent.create({
-    id: event.log.id,
-    data: {
-      fromId: event.args.from,
-      toId: event.args.to,
-      amount: event.args.value,
-      timestamp: Number(event.block.timestamp),
-    },
-  });
+    TransferEvent.create({
+      id: event.log.id,
+      data: {
+        fromId: event.args.from,
+        toId: event.args.to,
+        amount: event.args.value,
+        timestamp: Number(event.block.timestamp),
+      },
+    }),
+  ]);
 });
 
 ponder.on("RocketTokenRETH:Approval", async ({ event, context }) => {
   const { Approval, ApprovalEvent } = context.db;
 
-  const approvalId =
-    `${event.args.owner}-${event.args.spender}` as `0x${string}`;
+  const approvalId = `${event.args.owner}-${event.args.spender}`;
 
-  // Create or update the Approval.
-  await Approval.upsert({
-    id: approvalId,
-    create: {
-      ownerId: event.args.owner,
-      spenderId: event.args.spender,
-      amount: event.args.value,
-    },
-    update: {
-      amount: event.args.value,
-    },
-  });
+  await Promise.all([
+    Approval.upsert({
+      id: approvalId,
+      create: {
+        ownerId: event.args.owner,
+        spenderId: event.args.spender,
+        amount: event.args.value,
+      },
+      update: {
+        amount: event.args.value,
+      },
+    }),
 
-  // Create a TransferEvent.
-  await ApprovalEvent.create({
-    id: event.log.id,
-    data: {
-      ownerId: event.args.owner,
-      spenderId: event.args.spender,
-      amount: event.args.value,
-      timestamp: Number(event.block.timestamp),
-    },
-  });
+    await ApprovalEvent.create({
+      id: event.log.id,
+      data: {
+        ownerId: event.args.owner,
+        spenderId: event.args.spender,
+        amount: event.args.value,
+        timestamp: Number(event.block.timestamp),
+      },
+    }),
+  ]);
 });
