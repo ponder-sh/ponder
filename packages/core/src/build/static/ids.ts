7bc7f38efd1599da86d569ad9b7271e4edc92b60
Kyle Scott <kyscott@umich.edu>
2024-02-08 15:31:53 -0500

    deterministically resolve interdependencies between tables and functions

--- packages/core/src/build/static/ids.ts
+++ packages/core/src/build/static/ids.ts
@@ -1,45 +1,46 @@
 import crypto from "crypto";
 import type { Source } from "@/config/sources.js";
 import type { Schema } from "@/schema/types.js";
-import { isEnumColumn, isManyColumn, isOneColumn } from "@/schema/utils.js";
+import { isBaseColumn, isEnumColumn } from "@/schema/utils.js";
 import { dedupe } from "@/utils/dedupe.js";
 import type { TableAccess } from "./parseAst.js";
 
 export type FunctionIds = { [func: string]: string };
 export type TableIds = { [table: string]: string };
 
+type FunctionInputs = {
+  [key: string]: {
+    astHash: string;
+    sources: object[];
+  };
+};
+type TableInputs = {
+  [key: string]: {
+    schema: object;
+  };
+};
+
 export const getIds = ({
   sources,
   tableAccess,
   schema,
 }: { sources: Source[]; schema: Schema; tableAccess: TableAccess }) => {
-  const functionInputs: {
-    [key: string]: {
-      astHash: string;
-      sources: Source[];
-    };
-  } = {};
-  const tableInputs: {
-    [key: string]: {
-      schema: Schema["tables"][string];
-    };
-  } = {};
+  const functionInputs: FunctionInputs = {};
+  const tableInputs: TableInputs = {};
 
   // Dedupe indexing function keys
   const seenKeys: Set<string> = new Set();
-  const tableWrites = tableAccess
-    .filter((t) => t.access === "write")
-    .filter((t) => {
-      if (seenKeys.has(t.indexingFunctionKey)) {
-        return false;
-      } else {
-        seenKeys.add(t.indexingFunctionKey);
-        return true;
-      }
-    });
+  const _keys = tableAccess.filter((t) => {
+    if (seenKeys.has(t.indexingFunctionKey)) {
+      return false;
+    } else {
+      seenKeys.add(t.indexingFunctionKey);
+      return true;
+    }
+  });
 
-  // Build functions
-  for (const { hash, indexingFunctionKey } of tableWrites) {
+  // Build function inputs
+  for (const { hash, indexingFunctionKey } of _keys) {
     const contractName = indexingFunctionKey.split(":")[0]!;
     const tableSources = sources.filter((s) => s.contractName === contractName);
 
@@ -49,7 +50,7 @@ export const getIds = ({
     };
   }
 
-  // Build tables
+  // Build table inputs
   for (const tableName of Object.keys(schema.tables)) {
     tableInputs[tableName] = {
       schema: resolveSchema(schema.tables[tableName], schema.enums),
@@ -60,68 +61,135 @@ export const getIds = ({
   const tableIds: TableIds = {};
 
   // Build function IDs
-  for (const [indexingFunctionKey, { astHash, sources }] of Object.entries(
-    functionInputs,
-  )) {
-    // Find tables written to and read from?
-    const tableNames = tableAccess
-      .filter(
-        (t) =>
-          t.access === "write" && t.indexingFunctionKey === indexingFunctionKey,
-      )
-      .map((t) => t.table);
-
-    const tables = dedupe(tableNames).map(
-      (tableName) => tableInputs[tableName],
-    );
+  for (const indexingFunctionKey of Object.keys(functionInputs)) {
+    const { functions, tables } = resolveDependencies({
+      name: indexingFunctionKey,
+      type: "function",
+      tableAccess,
+    });
 
     functionIds[indexingFunctionKey] = crypto
       .createHash("sha256")
       .update(
         JSON.stringify({
-          indexingFunctionKey,
-          astHash,
-          sources,
+          name: indexingFunctionKey,
+          type: "function",
+
+          functions,
           tables,
+          functionInputs: functions.map((f) => functionInputs[f]),
+          tableInputs: tables.map((t) => tableInputs[t]),
         }),
       )
       .digest("hex");
   }
 
   // Build table IDs
-  for (const [tableName, { schema }] of Object.entries(tableInputs)) {
-    const functionKeys = tableAccess
-      .filter((t) => t.access === "write" && t.table === tableName)
-      .map((t) => t.indexingFunctionKey);
-
-    const functions = dedupe(functionKeys).map((key) => functionInputs[key]);
+  for (const tableName of Object.keys(tableInputs)) {
+    const { functions, tables } = resolveDependencies({
+      name: tableName,
+      type: "table",
+      tableAccess,
+    });
 
     tableIds[tableName] = crypto
       .createHash("sha256")
       .update(
         JSON.stringify({
-          tableName,
-          schema,
+          name: tableName,
+          type: "table",
+
           functions,
+          tables,
+          functionInputs: functions.map((f) => functionInputs[f]),
+          tableInputs: tables.map((t) => tableInputs[t]),
         }),
       )
       .digest("hex");
   }
 
+  console.log({
+    tableIds,
+    functionIds,
+  });
+
   return { tableIds, functionIds };
+};
+
+/**
+ * Deterministically resolve all dependencies of a given function or table.
+ */
+const resolveDependencies = ({
+  name,
+  type,
+  tableAccess,
+}: {
+  name: string;
+  type: "function" | "table";
+  tableAccess: TableAccess;
+}): { functions: string[]; tables: string[] } => {
+  const functions: string[] = [];
+  const tables: string[] = [];
+
+  const innerResolve = (name: string, type: "function" | "table") => {
+    if (type === "function" && functions.includes(name)) return;
+    if (type === "table" && tables.includes(name)) return;
+
+    // Tables are dependent on all the functions that write to them
+    if (type === "table") {
+      tables.push(name);
+
+      const functionNames = tableAccess
+        .filter((t) => t.access === "write" && t.table === name)
+        .map((t) => t.indexingFunctionKey);
+
+      for (const functionName of dedupe(functionNames)) {
+        innerResolve(functionName, "function");
+      }
+    }
+    // Functions are dependent on all the tables that they read from and write to
+    else {
+      functions.push(name);
+
+      const tableNames = tableAccess
+        .filter((t) => t.indexingFunctionKey === name)
+        .map((t) => t.table);
+
+      for (const tableName of dedupe(tableNames)) {
+        innerResolve(tableName, "table");
+      }
+    }
+  };
+
+  innerResolve(name, type);
+
+  return { functions, tables };
 };
 
 /** Resolve the enum columns of a table. Remove "one" and "many" column. */
 const resolveSchema = (
   table: Schema["tables"][string],
   enums: Schema["enums"],
-) => {
+): Object => {
+  let resolved: object = {};
+
   for (const [columnName, column] of Object.entries(table)) {
     if (isEnumColumn(column)) {
       const resolvedEnum = enums[column.type];
-      Object.defineProperty(table[columnName], "resolved", resolvedEnum);
-    } else if (isOneColumn(column) || isManyColumn(column)) {
-      delete table[columnName];
+      resolved = {
+        ...resolved,
+        [columnName]: {
+          ...column,
+          resolvedEnum,
+        },
+      };
+    } else if (isBaseColumn(column)) {
+      resolved = {
+        ...resolved,
+        [columnName]: {
+          ...column,
+        },
+      };
     }
   }
   return table;
