a62b2591696772982ebe24554ae352d0d9ce813d
typedarray <90073088+0xOlias@users.noreply.github.com>
2024-02-28 12:09:07 -0500

    wip docs

--- docs/pages/docs/guides/_meta.ts
+++ docs/pages/docs/guides/_meta.ts
@@ -6,5 +6,6 @@ export default {
   "create-update-records": "Create & update records",
   "read-contract-data": "Read contract data",
   "query-the-graphql-api": "Query the GraphQL API",
+  "query-the-database": "Query the database",
   production: "Deploy to production",
 };
