480e377fd9b2006867dcb18ba32be658754604e5
Kyle Scott <kyscott@umich.edu>
2024-03-05 12:02:47 -0500

    remove test-app

--- packages/create-ponder/src/_test/test-app/ponder.schema.ts
+++
@@ -1,8 +0,0 @@
-import { createSchema } from "@ponder/core";
-
-export default createSchema((p) => ({
-  Example: p.createTable({
-    id: p.string(),
-    name: p.string().optional(),
-  }),
-}));
--- packages/create-ponder/src/_test/test-app/src/index.ts
+++
@@ -1,1 +0,0 @@
-import { ponder } from "@/generated";
