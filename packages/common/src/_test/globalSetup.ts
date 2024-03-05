e19a147db5ab969bb08b8491922e7523303ff2d3
Kyle Scott <kyscott@umich.edu>
2024-03-04 22:09:20 -0500

    test actions

--- packages/common/src/_test/globalSetup.ts
+++
@@ -1,5 +0,0 @@
-import dotenv from "dotenv";
-
-export default async function () {
-  dotenv.config({ path: ".env.local" });
-}
--- packages/common/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globalSetup: ["src/_test/globalSetup.ts"],
-  },
-});
