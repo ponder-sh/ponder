25d40566f32ff5f1fd52620552ee87c1c47cf3fd
Jay Miller <3744812+jaylmiller@users.noreply.github.com>
2024-03-22 16:40:49 -0400

    modify sqlite migration

--- packages/core/src/database/sqlite/migrations.ts
+++ packages/core/src/database/sqlite/migrations.ts
@@ -31,30 +31,55 @@ const migrations: Record<string, Migration> = {
   },
   "2024_03_22_new_checkpoint_encoding": {
     async up(db: Kysely<any>) {
-      await db.schema.dropTable("function_metadata").execute();
-      await db.schema.dropTable("table_metadata").execute();
+      await alterColumnType({
+        db,
+        table: "function_metadata",
+        column: "from_checkpoint",
+        newType: "varchar(75)",
+      });
 
-      await db.schema
-        .createTable("function_metadata")
-        .addColumn("function_id", "text", (col) => col.notNull().primaryKey())
-        .addColumn("function_name", "text", (col) => col.notNull())
-        .addColumn("hash_version", "integer", (col) => col.notNull())
-        .addColumn("from_checkpoint", "varchar(75)")
-        .addColumn("to_checkpoint", "varchar(75)", (col) => col.notNull())
-        .addColumn("event_count", "integer", (col) => col.notNull())
-        .execute();
+      await alterColumnType({
+        db,
+        table: "function_metadata",
+        column: "to_checkpoint",
+        newType: "varchar(75)",
+      });
 
-      await db.schema
-        .createTable("table_metadata")
-        .addColumn("table_id", "text", (col) => col.notNull().primaryKey())
-        .addColumn("table_name", "text", (col) => col.notNull())
-        .addColumn("hash_version", "integer", (col) => col.notNull())
-        .addColumn("to_checkpoint", "varchar(75)", (col) => col.notNull())
-        .addColumn("schema", "jsonb", (col) => col.notNull())
-        .execute();
+      await alterColumnType({
+        db,
+        table: "table_metadata",
+        column: "to_checkpoint",
+        newType: "varchar(75)",
+      });
     },
   },
 };
+
+async function alterColumnType({
+  db,
+  table,
+  column,
+  newType,
+}: {
+  db: Kysely<any>;
+  table: string;
+  column: string;
+  newType: Parameters<
+    ReturnType<Kysely<any>["schema"]["alterTable"]>["addColumn"]
+  >[1];
+}) {
+  const tempName = `temp_${column}`;
+
+  await db.schema.alterTable(table).addColumn(tempName, newType).execute();
+  await db
+    .updateTable(table)
+    .set((eb: any) => ({
+      [tempName]: eb.selectFrom(table).select(column),
+    }))
+    .execute();
+  await db.schema.alterTable(table).dropColumn(column).execute();
+  await db.schema.alterTable(table).renameColumn(tempName, column).execute();
+}
 
 class StaticMigrationProvider implements MigrationProvider {
   async getMigrations() {
