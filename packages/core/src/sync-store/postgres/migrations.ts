6d8103faed1d9ad48946d0c744d85b7b790dad73
Jay Miller <3744812+jaylmiller@users.noreply.github.com>
2024-04-11 15:25:18 -0400

    Revert "move column DDL to separaate transaction"
    This reverts commit 4e491ebc4d3f35e785320b966e30b44a0df7ceb6.

--- packages/core/src/sync-store/postgres/migrations.ts
+++ packages/core/src/sync-store/postgres/migrations.ts
@@ -456,16 +456,12 @@ const migrations: Record<string, Migration> = {
         .execute();
     },
   },
-  "2024_03_20_0_add_checkpoint_column_to_logs_table": {
+  "2024_03_20_0_checkpoint_in_logs_table": {
     async up(db: Kysely<any>) {
       await db.schema
         .alterTable("logs")
         .addColumn("checkpoint", "varchar(75)")
         .execute();
-    },
-  },
-  "2024_03_20_1_set_checkpoint_in_logs_table": {
-    async up(db: Kysely<any>) {
       await db.executeQuery(
         sql`
           WITH checkpoint_vals AS (
@@ -519,10 +515,7 @@ const migrations: Record<string, Migration> = {
           );
         }
       }
-    },
-  },
-  "2024_03_20_2_index_on_logs_checkpoint": {
-    async up(db: Kysely<any>) {
+
       await db.schema
         .createIndex("logs_checkpoint_index")
         .on("logs")
