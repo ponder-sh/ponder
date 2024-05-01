bd1c0f5e98ca24662e443f7331be4e23b85c09ec
typedarray <90073088+0xOlias@users.noreply.github.com>
2024-05-01 12:48:52 -0400

    fix server uptime, process.uptime not reliable

--- packages/core/src/server/service.ts
+++ packages/core/src/server/service.ts
@@ -37,6 +37,7 @@ export async function createServer({
 
   let port = common.options.port;
   let isHealthy = false;
+  const uptimeClock = startClock();
 
   const metricsMiddleware = createMiddleware(async (c, next) => {
     const commonLabels = { method: c.req.method, path: c.req.path };
@@ -92,7 +93,7 @@ export async function createServer({
       }
 
       const max = common.options.maxHealthcheckDuration;
-      const elapsed = Math.floor(process.uptime());
+      const elapsed = Math.floor(uptimeClock() / 1000);
 
       if (elapsed > max) {
         common.logger.warn({
