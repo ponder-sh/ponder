a02756b0cb18dc8b0c485e167ececbaf9e5a542b
typedarray <90073088+0xOlias@users.noreply.github.com>
2024-05-13 16:46:25 -0400

    docs

--- docs/pages/docs/indexing/_meta.ts
+++ docs/pages/docs/indexing/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   "create-update-records": "Create & update records",
   "read-contract-data": "Read contract data",
+  "call-traces": "Call traces",
   "time-series": "Time-series data",
   "caching-and-concurrency": "Caching & concurrency",
 };
