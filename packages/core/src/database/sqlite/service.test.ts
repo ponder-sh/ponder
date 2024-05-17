7331b098337ab2717bba467797622703420493d4
Kyle Scott <kyscott@umich.edu>
2024-05-16 21:01:17 -0400

    rm .only

--- packages/core/src/database/sqlite/service.test.ts
+++ packages/core/src/database/sqlite/service.test.ts
@@ -336,7 +336,7 @@ describe.skipIf(shouldSkip)("sqlite database", () => {
     await databaseTwo.kill();
   });
 
-  test.only("setup throws if there is a table name collision", async (context) => {
+  test("setup throws if there is a table name collision", async (context) => {
     if (context.databaseConfig.kind !== "sqlite") return;
     const database = new SqliteDatabaseService({
       common: context.common,
