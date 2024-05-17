32d9480b43a8e72b0185f4e34a242f7fb0db4fcd
typedarray <90073088+0xOlias@users.noreply.github.com>
2024-05-17 11:58:14 -0400

    wip docs tweaks (#897)

--- docs/pages/docs/_meta.ts
+++ docs/pages/docs/_meta.ts
@@ -1,14 +1,62 @@
 export default {
-  "getting-started": "Getting started",
-  "contracts-and-networks": "Contracts & networks",
-  "block-events": "Block events",
-  schema: "Design your schema",
-  "indexing": "Write indexing functions",
-  "query": "Query the database",
-  production: "Deploy to production",
-  "api-reference": "API reference",
-  utilities: "Utilities",
-  advanced: "Advanced",
+  "why-ponder": {
+    title: "Why Ponder",
+    display: "hidden",
+  },
+
+  "-- Get started": {
+    type: "separator",
+    title: "Get started",
+  },
+  "getting-started": { display: "children", title: "Get started" },
   "migration-guide": "Migration guide",
-  "why-ponder": "Why Ponder",
+
+  "-- Config": {
+    type: "separator",
+    title: "Config",
+  },
+  "contracts-and-networks": "Contracts & networks",
+  "block-intervals": "Block intervals",
+
+  "-- Schema": {
+    type: "separator",
+    title: "Schema",
+  },
+  schema: "Design your schema",
+
+  "-- Indexing": {
+    type: "separator",
+    title: "Indexing",
+  },
+  indexing: { display: "children", title: "Indexing" },
+
+  "-- Query the database": {
+    type: "separator",
+    title: "Query the database",
+  },
+  query: { display: "children", title: "Query the database" },
+
+  "-- Production": {
+    type: "separator",
+    title: "Production",
+  },
+  production: { display: "children", title: "Production" },
+
+  "-- Utilities": {
+    type: "separator",
+    title: "Utilities",
+  },
+  utilities: { display: "children", title: "Utilities" },
+
+  "-- API reference": {
+    type: "separator",
+    title: "API reference",
+  },
+  "api-reference": { display: "children", title: "API reference" },
+
+  "-- Advanced": {
+    type: "separator",
+    title: "Advanced",
+  },
+  advanced: { display: "children", title: "Advanced" },
 };
