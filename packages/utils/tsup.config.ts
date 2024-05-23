6dedd0e8e0afccf9adc7f6992f8b8414fa32afdd
omar <omaraziz.dev@proton.me>
2024-05-23 11:50:51 -0700

    feat: tree shake utils pkg

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   name: "@ponder/utils",
-  entry: ["src/index.ts"],
+  entry: {
+    index: "src/index.ts",
+    "merge-abis": "src/mergeAbis.ts",
+    "rate-limit": "src/rateLimit.ts",
+    "load-balance": "src/loadBalance.ts",
+    "retry-helper": "src/getLogsRetryHelper.ts",
+  },
   outDir: "dist",
   format: ["esm", "cjs"],
   sourcemap: true,
