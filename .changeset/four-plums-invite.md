387b229df612d7f1e83aa5aa3d838bffb81a6564
Kyle Scott <kyscott@umich.edu>
2024-06-18 17:54:38 -0400

    chore: changeset

---
+++ .changeset/four-plums-invite.md
@@ -0,0 +1,5 @@
+---
+"@ponder/core": patch
+---
+
+Fixed "too many SQL variables" bug for tables with >30 columns.
--- packages/core/src/common/options.ts
+++ packages/core/src/common/options.ts
@@ -106,7 +106,8 @@ export const buildOptions = ({ cliOptions }: { cliOptions: CliOptions }) => {
 
     databaseHeartbeatInterval: 10 * 1000,
     databaseHeartbeatTimeout: 25 * 1000,
-    databaseMaxQueryParameters: 30_000,
+    // Half of the max query parameters for SQLite
+    databaseMaxQueryParameters: 16_000,
     databaseMaxRowLimit: 1_000,
 
     // os.freemem() / 4, bucketed closest to 64, 128, 256, 512, 1024, 2048 mB
