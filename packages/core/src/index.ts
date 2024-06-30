91bf27dce21097806dbb03da5df9b021c05668aa
R <16455953+robiiinos@users.noreply.github.com>
2024-06-30 16:39:04 +0800

    chore: Export TransactionReceipt type

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,6 +1,6 @@
 export { createConfig } from "@/config/config.js";
 export { createSchema } from "@/schema/schema.js";
-export type { Block, Log, Transaction } from "@/types/eth.js";
+export type { Block, Log, Transaction, TransactionReceipt } from "@/types/eth.js";
 export type { Virtual } from "@/types/virtual.js";
 export {
   type MergeAbis,
