8672adff0a52492ca80214fdf4f0044ff8bd4ba6
Kyle Scott <kyscott@umich.edu>
2024-07-16 13:06:20 -0400

    fix graphql middleware

--- packages/core/src/graphql/index.ts
+++ packages/core/src/graphql/index.ts
@@ -35,40 +35,45 @@ export const graphql = (
     maxOperationDepth: 100,
     maxOperationAliases: 30,
   },
-) =>
-  createMiddleware(async (c) => {
-    const readonlyStore = c.get("readonlyStore");
-    const metadataStore = c.get("metadataStore");
-    const schema = c.get("schema");
-    const graphqlSchema = buildGraphQLSchema(schema);
+) => {
+  let yoga: any | undefined = undefined;
 
+  return createMiddleware(async (c) => {
     if (c.req.method === "GET") {
       return c.html(graphiQLHtml(c.req.path));
     }
 
-    const yoga = createYoga({
-      schema: graphqlSchema,
-      context: () => {
-        const getLoader = buildLoaderCache({ store: readonlyStore });
-        return { readonlyStore, metadataStore, getLoader };
-      },
-      graphqlEndpoint: c.req.path,
-      maskedErrors: process.env.NODE_ENV === "production",
-      logging: false,
-      graphiql: false,
-      parserAndValidationCache: false,
-      plugins: [
-        maxTokensPlugin({ n: maxOperationTokens }),
-        maxDepthPlugin({
-          n: maxOperationDepth,
-          ignoreIntrospection: false,
-        }),
-        maxAliasesPlugin({
-          n: maxOperationAliases,
-          allowList: [],
-        }),
-      ],
-    });
+    if (yoga === undefined) {
+      const readonlyStore = c.get("readonlyStore");
+      const metadataStore = c.get("metadataStore");
+      const schema = c.get("schema");
+      const graphqlSchema = buildGraphQLSchema(schema);
+
+      yoga = createYoga({
+        schema: graphqlSchema,
+        context: () => {
+          const getLoader = buildLoaderCache({ store: readonlyStore });
+          return { readonlyStore, metadataStore, getLoader };
+        },
+        graphqlEndpoint: c.req.path,
+        maskedErrors: process.env.NODE_ENV === "production",
+        logging: false,
+        graphiql: false,
+        parserAndValidationCache: false,
+        plugins: [
+          maxTokensPlugin({ n: maxOperationTokens }),
+          maxDepthPlugin({
+            n: maxOperationDepth,
+            ignoreIntrospection: false,
+          }),
+          maxAliasesPlugin({
+            n: maxOperationAliases,
+            allowList: [],
+          }),
+        ],
+      });
+    }
 
     return yoga.handle(c.req.raw);
   });
+};
