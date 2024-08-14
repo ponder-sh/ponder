ca9f148aa300c2df74e010fd57b2fbc6a997c750
typedarray <90073088+0xOlias@users.noreply.github.com>
2024-08-14 16:57:54 -0400

    Update packages/core/src/utils/format.ts
    Co-authored-by: awkweb <tom@meagher.co>

--- packages/core/src/utils/format.ts
+++ packages/core/src/utils/format.ts
@@ -9,7 +9,7 @@ export const formatEta = (ms: number) => {
 
   const hstr = h > 0 ? `${h}h ` : "";
   const mstr = m > 0 || h > 0 ? `${m < 10 && h > 0 ? "0" : ""}${m}m ` : "";
-  const sstr = s > 0 || m > 0 ? `${s < 10 ? "0" : ""}${s}s` : "";
+  const sstr = s > 0 || m > 0 ? `${s < 10 && m > 0 ? "0" : ""}${s}s` : "";
 
   return `${hstr}${mstr}${sstr}`;
 };
