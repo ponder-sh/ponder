d939bbf6c5ef24c2396a4535101771c56a3554f5
kyscott18 <43524469+kyscott18@users.noreply.github.com>
2024-09-05 10:22:58 -0400

    Remove `maxHistoricalTaskConcurrency` (#1073)
    * remove deprecated maxHistoricalTaskConcurrency config
    
    * more
    
    * chore: changeset

--- packages/core/src/config/networks.ts
+++ packages/core/src/config/networks.ts
@@ -8,7 +8,6 @@ export type Network = {
   transport: ReturnType<Transport>;
   pollingInterval: number;
   maxRequestsPerSecond: number;
-  maxHistoricalTaskConcurrency: number;
   defaultMaxBlockRange: number;
   finalityBlockCount: number;
   disableCache: boolean;
