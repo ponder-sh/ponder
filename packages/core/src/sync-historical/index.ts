0f6213f7d907ea0de27deebb994823d20f379349
kyscott18 <43524469+kyscott18@users.noreply.github.com>
2024-09-06 12:01:28 -0400

    fix `ponder_realtime_is_connected` metric (#1077)
    * fix ponder_realtime_is_connected metric
    
    * chore: changeset

--- packages/core/src/sync-historical/index.ts
+++ packages/core/src/sync-historical/index.ts
@@ -38,7 +38,7 @@ export type HistoricalSync = {
   latestBlock: SyncBlock | undefined;
   /** Extract raw data for `interval`. */
   sync(interval: Interval): Promise<void>;
-  initializeMetrics(finalizedBlock: SyncBlock, showStart: boolean): void;
+  initializeMetrics(finalizedBlock: SyncBlock, isInitialCall: boolean): void;
   kill(): void;
 };
 
@@ -525,8 +525,14 @@ export const createHistoricalSync = async (
       blockCache.clear();
       transactionsCache.clear();
     },
-    initializeMetrics(finalizedBlock, showStart) {
-      args.common.metrics.ponder_historical_start_timestamp.set(Date.now());
+    initializeMetrics(finalizedBlock, isInitialCall) {
+      if (isInitialCall) {
+        args.common.metrics.ponder_historical_start_timestamp.set(Date.now());
+        args.common.metrics.ponder_realtime_is_connected.set(
+          { network: args.network.name },
+          0,
+        );
+      }
 
       for (const source of args.sources) {
         const label = {
@@ -538,7 +544,7 @@ export const createHistoricalSync = async (
         if (source.filter.fromBlock > hexToNumber(finalizedBlock.number)) {
           args.common.metrics.ponder_historical_total_blocks.set(label, 0);
 
-          if (showStart) {
+          if (isInitialCall) {
             args.common.logger.warn({
               service: "historical",
               msg: `Skipped syncing '${source.networkName}' for '${source.name}' because the start block is not finalized`,
@@ -571,7 +577,7 @@ export const createHistoricalSync = async (
             cachedBlocks,
           );
 
-          if (showStart) {
+          if (isInitialCall) {
             args.common.logger.info({
               service: "historical",
               msg: `Started syncing '${source.networkName}' for '${
