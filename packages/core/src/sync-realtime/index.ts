17852578c6e603be4d0016121e9ea4f483f6c4d0
Kyle Scott <kyscott@umich.edu>
2024-09-30 11:01:40 -0400

    filter out reverted call traces

--- packages/core/src/sync-realtime/index.ts
+++ packages/core/src/sync-realtime/index.ts
@@ -500,9 +500,9 @@ export const createRealtimeSync = (
         );
       }
 
-      callTraces = traces.filter(
-        (trace) => trace.type === "call",
-      ) as SyncCallTrace[];
+      callTraces = traces
+        .filter((trace) => trace.type === "call")
+        .filter((trace) => trace.result !== null) as SyncCallTrace[];
     }
 
     // Check that traces refer to the correct block
