988289e97bb47e122d0dd177a890bbb6fbd719ea
kyscott18 <43524469+kyscott18@users.noreply.github.com>
2024-10-09 15:14:23 -0400

    fix status containing unindexed blocks (#1149)
    * fix status containing unindexed blocks
    
    * chore: changeset
    
    * chore: changeset

--- packages/core/src/sync/index.ts
+++ packages/core/src/sync/index.ts
@@ -55,6 +55,7 @@ export type RealtimeEvent =
   | {
       type: "block";
       checkpoint: string;
+      status: Status;
       events: RawEvent[];
     }
   | {
@@ -641,7 +642,12 @@ export const createSync = async (args: CreateSyncParameters): Promise<Sync> => {
               updateRealtimeStatus({ checkpoint: cursor, network });
             }
             args
-              .onRealtimeEvent({ type: "block", checkpoint: to, events })
+              .onRealtimeEvent({
+                type: "block",
+                checkpoint: to,
+                status: JSON.parse(JSON.stringify(status)),
+                events,
+              })
               .then(() => {
                 if (events.length > 0 && isKilled === false) {
                   args.common.logger.info({
