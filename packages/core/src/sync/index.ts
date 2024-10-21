df01a9379604efa374881958cb2f6ca623343099
Kyle Scott <kyscott@umich.edu>
2024-10-21 19:12:29 -0400

    fix: realtime filtering with factory lists of addresses

--- packages/core/src/sync/index.ts
+++ packages/core/src/sync/index.ts
@@ -649,21 +649,6 @@ export const createSync = async (args: CreateSyncParameters): Promise<Sync> => {
             ({ checkpoint }) => checkpoint > to,
           );
 
-          if (event.logs.length > events.length) {
-            console.log("BAD!", {
-              block: event.block.number,
-              events: events.length,
-              logs: event.logs.length,
-            });
-          }
-
-          if (pendingEvents.length > 0) {
-            console.log("BAD!", {
-              block: event.block.number,
-              to,
-            });
-          }
-
           args
             .onRealtimeEvent({
               type: "block",
@@ -848,8 +833,6 @@ export const createSync = async (args: CreateSyncParameters): Promise<Sync> => {
     async startRealtime() {
       for (const network of args.networks) {
         const { syncProgress, realtimeSync } = localSyncContext.get(network)!;
-
-        console.log(syncProgress.finalized.number, syncProgress.finalized.hash);
 
         status[network.name]!.block = {
           number: hexToNumber(syncProgress.current!.number),
