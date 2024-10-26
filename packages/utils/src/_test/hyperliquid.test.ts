06bfd39de40292ae039aafad9ddbc3678082cdcc
Kyle Scott <kyscott@umich.edu>
2024-10-26 12:38:32 -0400

    support hyperliquid retry ranges

---
+++ packages/utils/src/_test/hyperliquid.test.ts
@@ -0,0 +1,59 @@
+import { RpcError, numberToHex } from "viem";
+import { expect, test } from "vitest";
+import { getLogsRetryHelper } from "../getLogsRetryHelper.js";
+import { type Params, getRequest } from "./utils.js";
+
+const request = getRequest("https://api.hyperliquid-testnet.xyz/evm");
+const fromBlock = 9419400;
+const maxBlockRange = 50;
+
+test("hyperliquid success", async () => {
+  const logs = await request({
+    method: "eth_getLogs",
+    params: [
+      {
+        fromBlock: numberToHex(fromBlock),
+        toBlock: numberToHex(fromBlock + maxBlockRange),
+      },
+    ],
+  });
+
+  expect(logs).toHaveLength(5);
+});
+
+test("hyperliquid block range", async () => {
+  const params: Params = [
+    {
+      fromBlock: numberToHex(fromBlock),
+      toBlock: numberToHex(fromBlock + maxBlockRange + 1),
+    },
+  ];
+
+  const error = await request({
+    method: "eth_getLogs",
+    params,
+  }).catch((error) => error);
+
+  expect(error).toBeInstanceOf(RpcError);
+  expect(JSON.stringify(error)).includes("query exceeds max block range 50");
+
+  const retry = getLogsRetryHelper({
+    params,
+    error: error,
+  });
+
+  expect(retry).toStrictEqual({
+    shouldRetry: true,
+    isSuggestedRange: true,
+    ranges: [
+      {
+        fromBlock: numberToHex(fromBlock),
+        toBlock: numberToHex(fromBlock + maxBlockRange),
+      },
+      {
+        fromBlock: numberToHex(fromBlock + maxBlockRange + 1),
+        toBlock: numberToHex(fromBlock + maxBlockRange + 1),
+      },
+    ],
+  });
+});
