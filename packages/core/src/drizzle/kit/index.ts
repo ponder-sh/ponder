0f3b01622b9e4b31976e0c78af7df8fa826e468c
Kyle Scott <kyscott@umich.edu>
2024-10-29 17:48:45 -0400

    fix complext array types

--- packages/core/src/drizzle/kit/index.ts
+++ packages/core/src/drizzle/kit/index.ts
@@ -305,74 +305,55 @@ type JsonStatement =
 // Generator
 ////////
 
-const pgNativeTypes = new Set([
-  "uuid",
-  "smallint",
-  "integer",
-  "bigint",
-  "boolean",
-  "text",
-  "varchar",
-  "serial",
-  "bigserial",
-  "decimal",
-  "numeric",
-  "real",
-  "json",
-  "jsonb",
-  "time",
-  "time with time zone",
-  "time without time zone",
-  "time",
-  "timestamp",
-  "timestamp with time zone",
-  "timestamp without time zone",
-  "date",
-  "interval",
-  "bigint",
-  "bigserial",
-  "double precision",
-  "interval year",
-  "interval month",
-  "interval day",
-  "interval hour",
-  "interval minute",
-  "interval second",
-  "interval year to month",
-  "interval day to hour",
-  "interval day to minute",
-  "interval day to second",
-  "interval hour to minute",
-  "interval hour to second",
-  "interval minute to second",
-]);
+const parseType = (schemaPrefix: string, type: string) => {
+  const pgNativeTypes = [
+    "uuid",
+    "smallint",
+    "integer",
+    "bigint",
+    "boolean",
+    "text",
+    "varchar",
+    "serial",
+    "bigserial",
+    "decimal",
+    "numeric",
+    "real",
+    "json",
+    "jsonb",
+    "time",
+    "time with time zone",
+    "time without time zone",
+    "time",
+    "timestamp",
+    "timestamp with time zone",
+    "timestamp without time zone",
+    "date",
+    "interval",
+    "bigint",
+    "bigserial",
+    "double precision",
+    "interval year",
+    "interval month",
+    "interval day",
+    "interval hour",
+    "interval minute",
+    "interval second",
+    "interval year to month",
+    "interval day to hour",
+    "interval day to minute",
+    "interval day to second",
+    "interval hour to minute",
+    "interval hour to second",
+    "interval minute to second",
+  ];
 
-const isPgNativeType = (it: string) => {
-  if (pgNativeTypes.has(it)) return true;
-  const toCheck = it.replace(/ /g, "");
-  return (
-    toCheck.startsWith("varchar(") ||
-    toCheck.startsWith("char(") ||
-    toCheck.startsWith("numeric(") ||
-    toCheck.startsWith("timestamp(") ||
-    toCheck.startsWith("doubleprecision[") ||
-    toCheck.startsWith("intervalyear(") ||
-    toCheck.startsWith("intervalmonth(") ||
-    toCheck.startsWith("intervalday(") ||
-    toCheck.startsWith("intervalhour(") ||
-    toCheck.startsWith("intervalminute(") ||
-    toCheck.startsWith("intervalsecond(") ||
-    toCheck.startsWith("intervalyeartomonth(") ||
-    toCheck.startsWith("intervaldaytohour(") ||
-    toCheck.startsWith("intervaldaytominute(") ||
-    toCheck.startsWith("intervaldaytosecond(") ||
-    toCheck.startsWith("intervalhourtominute(") ||
-    toCheck.startsWith("intervalhourtosecond(") ||
-    toCheck.startsWith("intervalminutetosecond(") ||
-    toCheck.startsWith("vector(") ||
-    toCheck.startsWith("geometry(") ||
-    /^(\w+)(\[\d*])+$/.test(it)
-  );
+  const arrayDefinitionRegex = /\[\d*(?:\[\d*\])*\]/g;
+  const arrayDefinition = (type.match(arrayDefinitionRegex) ?? []).join("");
+  const withoutArrayDefinition = type.replace(arrayDefinitionRegex, "");
+  return pgNativeTypes.some((it) => type.startsWith(it))
+    ? `${withoutArrayDefinition}${arrayDefinition}`
+    : `${schemaPrefix}"${withoutArrayDefinition}"${arrayDefinition}`;
 };
 
 abstract class Convertor {
@@ -413,9 +394,7 @@ class PgCreateTableConvertor extends Convertor {
           ? `"${column.typeSchema}".`
           : "";
 
-      const type = isPgNativeType(column.type)
-        ? column.type
-        : `${schemaPrefix}"${column.type}"`;
+      const type = parseType(schemaPrefix, column.type);
       // const generated = column.generated;
 
       // const generatedStatement = generated
