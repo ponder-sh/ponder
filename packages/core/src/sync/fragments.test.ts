308d102fa37a23ef66852e5a09893c1e77564507
Kyle Scott <kyscott@umich.edu>
2024-11-04 21:59:40 -0500

    fix fragment tests

--- packages/core/src/sync/fragments.test.ts
+++ packages/core/src/sync/fragments.test.ts
@@ -16,7 +16,7 @@ test("getLogFilterFragmentIds generates 1 log filter fragment for null filter", 
     includeTransactionReceipts: false,
   });
 
-  expect(logFilterFragments[0]!.id).toBe("1_null_null_null_null_null_0");
+  expect(logFilterFragments[0]!.id).toBe("log_1_null_null_null_null_null_0");
 });
 
 test("getLogFilterFragmentIds generates 1 log filter fragment for simple filter", () => {
@@ -28,7 +28,7 @@ test("getLogFilterFragmentIds generates 1 log filter fragment for simple filter"
     includeTransactionReceipts: false,
   });
 
-  expect(logFilterFragments[0]!.id).toBe("1_0xa_null_null_null_null_0");
+  expect(logFilterFragments[0]!.id).toBe("log_1_0xa_null_null_null_null_0");
 });
 
 test("getLogFilterFragmentIds generates 4 log filter fragment for 2x2 filter", () => {
@@ -40,10 +40,10 @@ test("getLogFilterFragmentIds generates 4 log filter fragment for 2x2 filter", (
     includeTransactionReceipts: false,
   });
 
-  expect(logFilterFragments[0]!.id).toBe("115511_0xa_0xc_null_0xe_null_0");
-  expect(logFilterFragments[1]!.id).toBe("115511_0xa_0xd_null_0xe_null_0");
-  expect(logFilterFragments[2]!.id).toBe("115511_0xb_0xc_null_0xe_null_0");
-  expect(logFilterFragments[3]!.id).toBe("115511_0xb_0xd_null_0xe_null_0");
+  expect(logFilterFragments[0]!.id).toBe("log_115511_0xa_0xc_null_0xe_null_0");
+  expect(logFilterFragments[1]!.id).toBe("log_115511_0xa_0xd_null_0xe_null_0");
+  expect(logFilterFragments[2]!.id).toBe("log_115511_0xb_0xc_null_0xe_null_0");
+  expect(logFilterFragments[3]!.id).toBe("log_115511_0xb_0xd_null_0xe_null_0");
 });
 
 test("getLogFilterFragmentIds generates 12 log filter fragment for 2x2x3 filter", () => {
@@ -67,7 +67,7 @@ test("getLogFilterFragmentIds includeTransactionReceipts", () => {
     includeTransactionReceipts: true,
   });
 
-  expect(logFilterFragments[0]!.id).toBe("1_null_null_null_null_null_1");
+  expect(logFilterFragments[0]!.id).toBe("log_1_null_null_null_null_null_1");
 });
 
 test("getLogFilterFragmentIds builds id containing factory topic", () => {
@@ -89,7 +89,7 @@ test("getLogFilterFragmentIds builds id containing factory topic", () => {
   expect(fragments).toHaveLength(1);
 
   expect(fragments[0]!.id).toBe(
-    "1_0xa_0x00fef2d461a2fabbb523f9f42752c61336f03b17a602af52cc6c83cb8b110599_topic1_null_null_null_null_0",
+    "log_1_0xa_0x00fef2d461a2fabbb523f9f42752c61336f03b17a602af52cc6c83cb8b110599_topic1_null_null_null_null_0",
   );
 });
 
@@ -112,7 +112,7 @@ test("getLogFilterFragmentIds builds id containing factory offset", () => {
   expect(fragments).toHaveLength(1);
 
   expect(fragments[0]!.id).toBe(
-    "115511_0xa_0x00fef2d461a2fabbb523f9f42752c61336f03b17a602af52cc6c83cb8b110599_offset64_null_null_null_null_0",
+    "log_115511_0xa_0x00fef2d461a2fabbb523f9f42752c61336f03b17a602af52cc6c83cb8b110599_offset64_null_null_null_null_0",
   );
 });
 
@@ -135,9 +135,9 @@ test("getLogFilterFragmentIds builds id with multiple factories", () => {
   expect(fragments).toHaveLength(2);
 
   expect(fragments[0]!.id).toBe(
-    "1_0xa_0x00fef2d461a2fabbb523f9f42752c61336f03b17a602af52cc6c83cb8b110599_offset64_null_null_null_null_0",
+    "log_1_0xa_0x00fef2d461a2fabbb523f9f42752c61336f03b17a602af52cc6c83cb8b110599_offset64_null_null_null_null_0",
   );
   expect(fragments[1]!.id).toBe(
-    "1_0xb_0x00fef2d461a2fabbb523f9f42752c61336f03b17a602af52cc6c83cb8b110599_offset64_null_null_null_null_0",
+    "log_1_0xb_0x00fef2d461a2fabbb523f9f42752c61336f03b17a602af52cc6c83cb8b110599_offset64_null_null_null_null_0",
   );
 });
