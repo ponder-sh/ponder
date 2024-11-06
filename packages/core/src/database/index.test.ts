e23b419207123608ea432c3ac5d0954862e2a2dc
Kyle Scott <kyscott@umich.edu>
2024-11-06 14:13:42 -0500

    skip database test for pglite

--- packages/core/src/database/index.test.ts
+++ packages/core/src/database/index.test.ts
@@ -397,44 +397,48 @@ test("setup() with the same build ID recovers if the lock expires after waiting"
   await databaseTwo.kill();
 });
 
-test("setup() with the same build ID succeeds if the lock doesn't expires after waiting", async (context) => {
-  context.common.options.databaseHeartbeatInterval = 250;
-  context.common.options.databaseHeartbeatTimeout = 625;
+// PGlite not being able to concurrently connect to the same database from two different clients
+// makes this test impossible.
+test.skipIf(process.env.DATABASE_URL === undefined)(
+  "setup() with the same build ID succeeds if the lock doesn't expires after waiting",
+  async (context) => {
+    context.common.options.databaseHeartbeatInterval = 250;
+    context.common.options.databaseHeartbeatTimeout = 1000;
 
-  const database = createDatabase({
-    common: context.common,
-    schema: { account },
-    databaseConfig: context.databaseConfig,
-    instanceId: "1234",
-    buildId: "abc",
-    ...buildSchema({
+    const database = createDatabase({
+      common: context.common,
       schema: { account },
+      databaseConfig: context.databaseConfig,
       instanceId: "1234",
-    }),
-  });
-  await database.setup();
-  await database.finalize({ checkpoint: createCheckpoint(10) });
+      buildId: "abc",
+      ...buildSchema({
+        schema: { account },
+        instanceId: "1234",
+      }),
+    });
+    await database.setup();
+    await database.finalize({ checkpoint: createCheckpoint(10) });
 
-  const databaseTwo = createDatabase({
-    common: context.common,
-    schema: { account },
-    databaseConfig: context.databaseConfig,
-    instanceId: "5678",
-    buildId: "abc",
-    ...buildSchema({
+    const databaseTwo = createDatabase({
+      common: context.common,
       schema: { account },
+      databaseConfig: context.databaseConfig,
       instanceId: "5678",
-    }),
-  });
+      buildId: "abc",
+      ...buildSchema({
+        schema: { account },
+        instanceId: "5678",
+      }),
+    });
 
-  const { checkpoint } = await databaseTwo.setup();
+    const { checkpoint } = await databaseTwo.setup();
 
-  expect(checkpoint).toMatchObject(encodeCheckpoint(zeroCheckpoint));
+    expect(checkpoint).toMatchObject(encodeCheckpoint(zeroCheckpoint));
 
-  await database.unlock();
-  await database.kill();
-  await databaseTwo.kill();
-});
+    await database.kill();
+    await databaseTwo.kill();
+  },
+);
 
 test("setup() drops old tables", async (context) => {
   for (let i = 0; i < 5; i++) {
