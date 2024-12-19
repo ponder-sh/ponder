e31c86c8d783ef5551e19ff60da4e255b55d52a5
Kyle Scott <kyscott@umich.edu>
2024-12-19 14:45:58 -0500

    fix browser compatibility

--- packages/core/src/drizzle/onchain.ts
+++ packages/core/src/drizzle/onchain.ts
@@ -149,7 +149,8 @@ export const onchainTable = <
   extra: extra;
   dialect: "pg";
 }> => {
-  const schema = global.PONDER_DATABASE_SCHEMA;
+  const schema =
+    typeof global === "undefined" ? undefined : global.PONDER_DATABASE_SCHEMA;
   const table = pgTableWithSchema(name, columns, extraConfig as any, schema);
 
   // @ts-ignore
@@ -181,7 +182,8 @@ export const onchainEnum = <U extends string, T extends Readonly<[U, ...U[]]>>(
   enumName: string,
   values: T | Writable<T>,
 ): OnchainEnum<Writable<T>> & { [onchain]: true } => {
-  const schema = global.PONDER_DATABASE_SCHEMA;
+  const schema =
+    typeof global === "undefined" ? undefined : global.PONDER_DATABASE_SCHEMA;
   const e = pgEnumWithSchema(enumName, values, schema);
 
   // @ts-ignore
