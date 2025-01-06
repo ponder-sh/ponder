a5cfc47d0c230fb6351bb0c65cd9fddec81feeb0
typedarray <90073088+0xOlias@users.noreply.github.com>
2025-01-06 12:47:22 -0500

    rm pglite memory test issue

--- packages/core/src/utils/pglite.ts
+++ packages/core/src/utils/pglite.ts
@@ -5,6 +5,7 @@ import { type PGliteOptions as Options, PGlite } from "@electric-sql/pglite";
 export type PGliteOptions = Prettify<Options & { dataDir: string }>;
 
 export function createPglite(options: PGliteOptions) {
-  mkdirSync(options.dataDir, { recursive: true });
+  if (options.dataDir !== "memory://")
+    mkdirSync(options.dataDir, { recursive: true });
   return new PGlite(options);
 }
