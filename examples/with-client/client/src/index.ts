d7e0dd5b42373cbe9beef43ba94f3f81e9c8a4ee
Kyle Scott <kyscott@umich.edu>
2025-01-06 13:53:44 -0500

    fix client example

--- examples/with-client/client/src/index.ts
+++ examples/with-client/client/src/index.ts
@@ -1,13 +1,12 @@
-import { createClient, sum } from "@ponder/client";
+import { createClient } from "@ponder/client";
 import * as schema from "../../ponder/ponder.schema";
 
 const client = createClient("http://localhost:42069", { schema });
 
-const { unsubscribe } = client.live(
-  (db) => db.select({ sum: sum(schema.account.balance) }).from(schema.account),
-  (result) => console.log(result),
-);
+const response = await client.db
+  //  ^?
+  .select()
+  .from(schema.account)
+  .limit(10);
 
-await new Promise((resolve) => setTimeout(resolve, 5_000));
-
-unsubscribe();
+console.log({ response });
