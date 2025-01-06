7b3a210c65e94a82ae57d81a0da5828a779dd4fa
kyscott18 <43524469+kyscott18@users.noreply.github.com>
2025-01-06 10:44:14 -0500

    Granular `sync-historical` (#1383)
    * initial
    
    * use fragment for granular cache
    
    * fix sync-store tests
    
    * cleanup and add test
    
    * fix test name
    
    * fix shutdown behavior
    
    * chore: changeset

--- packages/core/src/sync/index.ts
+++ packages/core/src/sync/index.ts
@@ -25,6 +25,7 @@ import {
   type Interval,
   intervalDifference,
   intervalIntersection,
+  intervalIntersectionMany,
   intervalSum,
   sortIntervals,
 } from "@/utils/interval.js";
@@ -986,10 +987,15 @@ export const getCachedBlock = ({
       filter.fromBlock ?? 0,
       filter.toBlock ?? Number.POSITIVE_INFINITY,
     ] satisfies Interval;
-    const cachedIntervals = historicalSync.intervalsCache.get(filter)!;
+    const fragmentIntervals = historicalSync.intervalsCache.get(filter)!;
 
     const completedIntervals = sortIntervals(
-      intervalIntersection([requiredInterval], cachedIntervals),
+      intervalIntersection(
+        [requiredInterval],
+        intervalIntersectionMany(
+          fragmentIntervals.map(({ intervals }) => intervals),
+        ),
+      ),
     );
 
     if (completedIntervals.length === 0) return undefined;
@@ -1080,7 +1086,7 @@ export async function* localHistoricalSyncGenerator({
 
   const requiredIntervals = Array.from(
     historicalSync.intervalsCache.entries(),
-  ).flatMap(([filter, interval]) =>
+  ).flatMap(([filter, fragmentIntervals]) =>
     intervalDifference(
       [
         [
@@ -1091,7 +1097,9 @@ export async function* localHistoricalSyncGenerator({
           ),
         ],
       ],
-      interval,
+      intervalIntersectionMany(
+        fragmentIntervals.map(({ intervals }) => intervals),
+      ),
     ),
   );
 
