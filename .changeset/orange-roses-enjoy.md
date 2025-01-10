8cb34bb0a95511bd1077b28b7e4d0ec66f41aa93
Kyle Scott <kyscott@umich.edu>
2025-01-10 17:57:03 -0500

    chore: changeset

---
+++ .changeset/orange-roses-enjoy.md
@@ -0,0 +1,5 @@
+---
+"ponder": patch
+---
+
+Fixed an issue where ZKsync system logs with transaction receipts failed with the error `Detected inconsistent RPC responses. 'transaction.hash' 0x0000000000000000000000000000000000000000000000000000000000000000 not found in eth_getBlockReceipts response for block (...)`.
--- packages/core/src/sync/events.ts
+++ packages/core/src/sync/events.ts
@@ -236,11 +236,13 @@ export const buildEvents = ({
                         transactionCache.get(log.transactionHash)!,
                       )
                     : undefined,
-                  transactionReceipt: shouldGetTransactionReceipt(filter)
-                    ? convertTransactionReceipt(
-                        transactionReceiptCache.get(log.transactionHash)!,
-                      )
-                    : undefined,
+                  transactionReceipt:
+                    transactionReceiptCache.has(log.transactionHash) &&
+                    shouldGetTransactionReceipt(filter)
+                      ? convertTransactionReceipt(
+                          transactionReceiptCache.get(log.transactionHash)!,
+                        )
+                      : undefined,
                   trace: undefined,
                 });
               }
