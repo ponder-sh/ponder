0a2b8933d251e661aba0e0616951673dec3a8f65
Kyle Scott <kyscott@umich.edu>
2025-02-11 09:57:15 -0500

    flush tests

--- packages/core/src/indexing-store/cache.test.ts
+++ packages/core/src/indexing-store/cache.test.ts
@@ -15,7 +15,7 @@ beforeEach(setupCommon);
 beforeEach(setupIsolatedDatabase);
 beforeEach(setupCleanup);
 
-test("flush", async (context) => {
+test("flush() insert", async (context) => {
   const schema = {
     account: onchainTable("account", (p) => ({
       address: p.hex().primaryKey(),
@@ -34,46 +34,212 @@ test("flush", async (context) => {
     checkpoint: ZERO_CHECKPOINT_STRING,
   });
 
-  const indexingStore = createHistoricalIndexingStore({
+  await database.transaction(async (client, tx) => {
+    const indexingStore = createHistoricalIndexingStore({
+      common: context.common,
+      database,
+      schemaBuild: { schema },
+      indexingCache,
+      db: tx,
+      client,
+    });
+
+    await indexingStore.insert(schema.account).values({
+      address: zeroAddress,
+      balance: 10n,
+    });
+
+    await indexingCache.flush({ client });
+
+    const result = await indexingStore.find(schema.account, {
+      address: zeroAddress,
+    });
+
+    expect(result).toStrictEqual({
+      address: zeroAddress,
+      balance: 10n,
+    });
+  });
+});
+
+test("flush() update", async (context) => {
+  const schema = {
+    account: onchainTable("account", (p) => ({
+      address: p.hex().primaryKey(),
+      balance: p.bigint().notNull(),
+    })),
+  };
+
+  const { database } = await setupDatabaseServices(context, {
+    schemaBuild: { schema },
+  });
+
+  const indexingCache = createIndexingCache({
     common: context.common,
     database,
     schemaBuild: { schema },
-    indexingCache,
-    db: database.qb.drizzle,
+    checkpoint: ZERO_CHECKPOINT_STRING,
   });
 
-  // insert
+  await database.transaction(async (client, tx) => {
+    const indexingStore = createHistoricalIndexingStore({
+      common: context.common,
+      database,
+      schemaBuild: { schema },
+      indexingCache,
+      db: tx,
+      client,
+    });
 
-  await indexingStore.insert(schema.account).values({
-    address: zeroAddress,
-    balance: 10n,
+    await indexingStore.insert(schema.account).values({
+      address: zeroAddress,
+      balance: 10n,
+    });
+
+    await indexingCache.flush({ client });
+    indexingCache.prepare();
+
+    await indexingStore.update(schema.account, { address: zeroAddress }).set({
+      balance: 12n,
+    });
+
+    await indexingCache.flush({ client });
+
+    const result = await indexingStore.find(schema.account, {
+      address: zeroAddress,
+    });
+
+    expect(result).toStrictEqual({
+      address: zeroAddress,
+      balance: 12n,
+    });
+  });
+});
+
+test("flush() encoding", async (context) => {
+  const schema = {
+    test: onchainTable("test", (p) => ({
+      hex: p.hex().primaryKey(),
+      bigint: p.bigint().notNull(),
+      array: p.integer().array().notNull(),
+      json: p.json().notNull(),
+      null: p.text(),
+    })),
+  };
+
+  const { database } = await setupDatabaseServices(context, {
+    schemaBuild: { schema },
   });
 
-  await indexingCache.flush({ db: database.qb.drizzle });
-
-  let result = await indexingStore.find(schema.account, {
-    address: zeroAddress,
+  const indexingCache = createIndexingCache({
+    common: context.common,
+    database,
+    schemaBuild: { schema },
+    checkpoint: ZERO_CHECKPOINT_STRING,
   });
 
-  expect(result).toStrictEqual({
-    address: zeroAddress,
-    balance: 10n,
+  await database.transaction(async (client, tx) => {
+    const indexingStore = createHistoricalIndexingStore({
+      common: context.common,
+      database,
+      schemaBuild: { schema },
+      indexingCache,
+      db: tx,
+      client,
+    });
+
+    await indexingStore.insert(schema.test).values({
+      hex: zeroAddress,
+      bigint: 10n,
+      array: [1, 2, 4],
+      json: { a: 1, b: 2 },
+      null: null,
+    });
+
+    await indexingCache.flush({ client });
+
+    indexingCache.clear();
+    const result = await indexingStore.sql.select().from(schema.test);
+
+    expect(result).toMatchInlineSnapshot(`
+      [
+        {
+          "array": [
+            1,
+            2,
+            4,
+          ],
+          "bigint": 10n,
+          "hex": "0x0000000000000000000000000000000000000000",
+          "json": {
+            "a": 1,
+            "b": 2,
+          },
+          "null": null,
+        },
+      ]
+    `);
+  });
+});
+
+test("flush() encoding backslash", async (context) => {
+  // \b
+  // \f
+  // \n
+  // \r
+  // \t
+  // \v
+  // \0
+  // \x0
+  const schema = {
+    test: onchainTable("test", (p) => ({
+      backslash: p.text().primaryKey(),
+    })),
+  };
+
+  const { database } = await setupDatabaseServices(context, {
+    schemaBuild: { schema },
   });
 
-  // update
-
-  await indexingStore.update(schema.account, { address: zeroAddress }).set({
-    balance: 12n,
+  const indexingCache = createIndexingCache({
+    common: context.common,
+    database,
+    schemaBuild: { schema },
+    checkpoint: ZERO_CHECKPOINT_STRING,
   });
 
-  await indexingCache.flush({ db: database.qb.drizzle });
+  await database.transaction(async (client, tx) => {
+    const indexingStore = createHistoricalIndexingStore({
+      common: context.common,
+      database,
+      schemaBuild: { schema },
+      indexingCache,
+      db: tx,
+      client,
+    });
 
-  result = await indexingStore.find(schema.account, {
-    address: zeroAddress,
-  });
+    await indexingStore.insert(schema.test).values([
+      { backslash: "\\\\b" },
+      // { backslash: "\f" },
+      // { backslash: "\n" },
+      // { backslash: "\r" },
+      // { backslash: "\t" },
+      // { backslash: "\v" },
+      // { backslash: "\0" },
+      // { backslash: "\\x0" },
+    ]);
 
-  expect(result).toStrictEqual({
-    address: zeroAddress,
-    balance: 12n,
+    await indexingCache.flush({ client });
+
+    indexingCache.clear();
+    const result = await indexingStore.sql.select().from(schema.test);
+
+    expect(result).toMatchInlineSnapshot(`
+      [
+        {
+          "backslash": "\\b",
+        },
+      ]
+    `);
   });
 });
