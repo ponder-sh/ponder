82b1a7e3a1fb31d5b918f620969279851f8f9543
Kyle Scott <kyscott@umich.edu>
2025-02-12 15:20:54 -0500

    set readonly statement timeout to 30s

--- packages/core/src/utils/pg.ts
+++ packages/core/src/utils/pg.ts
@@ -101,9 +101,7 @@ export function createReadonlyPool(
           SET search_path = "${namespace}";
           SET SESSION CHARACTERISTICS AS TRANSACTION READ ONLY;
           SET work_mem = '512MB';
-          SET statement_timeout = '500ms';
-          SET lock_timeout = '500ms';
-        `,
+          SET lock_timeout = '500ms';`,
             callback,
           );
         });
@@ -114,9 +112,7 @@ export function createReadonlyPool(
           SET search_path = "${namespace}";
           SET SESSION CHARACTERISTICS AS TRANSACTION READ ONLY;
           SET work_mem = '512MB';
-          SET statement_timeout = '500ms';
-          SET lock_timeout = '500ms';
-        `,
+          SET lock_timeout = '500ms';`,
           ).then(() => {}),
         );
       }
@@ -125,7 +121,7 @@ export function createReadonlyPool(
 
   const pool = new pg.Pool({
     // https://stackoverflow.com/questions/59155572/how-to-set-query-timeout-in-relation-to-statement-timeout
-    statement_timeout: 2 * 60 * 1000, // 2 minutes
+    statement_timeout: 30 * 1000, // 30s
     // @ts-expect-error: The custom Client is an undocumented option.
     Client: ReadonlyClient,
     ...config,
