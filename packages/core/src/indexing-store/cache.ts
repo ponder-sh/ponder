98bac31f6c0c19a6d2a87d383d2ac2bea0ec6338
Kyle Scott <kyscott@umich.edu>
2025-02-24 19:39:38 -0500

    .

--- packages/core/src/indexing-store/cache.ts
+++ packages/core/src/indexing-store/cache.ts
@@ -742,11 +742,9 @@ export const createIndexingCache = ({
       let cacheSize = 0;
       for (const { size } of cache.values()) cacheSize += size;
 
-      const flushIndex = Math.max(
+      const flushIndex =
         totalCacheOps -
-          cacheSize * (1 - common.options.indexingCacheEvictRatio),
-        0,
-      );
+        cacheSize * (1 - common.options.indexingCacheEvictRatio);
 
       if (cacheBytes + spilloverBytes > common.options.indexingCacheMaxBytes) {
         isCacheComplete = false;
