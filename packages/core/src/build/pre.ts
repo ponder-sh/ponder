2c6f2aaa743483169f3913ae3757e70eda38f073
Kevin <90073088+typedarray@users.noreply.github.com>
2025-02-26 12:16:59 -0500

    feat: pg ssl connection options (#1447)
    * feat: ssl postgres connection options
    
    * changeset
    
    * docs
    
    ---------
    
    Co-authored-by: typedarray <90073088+0xOlias@users.noreply.github.com>
    Co-authored-by: mehmetraufoguz <88249309+mehmetraufoguz@users.noreply.github.com>

--- packages/core/src/build/pre.ts
+++ packages/core/src/build/pre.ts
@@ -65,8 +65,9 @@ export function buildPre({
       });
 
       const poolConfig = {
-        max: config.database.poolConfig?.max ?? 30,
         connectionString,
+        max: config.database.poolConfig?.max ?? 30,
+        ssl: config.database.poolConfig?.ssl ?? false,
       };
 
       databaseConfig = { kind: "postgres", poolConfig };
@@ -96,7 +97,7 @@ export function buildPre({
         msg: `Using Postgres database ${getDatabaseName(connectionString)} (${source})`,
       });
 
-      const poolConfig = { max: 30, connectionString };
+      const poolConfig = { connectionString, max: 30 };
 
       databaseConfig = { kind: "postgres", poolConfig };
     } else {
