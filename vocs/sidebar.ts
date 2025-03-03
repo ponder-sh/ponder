89b3ae5b6ef30ca464107bf7336c2492ca854221
typedarray <90073088+0xOlias@users.noreply.github.com>
2025-03-03 14:15:10 -0500

    scaffold sidebar

--- vocs/sidebar.ts
+++ vocs/sidebar.ts
@@ -3,12 +3,108 @@ import type { Sidebar } from "vocs";
 export const sidebar = {
   "/docs/": [
     {
-      text: "Getting Started",
-      link: "/docs/getting-started",
+      text: "Introduction",
+      items: [
+        { text: "Getting started", link: "/docs/getting-started" },
+        { text: "System requirements", link: "/docs/system-requirements" },
+        { text: "Migration guide", link: "/docs/migration-guide" },
+      ],
     },
     {
-      text: "Contracts & Networks",
-      link: "/docs/contracts-and-networks",
+      text: "Config",
+      items: [
+        { text: "Introduction", link: "/docs/config" },
+        { text: "Networks", link: "/docs/config/networks" },
+        { text: "Contracts", link: "/docs/config/contracts" },
+        { text: "Accounts", link: "/docs/config/accounts" },
+        { text: "Block intervals", link: "/docs/config/block-intervals" },
+      ],
+    },
+    {
+      text: "Schema",
+      items: [
+        { text: "Introduction", link: "/docs/schema" },
+        { text: "Tables", link: "/docs/schema/tables" },
+        { text: "Relations", link: "/docs/schema/relations" },
+      ],
+    },
+    {
+      text: "Indexing",
+      items: [
+        { text: "Introduction", link: "/docs/indexing" },
+        { text: "Write to the database", link: "/docs/indexing/write" },
+        { text: "Fetch RPC data", link: "/docs/indexing/fetch" },
+      ],
+    },
+    {
+      text: "Query",
+      items: [
+        { text: "Introduction", link: "/docs/query" },
+        { text: "API functions", link: "/docs/query/api-functions" },
+        { text: "SQL client", link: "/docs/query/sql-client" },
+        { text: "GraphQL", link: "/docs/query/graphql" },
+        { text: "Direct SQL", link: "/docs/query/direct-sql" },
+      ],
+    },
+    {
+      text: "Production",
+      items: [
+        { text: "Introduction", link: "/docs/production" },
+        {
+          text: "Deploy",
+          items: [
+            { text: "Railway", link: "/docs/production/deploy/railway" },
+            {
+              text: "Self-hosting",
+              link: "/docs/production/deploy/self-hosting",
+            },
+          ],
+        },
+      ],
+    },
+    {
+      text: "Recipes",
+      items: [
+        { text: "Factory contracts", link: "/docs/recipes/factory-contracts" },
+        { text: "Proxy contracts", link: "/docs/recipes/proxy-contracts" },
+        { text: "Time series data", link: "/docs/recipes/time-series-data" },
+        {
+          text: "With Foundry",
+          link: "/docs/recipes/with-foundry",
+        },
+      ],
+    },
+    {
+      text: "API reference",
+      items: [
+        {
+          text: "ponder.config.ts",
+          link: "/docs/api-reference/ponder.config.ts",
+        },
+        {
+          text: "ponder.schema.ts",
+          link: "/docs/api-reference/ponder.schema.ts",
+        },
+        {
+          text: "Indexing functions",
+          link: "/docs/api-reference/indexing-functions",
+        },
+        { text: "API functions", link: "/docs/api-reference/api-functions" },
+        { text: "CLI (dev, start, serve)", link: "/docs/api-reference/cli" },
+        { text: "create-ponder", link: "/docs/api-reference/create-ponder" },
+        { text: "Logs", link: "/docs/api-reference/logs" },
+        { text: "Metrics", link: "/docs/api-reference/metrics" },
+      ],
+    },
+    {
+      text: "Packages",
+      items: [
+        { text: "ponder", link: "/docs/packages/ponder" },
+        { text: "@ponder/client", link: "/docs/packages/ponder-client" },
+        { text: "@ponder/react", link: "/docs/packages/ponder-react" },
+        { text: "@ponder/utils", link: "/docs/packages/ponder-utils" },
+        { text: "create-ponder", link: "/docs/packages/create-ponder" },
+      ],
     },
   ],
 } satisfies Sidebar;
