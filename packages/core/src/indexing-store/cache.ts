5e4fb64fc866f9962202144c28586a07859a4c36
Kyle Scott <kyscott@umich.edu>
2025-03-10 17:09:37 -0400

    early exit prediction

--- packages/core/src/indexing-store/cache.ts
+++ packages/core/src/indexing-store/cache.ts
@@ -664,6 +664,7 @@ export const createIndexingCache = ({
               ([key]) => key,
             ).map(([, prediction]) => getWhereCondition(table, prediction));
 
+            if (conditions.length === 0) return;
             const endClock = startClock();
 
             await db
