cc94be078d1a8572d0028be586f740fd5cbbb8a0
Kyle Scott <kyscott@umich.edu>
2025-04-18 15:30:41 -0400

    nits

--- packages/core/src/build/plugin.ts
+++ packages/core/src/build/plugin.ts
@@ -19,7 +19,7 @@ export default schema;
 const apiModule = () => `import { createPublicClient } from "viem";
 
 if (globalThis.PONDER_INDEXING_BUILD === undefined || globalThis.PONDER_DATABASE === undefined) {
-  throw new Error('Invalid dependency graph. Please ensure that nothing is imported from "src/api/index.ts"')
+  throw new Error('Invalid dependency graph. Config, schema, and indexing function files cannot import objects from the API function file "src/api/index.ts".')
 }
 
 const publicClients = {};
