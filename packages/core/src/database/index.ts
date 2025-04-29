ba68891cc2a9ee9a241760dae3f3c721f5fb4e34
Kyle Scott <kyscott@umich.edu>
2025-04-29 17:50:32 -0400

    don't error for dev command

--- packages/core/src/database/index.ts
+++ packages/core/src/database/index.ts
@@ -780,15 +780,6 @@ EXECUTE PROCEDURE "${namespace}".${notification};`),
               } as const;
             }
 
-            // Note: ponder <=0.8 will evaluate this as true because the version is undefined
-            if (previousApp.version !== VERSION) {
-              const error = new NonRetryableError(
-                `Schema '${namespace}' was previously used by a Ponder app with a different minor version. Drop the schema first, or use a different schema. Read more: https://ponder.sh/docs/getting-started/database#database-schema`,
-              );
-              error.stack = undefined;
-              throw error;
-            }
-
             if (
               previousApp.is_dev === 1 ||
               (process.env.PONDER_EXPERIMENTAL_DB === "platform" &&
@@ -833,6 +824,15 @@ EXECUTE PROCEDURE "${namespace}".${notification};`),
                 status: "success",
                 crashRecoveryCheckpoint: undefined,
               } as const;
+            }
+
+            // Note: ponder <=0.8 will evaluate this as true because the version is undefined
+            if (previousApp.version !== VERSION) {
+              const error = new NonRetryableError(
+                `Schema '${namespace}' was previously used by a Ponder app with a different minor version. Drop the schema first, or use a different schema. Read more: https://ponder.sh/docs/getting-started/database#database-schema`,
+              );
+              error.stack = undefined;
+              throw error;
             }
 
             if (
