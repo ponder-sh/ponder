7b950e5f06550e0c0eb2ad70e21c6a6c86facf3c
Kevin <90073088+typedarray@users.noreply.github.com>
2025-05-12 17:18:38 -0400

    [draft] new docs (#1732)
    * basic idea working
    
    * style fixes
    
    * scaffold sidebar
    
    * config tweaks
    
    * more styles, start content
    
    * more docs migration
    
    * wip footer
    
    * checkpoint
    
    * contracts done
    
    * networks page
    
    * schema docs
    
    * indexing docs done
    
    * wip indexing pages
    
    * indexing db done
    
    * more docs
    
    * wip graphql
    
    * graphql done
    
    * api reference updates
    
    * query over http done
    
    * lots
    
    * api endpoints guide done
    
    * move tidy
    
    * database stuff
    
    * move
    
    * database done
    
    * installation
    
    * mermaid sorta working
    
    * utils
    
    * use headings
    
    * indexing fn api ref
    
    * fix config lyout
    
    * getting started begin
    
    * migration guide
    
    * migration
    
    * wip
    
    * fix lockfile
    
    * cli docs
    
    * create ponder
    
    * get started
    
    * foundry
    
    * guides
    
    * callouts
    
    * gitignore
    
    * links
    
    * links
    
    * improve footer
    
    * fix blog
    
    * blog and light fixes
    
    * telemetry docs
    
    * fix lint
    
    * fix dead link
    
    ---------
    
    Co-authored-by: typedarray <90073088+0xOlias@users.noreply.github.com>

---
+++ vocs/sidebar.ts
@@ -0,0 +1,115 @@
+import type { Sidebar } from "vocs";
+
+export const sidebar = {
+  "/docs/": [
+    {
+      text: "Introduction",
+      items: [
+        { text: "Get started", link: "/docs/get-started" },
+        { text: "Requirements", link: "/docs/requirements" },
+        { text: "Database", link: "/docs/database" },
+        { text: "Migration guide", link: "/docs/migration-guide" },
+      ],
+    },
+    {
+      text: "Config",
+      items: [
+        { text: "Networks", link: "/docs/config/networks" },
+        { text: "Contracts", link: "/docs/config/contracts" },
+        { text: "Accounts", link: "/docs/config/accounts" },
+        { text: "Block intervals", link: "/docs/config/block-intervals" },
+      ],
+    },
+    {
+      text: "Schema",
+      items: [
+        { text: "Tables", link: "/docs/schema/tables" },
+        { text: "Relations", link: "/docs/schema/relations" },
+      ],
+    },
+    {
+      text: "Indexing",
+      items: [
+        { text: "Overview", link: "/docs/indexing/overview" },
+        { text: "Write to the database", link: "/docs/indexing/write" },
+        { text: "Read contract data", link: "/docs/indexing/read-contracts" },
+      ],
+    },
+    {
+      text: "Query",
+      items: [
+        {
+          text: "HTTP",
+          items: [
+            { text: "GraphQL", link: "/docs/query/graphql" },
+            { text: "SQL client", link: "/docs/query/sql-client" },
+            { text: "API endpoints", link: "/docs/query/api-endpoints" },
+          ],
+        },
+        { text: "Direct SQL", link: "/docs/query/direct-sql" },
+      ],
+    },
+    {
+      text: "Production",
+      items: [
+        { text: "Railway", link: "/docs/production/railway" },
+        { text: "Self-hosting", link: "/docs/production/self-hosting" },
+      ],
+    },
+    {
+      text: "Guides",
+      items: [
+        { text: "Factory pattern", link: "/docs/guides/factory" },
+        { text: "Call traces", link: "/docs/guides/call-traces" },
+        { text: "Transaction receipts", link: "/docs/guides/receipts" },
+        { text: "Time-series data", link: "/docs/guides/time-series" },
+        { text: "Foundry", link: "/docs/guides/foundry" },
+      ],
+    },
+    {
+      text: "API reference",
+      items: [
+        { text: "create-ponder", link: "/docs/api-reference/create-ponder" },
+        {
+          text: "ponder",
+          items: [
+            {
+              text: "CLI (dev, start, serve)",
+              link: "/docs/api-reference/ponder/cli",
+            },
+            {
+              text: "ponder.config.ts",
+              link: "/docs/api-reference/ponder/config",
+            },
+            {
+              text: "ponder.schema.ts",
+              link: "/docs/api-reference/ponder/schema",
+            },
+            {
+              text: "Indexing functions",
+              link: "/docs/api-reference/ponder/indexing-functions",
+            },
+            {
+              text: "API endpoints",
+              link: "/docs/api-reference/ponder/api-endpoints",
+            },
+            {
+              text: "Database reference",
+              link: "/docs/api-reference/ponder/database",
+            },
+          ],
+        },
+        { text: "@ponder/client", link: "/docs/api-reference/ponder-client" },
+        { text: "@ponder/react", link: "/docs/api-reference/ponder-react" },
+        { text: "@ponder/utils", link: "/docs/api-reference/ponder-utils" },
+      ],
+    },
+    {
+      text: "Advanced",
+      items: [
+        { text: "Observability", link: "/docs/advanced/observability" },
+        { text: "Telemetry", link: "/docs/advanced/telemetry" },
+      ],
+    },
+  ],
+} satisfies Sidebar;
---
+++ vocs/vocs.config.tsx
@@ -0,0 +1,117 @@
+import { remarkMermaid } from "@theguild/remark-mermaid";
+import { defineConfig } from "vocs";
+import pkg from "../packages/core/package.json";
+import { sidebar } from "./sidebar";
+
+export default defineConfig({
+  title: "Ponder",
+  titleTemplate: "%s – Ponder",
+  description:
+    "Ponder is an open-source backend framework for robust, performant, and maintainable crypto apps.",
+  rootDir: ".",
+  baseUrl:
+    process.env.VERCEL_ENV === "production"
+      ? "https://ponder.sh"
+      : process.env.VERCEL_URL,
+  markdown: { remarkPlugins: [remarkMermaid] },
+  sidebar,
+  topNav: [
+    { text: "Docs", link: "/docs/get-started", match: "/docs" },
+    {
+      text: "Blog",
+      link: "/blog",
+      // items: [
+      //   {
+      //     text: "Introducing Ponder",
+      //     link: "/blog/introducing-ponder",
+      //   },
+      // ],
+    },
+    {
+      text: "Examples",
+      link: "https://github.com/ponder-sh/ponder/tree/main/examples",
+    },
+    {
+      text: pkg.version,
+      items: [
+        {
+          text: "Migration guide",
+          link: "/docs/migration-guide",
+        },
+        {
+          text: "Changelog",
+          link: "https://github.com/ponder-sh/ponder/blob/main/packages/core/CHANGELOG.md",
+        },
+        {
+          text: "Contribute",
+          link: "https://github.com/ponder-sh/ponder/blob/main/.github/CONTRIBUTING.md",
+        },
+      ],
+    },
+  ],
+  head() {
+    return (
+      <>
+        <link rel="preload" as="image" href="/hero.png" />
+        <script async src="https://sa-api.ponder.sh/latest.js" />
+        <noscript>
+          <img
+            src="https://sa-api.ponder.sh/noscript.gif"
+            alt=""
+            referrerPolicy="no-referrer-when-downgrade"
+          />
+        </noscript>
+      </>
+    );
+  },
+  // ogImageUrl: {
+  //   '/': '/og-image.png',
+  //   '/docs':
+  //     'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
+  //   '/op-stack':
+  //     'https://vocs.dev/api/og?logo=%logo&title=%title&description=%description',
+  // },
+  // iconUrl: { light: '/favicons/light.png', dark: '/favicons/dark.png' },
+  logoUrl: { light: "/ponder-light.svg", dark: "/ponder-dark.svg" },
+  socials: [
+    {
+      icon: "github",
+      link: "https://github.com/ponder-sh/ponder",
+    },
+    {
+      icon: "telegram",
+      link: "https://t.me/ponder_sh",
+    },
+    {
+      icon: "x",
+      link: "https://x.com/ponder_sh",
+    },
+    // {
+    //   icon: "warpcast",
+    //   link: "https://warpcast.com/~/channel/ponder-sh",
+    // },
+  ],
+  editLink: {
+    pattern: "https://github.com/ponder-sh/ponder/edit/main/vocs/pages/:path",
+    text: "Suggest changes",
+  },
+  theme: {
+    accentColor: {
+      light: "#0a9fb2",
+      dark: "#10c2d5",
+    },
+    variables: {
+      content: {
+        width: "calc(70ch + (var(--vocs-content_horizontalPadding) * 2))",
+      },
+      fontSize: {
+        codeBlock: "13px", // Default: 14px
+      },
+    },
+  },
+});
+
+function toPatchVersionRange(version: string) {
+  const [major, minor] = version.split(".").slice(0, 2);
+  return `${major}.${minor}.x`;
+}
