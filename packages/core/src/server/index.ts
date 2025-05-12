c716713c71fca50b9f3529298588e9e587ac52a4
Kyle Scott <kyscott@umich.edu>
2025-05-11 20:27:17 -0400

    undo status updates

--- packages/core/src/server/index.ts
+++ packages/core/src/server/index.ts
@@ -94,9 +94,8 @@ export async function createServer({
     .get("/status", async (c) => {
       const checkpoints = await globalThis.PONDER_DATABASE.getCheckpoints();
       const status: Status = {};
-      for (const { chainName, chainId, latestCheckpoint } of checkpoints) {
+      for (const { chainName, latestCheckpoint } of checkpoints) {
         status[chainName] = {
-          chainId,
           block: {
             number: Number(decodeCheckpoint(latestCheckpoint).blockNumber),
             timestamp: Number(
