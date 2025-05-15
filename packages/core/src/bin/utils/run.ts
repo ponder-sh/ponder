d27354ac3c39aa304fdf542899d780409475c430
Kyle Scott <kyscott@umich.edu>
2025-05-15 12:06:18 -0400

    fix empty checkpoints

--- packages/core/src/bin/utils/run.ts
+++ packages/core/src/bin/utils/run.ts
@@ -458,6 +458,8 @@ export async function run({
         }
 
         await database.wrap({ method: "setCheckpoints" }, async () => {
+          if (event.checkpoints.length === 0) return;
+
           await database.qb.drizzle
             .insert(database.PONDER_CHECKPOINT)
             .values(
