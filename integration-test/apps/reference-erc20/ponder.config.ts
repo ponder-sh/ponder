0c2a554b096e00f89575436202cc1f58fd8fd954
Kyle Scott <kyscott@umich.edu>
2025-05-17 19:34:48 -0400

    .

--- integration-test/apps/reference-erc20/ponder.config.ts
+++ integration-test/apps/reference-erc20/ponder.config.ts
@@ -3,10 +3,7 @@ import { erc20ABI } from "./abis/erc20ABI";
 
 export default createConfig({
   chains: {
-    mainnet: {
-      id: 1,
-      rpc: "https://eth-mainnet.g.alchemy.com/v2/SF8Vq82Y1rHsKx3N3T8sVfyd-CNH3Va8",
-    },
+    mainnet: { id: 1, rpc: process.env.PONDER_RPC_URL_1 },
   },
   contracts: {
     ERC20: {
