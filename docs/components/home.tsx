1abda9944572d6042e6c5094a262a7932fc9bae5
Kevin <90073088+typedarray@users.noreply.github.com>
2025-06-16 10:17:31 -0400

    marble announcement (#1829)
    * banner
    
    * spacing
    
    * marble page
    
    ---------
    
    Co-authored-by: typedarray <90073088+0xOlias@users.noreply.github.com>

--- docs/components/home.tsx
+++ docs/components/home.tsx
@@ -16,10 +16,10 @@ export function Home() {
   return (
     <>
       <main className="w-full max-w-full relative">
-        <div className="max-w-[1180px] mx-auto md:mt-6">
+        <div className="max-w-[1180px] mx-auto md:mt-4">
           <div className="w-full flex flex-col justify-between md:flex-row gap-8 p-4 mb-6 md:p-8 rounded-lg shadow-sm hero-texture">
             <div className="flex flex-col items-start space-y-1.5 max-w-[540px] text-neutral-950 dark:text-neutral-50">
-              <p className="pb-4">Introducing Ponder</p>
+              <div className="h-5"/>
               <h1 className="text-4xl font-semibold tracking-tight pb-4">
                 Rapid custom indexing for any EVM smart contract
               </h1>
