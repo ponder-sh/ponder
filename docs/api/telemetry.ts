c10c6774273ddcff173cc6ea052eff6b2faf3989
Kevin <90073088+typedarray@users.noreply.github.com>
2025-06-23 16:46:30 -0400

    Revert "update telemetry (#1843)"
    This reverts commit 7cc739ba3897c328c0b4291c3c3aa1a3bcb417b4.

--- docs/api/telemetry.ts
+++ docs/api/telemetry.ts
@@ -1,4 +1,3 @@
-import { createClient } from "@clickhouse/client"
 import { Analytics, type TrackParams } from "@segment/analytics-node";
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { PostHog } from "posthog-node";
@@ -9,8 +8,6 @@ if (!process.env.POSTHOG_PROJECT_API_KEY)
   throw new Error(
     'Missing required environment variable "POSTHOG_PROJECT_API_KEY".',
   );
-if (!process.env.CLICKHOUSE_URL)
-  throw new Error('Missing required environment variable "CLICKHOUSE_URL".');
 
 const analytics = new Analytics({
   writeKey: process.env.SEGMENT_WRITE_KEY,
@@ -26,8 +23,6 @@ const client = new PostHog(process.env.POSTHOG_PROJECT_API_KEY, {
   flushAt: 1,
   flushInterval: 0,
 });
-
-const clickhouse = createClient({ url: process.env.CLICKHOUSE_URL })
 
 const asyncTrack = (payload: TrackParams) => {
   return new Promise<void>((resolve, reject) => {
@@ -60,54 +55,6 @@ export default async function forwardTelemetry(
     client.on("error", handleError);
     client.capture(body);
     await client.shutdown();
-
-    if (body.event === "lifecycle:heartbeat_send") {
-      await clickhouse.exec({
-        query: `
-    INSERT INTO telemetry.telemetry_heartbeat 
-      (timestamp, project_id, session_id, device_id, duration) VALUES
-      (
-        now(), 
-        reinterpretAsUInt64(unhex({project_id:String})), 
-        reinterpretAsUInt64(unhex({session_id:String})),
-        reinterpretAsUInt64(unhex({device_id:String})),
-        {duration:UInt32}
-      )`,
-        query_params: {
-          project_id: body.properties.project_id, 
-          session_id: body.properties.session_id,
-          device_id: body.distinctId,
-          duration: Math.round(+body.properties.duration_seconds),
-        },
-        clickhouse_settings:{
-          "async_insert": 1,
-          "async_insert_busy_timeout_ms": 30_000,
-        },
-      }).catch(handleError);
-    } else if (body.event === "lifecycle:session_start") {
-      await clickhouse.exec({
-        query: `
-    INSERT INTO telemetry.telemetry_heartbeat 
-      (timestamp, project_id, session_id, device_id, duration) VALUES
-      (
-        now(), 
-        reinterpretAsUInt64(unhex({project_id:String})), 
-        reinterpretAsUInt64(unhex({session_id:String})),
-        reinterpretAsUInt64(unhex({device_id:String})),
-        {duration:UInt32}
-      )`,
-        query_params: {
-          project_id: body.properties.project_id, 
-          session_id: body.properties.session_id,
-          device_id: body.distinctId,
-          duration: 0,
-        },
-        clickhouse_settings:{
-          "async_insert": 1,
-          "async_insert_busy_timeout_ms": 30_000,
-        },
-      }).catch(handleError);
-    }
   }
   // Otherwise, assume it's a Segment event
   else {
