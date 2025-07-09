27e4780159e779bb1784885b2a63a34a029321c7
Kyle Scott <kyscott@umich.edu>
2025-07-09 18:45:14 -0400

    retries

--- packages/core/src/database/queryBuilder.ts
+++ packages/core/src/database/queryBuilder.ts
@@ -1,12 +1,30 @@
+import type { Common } from "@/internal/common.js";
+import {
+  BigIntSerializationError,
+  CheckConstraintError,
+  NonRetryableError,
+  NotNullConstraintError,
+  ShutdownError,
+  TransactionError,
+  UniqueConstraintError,
+  getBaseError,
+} from "@/internal/errors.js";
 import type { Schema } from "@/internal/types.js";
 import type { Drizzle } from "@/types/db.js";
+// import { startClock } from "@/utils/timer.js";
+import { wait } from "@/utils/wait.js";
 import type { PGlite } from "@electric-sql/pglite";
-import type {
-  PgDatabase,
-  PgQueryResultHKT,
-  PgTransactionConfig,
+import {
+  type PgDatabase,
+  PgDialect,
+  type PgQueryResultHKT,
+  type PgTransactionConfig,
 } from "drizzle-orm/pg-core";
 import pg from "pg";
+
+const RETRY_COUNT = 9;
+const BASE_DURATION = 125;
+const SQL_LENGTH_LIMIT = 50;
 
 /**
  * Query builder with built-in retry logic, logging, and metrics.
@@ -28,13 +46,36 @@ export type QB<
     | { $dialect: "postgres"; $client: pg.Pool | pg.PoolClient }
   );
 
+export const parseSqlError = (e: any): Error => {
+  let error = getBaseError(e);
+
+  if (error?.message?.includes("violates not-null constraint")) {
+    error = new NotNullConstraintError(error.message);
+  } else if (error?.message?.includes("violates unique constraint")) {
+    error = new UniqueConstraintError(error.message);
+  } else if (error?.message?.includes("violates check constraint")) {
+    error = new CheckConstraintError(error.message);
+  } else if (
+    error?.message?.includes("Do not know how to serialize a BigInt")
+  ) {
+    error = new BigIntSerializationError(error.message);
+    error.meta.push(
+      "Hint:\n  The JSON column type does not support BigInt values. Use the replaceBigInts() helper function before inserting into the database. Docs: https://ponder.sh/docs/api-reference/ponder-utils#replacebigints",
+    );
+  } else if (error?.message?.includes("does not exist")) {
+    error = new NonRetryableError(error.message);
+  }
+
+  return error;
+};
+
 /**
  * Create a query builder.
  *
  * @example
  * ```ts
- * const qb = createQB(common, drizzle(pool, { casing: "snake_case" }));
- * const result = await qb.label("test").select().from(accounts);
+ * const qb = createQB(drizzle(pool, { casing: "snake_case" }));
+ * const result = await qb.select().from(accounts);
  * ```
  */
 export const createQB = <
@@ -45,7 +86,108 @@ export const createQB = <
     | pg.PoolClient,
 >(
   db: PgDatabase<PgQueryResultHKT, TSchema> & { $client: TClient },
+  { common, isAdmin }: { common: Common; isAdmin: boolean },
 ): QB<TSchema, TClient> => {
+  const dialect = new PgDialect({ casing: "snake_case" });
+  const isClient = db.$client instanceof pg.Client;
+
+  const wrap = async <T>(fn: () => Promise<T>, sql: string): Promise<T> => {
+    // First error thrown is often the most useful
+    let firstError: any;
+    let hasError = false;
+
+    for (let i = 0; i <= RETRY_COUNT; i++) {
+      // const endClock = startClock();
+      const id = crypto.randomUUID().slice(0, 8);
+
+      // if (label) {
+      //   common.logger.trace({
+      //     service: "database",
+      //     msg: `Started '${label}' database method (id=${id})`,
+      //   });
+      // }
+
+      try {
+        if (common.shutdown.isKilled && isAdmin === false) {
+          throw new ShutdownError();
+        }
+
+        const result = await fn();
+        // if (label) {
+        //   common.metrics.ponder_database_method_duration.observe(
+        //     { method: label },
+        //     endClock(),
+        //   );
+        // }
+
+        if (common.shutdown.isKilled && isAdmin === false) {
+          throw new ShutdownError();
+        }
+
+        return result;
+      } catch (e) {
+        const error = parseSqlError(e);
+
+        if (common.shutdown.isKilled) {
+          throw new ShutdownError();
+        }
+
+        // if (label) {
+        //   common.metrics.ponder_database_method_duration.observe(
+        //     { method: label },
+        //     endClock(),
+        //   );
+        //   common.metrics.ponder_database_method_error_total.inc({
+        //     method: label,
+        //   });
+        // }
+
+        if (!hasError) {
+          hasError = true;
+          firstError = error;
+        }
+
+        if (
+          error instanceof NonRetryableError &&
+          error instanceof TransactionError === false
+        ) {
+          common.logger.warn({
+            service: "database",
+            msg: `Failed '${sql}' database query (id=${id})`,
+            error,
+          });
+          throw error;
+        }
+
+        if (i === RETRY_COUNT) {
+          common.logger.warn({
+            service: "database",
+            msg: `Failed '${sql}' database query after '${i + 1}' attempts (id=${id})`,
+            error,
+          });
+          throw firstError;
+        }
+
+        const duration = BASE_DURATION * 2 ** i;
+        common.logger.debug({
+          service: "database",
+          msg: `Failed '${sql}' database query, retrying after ${duration} milliseconds (id=${id})`,
+          error,
+        });
+        await wait(duration);
+      } finally {
+        // if (label) {
+        //   common.logger.trace({
+        //     service: "database",
+        //     msg: `Completed '${label}' database method in ${Math.round(endClock())}ms (id=${id})`,
+        //   });
+        // }
+      }
+    }
+
+    throw "unreachable";
+  };
+
   const assignClient = (qb: QB<TSchema, TClient>, client: TClient) => {
     if (client instanceof pg.Pool || client instanceof pg.Client) {
       Object.assign(qb, { $dialect: "postgres" });
@@ -71,8 +213,76 @@ export const createQB = <
     };
   };
 
-  wrapTx(db);
+  // non-transaction queries (retryable)
 
+  const execute = db._.session.execute.bind(db._.session);
+  db._.session.execute = async (...args) => {
+    return wrap(
+      () => execute(...args),
+      dialect.sqlToQuery(args[0]).sql.slice(0, SQL_LENGTH_LIMIT),
+    );
+  };
+
+  const prepareQuery = db._.session.prepareQuery.bind(db._.session);
+  db._.session.prepareQuery = (...args) => {
+    const result = prepareQuery(...args);
+    const execute = result.execute.bind(result);
+    result.execute = async (..._args) => {
+      return wrap(
+        () => execute(..._args),
+        args[0].sql.slice(0, SQL_LENGTH_LIMIT),
+      );
+    };
+    return result;
+  };
+
+  // transaction queries (non-retryable)
+
+  const transaction = db._.session.transaction.bind(db._.session);
+  db._.session.transaction = async (...args) => {
+    const callback = args[0];
+    args[0] = async (..._args) => {
+      const tx = _args[0] as PgDatabase<PgQueryResultHKT, TSchema>;
+      const txExecute = isClient
+        ? execute
+        : tx._.session.execute.bind(tx._.session);
+      // @ts-expect-error
+      tx._.session.execute = async (...args) => {
+        return wrap(
+          () =>
+            txExecute(...args).catch((error) => {
+              throw new TransactionError(error.message);
+            }),
+          dialect.sqlToQuery(args[0]).sql.slice(0, SQL_LENGTH_LIMIT),
+        );
+      };
+
+      const txPrepareQuery = isClient
+        ? prepareQuery
+        : tx._.session.prepareQuery.bind(tx._.session);
+      // @ts-ignore
+      tx._.session.prepareQuery = (...args) => {
+        const result = txPrepareQuery(...args);
+        const execute = result.execute.bind(result);
+        result.execute = async (..._args) => {
+          return wrap(
+            () =>
+              execute(..._args).catch((error) => {
+                throw new TransactionError(error.message);
+              }),
+            args[0].sql.slice(0, SQL_LENGTH_LIMIT),
+          );
+        };
+        return result;
+      };
+
+      return callback(..._args);
+    };
+
+    return wrap(() => transaction(...args), "begin");
+  };
+
+  wrapTx(db);
   assignClient(db as unknown as QB<TSchema, TClient>, db.$client);
 
   return db as unknown as QB<TSchema, TClient>;
