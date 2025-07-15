3030465b7926fc32efd180a8c33f878515c5bc16
Kyle Scott <kyscott@umich.edu>
2025-07-15 14:33:32 -0400

    fix dev server error

--- packages/core/src/drizzle/kit/index.ts
+++ packages/core/src/drizzle/kit/index.ts
@@ -52,7 +52,9 @@ export const getReorgTable = <config extends TableConfig>(
         operation_id: integer()
           .notNull()
           .primaryKey()
-          .default(sql.raw(`nextval('${SHARED_OPERATION_ID_SEQUENCE}')`)),
+          .default(
+            sql.raw(`nextval('"${schema}"."${SHARED_OPERATION_ID_SEQUENCE}"')`),
+          ),
         operation: integer().notNull().$type<0 | 1 | 2>(),
         checkpoint: varchar({ length: 75 }).notNull(),
       },
@@ -65,7 +67,7 @@ export const getReorgTable = <config extends TableConfig>(
       operation_id: integer()
         .notNull()
         .primaryKey()
-        .default(sql.raw(`nextval('${SHARED_OPERATION_ID_SEQUENCE}')`)),
+        .default(sql.raw(`nextval('"${SHARED_OPERATION_ID_SEQUENCE}"')`)),
       operation: integer().notNull().$type<0 | 1 | 2>(),
       checkpoint: varchar({ length: 75 }).notNull(),
     },
