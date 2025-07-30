75885957617aeadc11f1c42209f88bab2f360fcf
typedarray <90073088+0xOlias@users.noreply.github.com>
2025-07-30 18:35:39 -0400

    faster

--- examples/feature-multichain/src/index.ts
+++ examples/feature-multichain/src/index.ts
@@ -2,7 +2,7 @@ import { ponder } from "ponder:registry";
 import { account } from "ponder:schema";
 
 ponder.on("weth9:Deposit", async ({ event, context }) => {
-  await new Promise((resolve) => setTimeout(resolve, 200));
+  await new Promise((resolve) => setTimeout(resolve, 3));
 
   await context.db
     .insert(account)
