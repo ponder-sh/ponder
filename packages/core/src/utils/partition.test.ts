610075ed233a4cc336b0738bd230eb86497ffcb8
Kyle Scott <kyscott@umich.edu>
2025-08-06 17:36:15 -0400

    simplify partition

--- packages/core/src/utils/partition.test.ts
+++ packages/core/src/utils/partition.test.ts
@@ -13,4 +13,16 @@ test("partition", () => {
   [left, right] = partition([2, 5], (n) => n <= 5);
   expect(left).toStrictEqual([2, 5]);
   expect(right).toStrictEqual([]);
+
+  [left, right] = partition([1], (n) => n > 1);
+  expect(left).toStrictEqual([]);
+  expect(right).toStrictEqual([1]);
+
+  [left, right] = partition([1, 2, 3], (n) => n > 0);
+  expect(left).toStrictEqual([1, 2, 3]);
+  expect(right).toStrictEqual([]);
+
+  [left, right] = partition([1, 2, 3], (n) => n > 5);
+  expect(left).toStrictEqual([]);
+  expect(right).toStrictEqual([1, 2, 3]);
 });
