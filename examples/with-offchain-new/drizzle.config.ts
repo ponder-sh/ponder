58816fb5de9283a4f5ad105aacde94b6977fd57c
typedarray <90073088+0xOlias@users.noreply.github.com>
2025-08-29 16:54:36 -0400

    generate migrations

--- examples/with-offchain-new/drizzle.config.ts
+++ examples/with-offchain-new/drizzle.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./migrations",
-  schema: "./schemas/offchain.ts",
+  schema: "./schemas/offchain.schema.ts",
   dialect: "postgresql",
 });
---
+++ examples/with-offchain-new/migrations/0000_noisy_fallen_one.sql
@@ -0,0 +1,4 @@
+CREATE TABLE "offchain"."metadata" (
+	"address" text PRIMARY KEY NOT NULL,
+	"graffiti" text
+);
