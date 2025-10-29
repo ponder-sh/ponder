5c75e46a2fe8b6e53f8ea5dbe652255de7267f35
typedarray <90073088+0xOlias@users.noreply.github.com>
2025-10-29 12:21:27 -0400

    fix: merkle eth_getLogs range

--- packages/utils/src/_test/merkle.test.ts
+++ packages/utils/src/_test/merkle.test.ts
@@ -4,16 +4,17 @@ import { getLogsRetryHelper } from "../getLogsRetryHelper.js";
 import { type Params, UNI, WETH, fromBlock, getRequest } from "./utils.js";
 
 const request = getRequest("https://eth.merkle.io");
-const maxBlockRange = 10_000n;
+const maxBlockRange10k = 10_000n;
+const maxBlockRange1k = 1_000n;
 
-test("merkle success", async () => {
+test("merkle success 10k", async () => {
   const logs = await request({
     method: "eth_getLogs",
     params: [
       {
         address: UNI,
         fromBlock: numberToHex(fromBlock),
-        toBlock: numberToHex(fromBlock + maxBlockRange),
+        toBlock: numberToHex(fromBlock + maxBlockRange10k),
       },
     ],
   });
@@ -21,12 +22,12 @@ test("merkle success", async () => {
   expect(logs).toHaveLength(49);
 });
 
-test("merkle block range", async () => {
+test("merkle block range 10k", async () => {
   const params: Params = [
     {
       address: WETH,
       fromBlock: numberToHex(fromBlock),
-      toBlock: numberToHex(fromBlock + maxBlockRange + 1n),
+      toBlock: numberToHex(fromBlock + maxBlockRange10k + 1n),
     },
   ];
 
@@ -52,11 +53,67 @@ test("merkle block range", async () => {
     ranges: [
       {
         fromBlock: numberToHex(fromBlock),
-        toBlock: numberToHex(fromBlock + maxBlockRange),
+        toBlock: numberToHex(fromBlock + maxBlockRange10k),
       },
       {
-        fromBlock: numberToHex(fromBlock + maxBlockRange + 1n),
-        toBlock: numberToHex(fromBlock + maxBlockRange + 1n),
+        fromBlock: numberToHex(fromBlock + maxBlockRange10k + 1n),
+        toBlock: numberToHex(fromBlock + maxBlockRange10k + 1n),
+      },
+    ],
+  });
+});
+
+test("merkle success 10k", async () => {
+  const logs = await request({
+    method: "eth_getLogs",
+    params: [
+      {
+        address: UNI,
+        fromBlock: numberToHex(fromBlock),
+        toBlock: numberToHex(fromBlock + maxBlockRange1k),
+      },
+    ],
+  });
+
+  expect(logs).toHaveLength(49);
+});
+
+test("merkle block range 1k", async () => {
+  const params: Params = [
+    {
+      address: WETH,
+      fromBlock: numberToHex(fromBlock),
+      toBlock: numberToHex(fromBlock + maxBlockRange1k + 1n),
+    },
+  ];
+
+  const error = await request({
+    method: "eth_getLogs",
+    params,
+  }).catch((error) => error);
+
+  expect(error).toBeInstanceOf(RpcError);
+  expect(JSON.stringify(error)).includes(
+    "eth_getLogs range is too large, max is 1k blocks",
+  );
+
+  const retry = getLogsRetryHelper({
+    params,
+    error,
+  });
+
+  expect(retry.shouldRetry).toBe(true);
+  expect(retry).toStrictEqual({
+    isSuggestedRange: true,
+    shouldRetry: true,
+    ranges: [
+      {
+        fromBlock: numberToHex(fromBlock),
+        toBlock: numberToHex(fromBlock + maxBlockRange1k),
+      },
+      {
+        fromBlock: numberToHex(fromBlock + maxBlockRange1k + 1n),
+        toBlock: numberToHex(fromBlock + maxBlockRange1k + 1n),
       },
     ],
   });
