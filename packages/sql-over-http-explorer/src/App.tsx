3bd0a2ca5c4d1da13033d38e1f8d596d99fce03c
Kyle Scott <kyscott@umich.edu>
2025-11-17 16:53:21 -0500

    add sidebar

--- packages/sql-over-http-explorer/src/App.tsx
+++ packages/sql-over-http-explorer/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import viteLogo from "/vite.svg";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
@@ -8,14 +6,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)} type="button">
--- packages/sql-over-http-explorer/vite.config.ts
+++ packages/sql-over-http-explorer/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
@@ -5,4 +6,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
