4b43534140975029793df4b8846ae20936069810
Kyle Scott <kyscott@umich.edu>
2025-11-18 17:08:12 -0500

    start table viewer

--- packages/sql-over-http-explorer/src/components/table-viewer.tsx
+++ packages/sql-over-http-explorer/src/components/table-viewer.tsx
@@ -1,89 +1,93 @@
 import { useSchema } from "@/hooks/use-schema";
+import { usePonderQuery } from "@ponder/react";
+import {
+  createColumnHelper,
+  flexRender,
+  getCoreRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { useMemo } from "react";
 
 function TableViewer() {
   const selectedTable = useSchema().data?.tables.json[0];
+
+  const tableQuery = usePonderQuery({
+    queryFn: (db) =>
+      db.execute(`SELECT * FROM "${selectedTable?.tableName}" LIMIT 50`),
+    enabled: !!selectedTable,
+    staleTime: Number.POSITIVE_INFINITY,
+  });
+
+  if (selectedTable === undefined) return null;
+
+  const columnHelper = createColumnHelper();
+
+  const columns = useMemo<any>(() => {
+    return selectedTable?.columns.map((column) => {
+      console.log(column.name);
+      return columnHelper.accessor(column.name, {
+        // @ts-ignore
+        cell: (info) => <code className="">{info.getValue().toString()}</code>,
+        header: () => <code className="">{column.name}</code>,
+      });
+    });
+  }, [selectedTable, columnHelper]);
+
+  const table = useReactTable({
+    data: tableQuery.data ?? [],
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+  });
+
   return (
     <div className="grid grid-rows-[56px_1fr]">
       <header className="flex justify-start p-3 text-brand-1 border-b-1 border-brand-2">
-        <button
+        {/* <button
           className="rounded-md border-2 border-brand-2 py-1 px-2 flex items-center"
           type="button"
         >
           Columns
-        </button>
+        </button> */}
       </header>
-      <p>Table Viewer</p>
-      <pre>{JSON.stringify(selectedTable, null, 2)}</pre>
+      <div className="overflow-auto">
+        <table className="table-fixed">
+          <thead className="">
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr className="h-[32px]" key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    className="border-1 border-l-0 border-t-0 border-brand-2 min-w-[200px] text-xs font-semibold text-left px-2"
+                    key={header.id}
+                  >
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody className="">
+            {table.getRowModel().rows.map((row) => (
+              <tr className="" key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td
+                    className="h-[32px] border-1 border-l-0 border-brand-2 min-w-[200px] text-xs text-left px-2"
+                    key={cell.id}
+                  >
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
 
 export default TableViewer;
-
-// type Person = {
-//   firstName: string;
-//   lastName: string;
-//   age: number;
-//   visits: number;
-//   status: string;
-//   progress: number;
-// };
-
-// const defaultData: Person[] = [
-//   {
-//     firstName: "tanner",
-//     lastName: "linsley",
-//     age: 24,
-//     visits: 100,
-//     status: "In Relationship",
-//     progress: 50,
-//   },
-//   {
-//     firstName: "tandy",
-//     lastName: "miller",
-//     age: 40,
-//     visits: 40,
-//     status: "Single",
-//     progress: 80,
-//   },
-//   {
-//     firstName: "joe",
-//     lastName: "dirte",
-//     age: 45,
-//     visits: 20,
-//     status: "Complicated",
-//     progress: 10,
-//   },
-// ];
-
-// const columnHelper = createColumnHelper<Person>()
-
-// const columns = [
-//   columnHelper.accessor('firstName', {
-//     cell: info => info.getValue(),
-//     footer: info => info.column.id,
-//   }),
-//   columnHelper.accessor(row => row.lastName, {
-//     id: 'lastName',
-//     cell: info => <i>{info.getValue()}</i>,
-//     header: () => <span>Last Name</span>,
-//     footer: info => info.column.id,
-//   }),
-//   columnHelper.accessor('age', {
-//     header: () => 'Age',
-//     cell: info => info.renderValue(),
-//     footer: info => info.column.id,
-//   }),
-//   columnHelper.accessor('visits', {
-//     header: () => <span>Visits</span>,
-//     footer: info => info.column.id,
-//   }),
-//   columnHelper.accessor('status', {
-//     header: 'Status',
-//     footer: info => info.column.id,
-//   }),
-//   columnHelper.accessor('progress', {
-//     header: 'Profile Progress',
-//     footer: info => info.column.id,
-//   }),
-// ]
