3d3aeef2d156a382c21a689bbfe75ab68fb2a918
Kyle Scott <kyscott@umich.edu>
2025-11-18 22:23:38 -0500

    remove unused deps

--- packages/sql-over-http-explorer/src/lib/utils.ts
+++ packages/sql-over-http-explorer/src/lib/utils.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
-
 /** Get the URL of the server. */
 export function getServerUrl() {
   if (process.env.NODE_ENV === "development") {
