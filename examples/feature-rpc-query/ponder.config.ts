e3f8bcd6f7c4a6eefb40bb3e8f3bf714dc23d935
kyscott18 <43524469+kyscott18@users.noreply.github.com>
2026-08-04 22:23:19 +0000

    fix: support incomplete RPC query responses

--- examples/feature-rpc-query/ponder.config.ts
+++ examples/feature-rpc-query/ponder.config.ts
@@ -1,11 +1,28 @@
 import { createConfig } from "ponder";
-import { parseAbi } from "viem";
+import { custom, http, parseAbi } from "viem";
+
+const queryTransport = http(process.env.PONDER_RPC_URL_QUERY_143, {
+  retryCount: 0,
+})({});
+const standardTransport = http(process.env.PONDER_RPC_URL_143, {
+  retryCount: 0,
+})({});
+
+const monadTransport = custom({
+  request(request) {
+    const transport = request.method.startsWith("eth_query")
+      ? queryTransport
+      : standardTransport;
+
+    return transport.request(request);
+  },
+});
 
 export default createConfig({
   chains: {
     monad: {
       id: 143,
-      rpc: process.env.PONDER_RPC_URL_143,
+      rpc: monadTransport,
       experimental_rpcQuery: true,
     },
   },
@@ -16,7 +33,7 @@ export default createConfig({
         "event Transfer(address indexed from, address indexed to, uint256 amount)",
       ]),
       address: "0x754704Bc059F8C67012fEd69BC8A327a5aafb603",
-      startBlock: 93_270_922,
+      startBlock: 92_270_922,
       includeCallTraces: true,
     },
   },
@@ -24,7 +41,7 @@ export default createConfig({
     USDCMinter: {
       chain: "monad",
       address: "0xfd78ee919681417d192449715b2594ab58f5d002",
-      startBlock: 93_270_922,
+      startBlock: 92_270_922,
     },
   },
 });
