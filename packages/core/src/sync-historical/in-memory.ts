a97176b8ab00ca53af666f90fcd21b963e22b877
kyscott18 <43524469+kyscott18@users.noreply.github.com>
2026-09-16 23:27:09 +0000

    scaffold in memory sync

--- packages/core/src/sync-historical/in-memory.ts
+++ packages/core/src/sync-historical/in-memory.ts
@@ -1,18 +1,56 @@
+import { type Hash, numberToHex, toHex, zeroHash } from "viem";
 import type { Common } from "@/internal/common.js";
-import type { Chain } from "@/internal/types.js";
+import type {
+  Chain,
+  SyncBlock,
+  SyncLog,
+  SyncTrace,
+  SyncTransaction,
+  SyncTransactionReceipt,
+} from "@/internal/types.js";
+import {
+  debug_traceBlockByNumber,
+  eth_getBlockByNumber,
+  eth_getBlockReceipts,
+  eth_getTransactionReceipt,
+  validateLogsAndBlock,
+  validateReceiptsAndBlock,
+  validateTracesAndBlock,
+  validateTransactionsAndBlock,
+} from "@/rpc/actions.js";
 import type { Rpc } from "@/rpc/index.js";
+import {
+  syncBlockToInternal,
+  syncLogToInternal,
+  syncTraceToInternal,
+  syncTransactionReceiptToInternal,
+  syncTransactionToInternal,
+} from "@/runtime/events.js";
+import {
+  isAddressFactory,
+  isAddressMatched,
+  isBlockFilterMatched,
+  isBlockInFilter,
+  isLogFilterMatched,
+  isTraceFilterMatched,
+  isTransactionFilterMatched,
+  isTransferFilterMatched,
+} from "@/runtime/filter.js";
 import type {
   ChildAddresses,
   IntervalWithFactory,
   IntervalWithFilter,
 } from "@/runtime/index.js";
 import type { SyncStore } from "@/sync-store/index.js";
+import type { Interval } from "@/utils/interval.js";
+
+type BlockData = Awaited<ReturnType<SyncStore["getEventData"]>>;
 
 export type InMemoryHistoricalSync = {
   syncBlockData(params: {
     requiredIntervals: IntervalWithFilter[];
     requiredFactoryIntervals: IntervalWithFactory[];
-  }): AsyncGenerator<Awaited<ReturnType<SyncStore["getEventData"]>>>;
+  }): AsyncGenerator<BlockData>;
 };
 
 export function createInMemoryHistoricalSync(params: {
@@ -21,7 +59,496 @@ export function createInMemoryHistoricalSync(params: {
   rpc: Rpc;
   childAddress: ChildAddresses;
 }): InMemoryHistoricalSync {
-  return {
-    async *syncBlockData() {},
+  let isBlockReceipts = true;
+
+  const syncTransactionReceipts = async (
+    block: SyncBlock,
+    transactionHashes: Set<Hash>,
+    context?: Parameters<Rpc["request"]>[1],
+  ): Promise<SyncTransactionReceipt[]> => {
+    if (transactionHashes.size === 0) {
+      return [];
+    }
+
+    if (isBlockReceipts === false) {
+      const transactionReceipts = await Promise.all(
+        Array.from(transactionHashes).map(async (hash) => {
+          const receipt = await eth_getTransactionReceipt(
+            params.rpc,
+            [hash],
+            context,
+          );
+
+          validateReceiptsAndBlock(
+            [receipt],
+            block,
+            {
+              method: "eth_getTransactionReceipt",
+              params: [hash],
+            },
+            {
+              method: "eth_getBlockByNumber",
+              params: [block.number, true],
+            },
+          );
+
+          return receipt;
+        }),
+      );
+
+      return transactionReceipts;
+    }
+
+    let blockReceipts: SyncTransactionReceipt[];
+    try {
+      blockReceipts = await eth_getBlockReceipts(
+        params.rpc,
+        [block.hash],
+        context,
+      );
+    } catch (_error) {
+      const error = _error as Error;
+      params.common.logger.warn({
+        msg: "Caught eth_getBlockReceipts error, switching to eth_getTransactionReceipt method",
+        action: "fetch_block_data",
+        chain: params.chain.name,
+        chain_id: params.chain.id,
+        error,
+      });
+
+      isBlockReceipts = false;
+      return syncTransactionReceipts(block, transactionHashes, context);
+    }
+
+    validateReceiptsAndBlock(
+      blockReceipts,
+      block,
+      {
+        method: "eth_getBlockReceipts",
+        params: [block.hash],
+      },
+      {
+        method: "eth_getBlockByNumber",
+        params: [block.number, true],
+      },
+    );
+
+    const transactionReceipts = blockReceipts.filter((receipt) =>
+      transactionHashes.has(receipt.transactionHash),
+    );
+
+    return transactionReceipts;
   };
+
+  return {
+    async *syncBlockData({ requiredIntervals }) {
+      const context = {
+        logger: params.common.logger.child({ action: "fetch_block_data" }),
+      };
+      // TODO(kyle) factory progress
+
+      // TODO(kyle) filter progress
+
+      const perBlockLogs = new Map<number, SyncLog[]>();
+
+      const filterGenerators = new Map<
+        IntervalWithFilter,
+        AsyncGenerator<Interval>
+      >();
+
+      const syncBlock = async (blockNumber: number): Promise<BlockData> => {
+        const filters = requiredIntervals
+          .filter(
+            ({ interval }) =>
+              interval[0] <= blockNumber && blockNumber <= interval[1],
+          )
+          .map(({ filter }) => filter);
+        const blockFilters = filters.filter(
+          (filter) => filter.type === "block",
+        );
+        const transactionFilters = filters.filter(
+          (filter) => filter.type === "transaction",
+        );
+        const traceFilters = filters.filter(
+          (filter) => filter.type === "trace",
+        );
+        const transferFilters = filters.filter(
+          (filter) => filter.type === "transfer",
+        );
+        const logFilters = filters.filter((filter) => filter.type === "log");
+        let block: SyncBlock | undefined;
+
+        const requiredTransactions = new Set<Hash>();
+        const requiredTransactionReceipts = new Set<Hash>();
+
+        ////////
+        // Logs
+        ////////
+
+        perBlockLogs.delete(blockNumber);
+        let logs: SyncLog[] = [];
+        if (perBlockLogs.has(blockNumber)) {
+          block = await eth_getBlockByNumber(
+            params.rpc,
+            [numberToHex(blockNumber), true],
+            context,
+          );
+
+          logs = perBlockLogs.get(blockNumber)!.filter((log) => {
+            let isMatched = false;
+
+            for (const filter of logFilters) {
+              if (
+                isLogFilterMatched({ filter, log }) &&
+                (isAddressFactory(filter.address)
+                  ? isAddressMatched({
+                      address: log.address,
+                      blockNumber,
+                      childAddresses: params.childAddress.get(
+                        filter.address.id,
+                      )!,
+                    })
+                  : true)
+              ) {
+                isMatched = true;
+
+                if (log.transactionHash !== zeroHash) {
+                  requiredTransactions.add(log.transactionHash);
+                  if (filter.hasTransactionReceipt) {
+                    requiredTransactionReceipts.add(log.transactionHash);
+
+                    // skip to next log
+                    break;
+                  }
+                }
+              }
+            }
+
+            return isMatched;
+          });
+
+          if (logs.length > 0) {
+            // Note: `logsRequest` could be more accurate by tracking the exact
+            // request made to include `address` and `topics`.
+            validateLogsAndBlock(
+              logs,
+              block,
+              {
+                method: "eth_getLogs",
+                params: [
+                  {
+                    fromBlock: toHex(blockNumber),
+                    toBlock: toHex(blockNumber),
+                  },
+                ],
+              },
+              {
+                method: "eth_getBlockByNumber",
+                params: [toHex(blockNumber), true],
+              },
+            );
+          }
+        }
+
+        ////////
+        // Traces
+        ////////
+
+        const shouldRequestTraces =
+          traceFilters.some((filter) => isBlockInFilter(filter, blockNumber)) ||
+          transferFilters.some((filter) =>
+            isBlockInFilter(filter, blockNumber),
+          );
+
+        let traces: SyncTrace[] = [];
+        if (shouldRequestTraces) {
+          if (block === undefined) {
+            [block, traces] = await Promise.all([
+              eth_getBlockByNumber(
+                params.rpc,
+                [numberToHex(blockNumber), true],
+                context,
+              ),
+              debug_traceBlockByNumber(
+                params.rpc,
+                [numberToHex(blockNumber), { tracer: "callTracer" }],
+                context,
+              ),
+            ]);
+          } else {
+            traces = await debug_traceBlockByNumber(
+              params.rpc,
+              [numberToHex(blockNumber), { tracer: "callTracer" }],
+              context,
+            );
+          }
+
+          traces = traces.filter((trace) => {
+            let isMatched = false;
+            for (const filter of transferFilters) {
+              if (
+                isTransferFilterMatched({
+                  filter,
+                  trace: trace.trace,
+                  block: { number: BigInt(blockNumber) },
+                }) &&
+                (isAddressFactory(filter.fromAddress)
+                  ? isAddressMatched({
+                      address: trace.trace.from,
+                      blockNumber,
+                      childAddresses: params.childAddress.get(
+                        filter.fromAddress.id,
+                      )!,
+                    })
+                  : true) &&
+                (isAddressFactory(filter.toAddress)
+                  ? isAddressMatched({
+                      address: trace.trace.to,
+                      blockNumber,
+                      childAddresses: params.childAddress.get(
+                        filter.toAddress.id,
+                      )!,
+                    })
+                  : true)
+              ) {
+                isMatched = true;
+                requiredTransactions.add(trace.transactionHash);
+                if (filter.hasTransactionReceipt) {
+                  requiredTransactionReceipts.add(trace.transactionHash);
+                  // skip to next trace
+                  break;
+                }
+              }
+            }
+
+            for (const filter of traceFilters) {
+              if (
+                isTraceFilterMatched({
+                  filter,
+                  trace: trace.trace,
+                  block: { number: BigInt(blockNumber) },
+                }) &&
+                (isAddressFactory(filter.fromAddress)
+                  ? isAddressMatched({
+                      address: trace.trace.from,
+                      blockNumber,
+                      childAddresses: params.childAddress.get(
+                        filter.fromAddress.id,
+                      )!,
+                    })
+                  : true) &&
+                (isAddressFactory(filter.toAddress)
+                  ? isAddressMatched({
+                      address: trace.trace.to,
+                      blockNumber,
+                      childAddresses: params.childAddress.get(
+                        filter.toAddress.id,
+                      )!,
+                    })
+                  : true)
+              ) {
+                isMatched = true;
+                requiredTransactions.add(trace.transactionHash);
+                if (filter.hasTransactionReceipt) {
+                  requiredTransactionReceipts.add(trace.transactionHash);
+                  // skip to next trace
+                  break;
+                }
+              }
+            }
+
+            return isMatched;
+          });
+
+          if (traces.length > 0) {
+            validateTracesAndBlock(
+              traces,
+              block,
+              {
+                method: "debug_traceBlockByNumber",
+                params: [toHex(blockNumber), { tracer: "callTracer" }],
+              },
+              {
+                method: "eth_getBlockByNumber",
+                params: [toHex(blockNumber), true],
+              },
+            );
+          }
+        }
+
+        ////////
+        // Block
+        ////////
+
+        if (
+          block === undefined &&
+          blockFilters.some((filter) =>
+            isBlockFilterMatched({
+              filter,
+              block: { number: BigInt(blockNumber) },
+            }),
+          )
+        ) {
+          block = await eth_getBlockByNumber(
+            params.rpc,
+            [numberToHex(blockNumber), true],
+            context,
+          );
+        }
+
+        ////////
+        // Transactions
+        ////////
+
+        // Return early if no data is fetched
+        if (
+          block === undefined &&
+          transactionFilters.some((filter) =>
+            isBlockInFilter(filter, blockNumber),
+          ) === false
+        ) {
+          return {
+            blocks: [],
+            logs: [],
+            transactions: [],
+            transactionReceipts: [],
+            traces: [],
+            cursor: blockNumber,
+          };
+        }
+
+        if (block === undefined) {
+          block = await eth_getBlockByNumber(
+            params.rpc,
+            [numberToHex(blockNumber), true],
+            context,
+          );
+        }
+
+        const transactions = block.transactions.filter((transaction) => {
+          let isMatched = requiredTransactions.has(transaction.hash);
+          for (const filter of transactionFilters) {
+            if (
+              isTransactionFilterMatched({ filter, transaction }) &&
+              (isAddressFactory(filter.fromAddress)
+                ? isAddressMatched({
+                    address: transaction.from,
+                    blockNumber,
+                    childAddresses: params.childAddress.get(
+                      filter.fromAddress.id,
+                    )!,
+                  })
+                : true) &&
+              (isAddressFactory(filter.toAddress)
+                ? isAddressMatched({
+                    address: transaction.to ?? undefined,
+                    blockNumber,
+                    childAddresses: params.childAddress.get(
+                      filter.toAddress.id,
+                    )!,
+                  })
+                : true)
+            ) {
+              requiredTransactionReceipts.add(transaction.hash);
+              isMatched = true;
+            }
+          }
+          return isMatched;
+        });
+
+        if (transactions.length > 0) {
+          validateTransactionsAndBlock(block, {
+            method: "eth_getBlockByNumber",
+            params: [toHex(blockNumber), true],
+          });
+        }
+
+        const transactionsByHash = new Map<Hash, SyncTransaction>();
+        for (const transaction of transactions) {
+          transactionsByHash.set(transaction.hash, transaction);
+        }
+
+        ////////
+        // Transaction Receipts
+        ////////
+
+        const transactionReceipts = await syncTransactionReceipts(
+          block,
+          requiredTransactionReceipts,
+          context,
+        );
+
+        // TODO(kyle) dedupe logs?
+
+        return {
+          blocks: [syncBlockToInternal({ block })],
+          logs: logs.map((log) => syncLogToInternal({ log })),
+          transactions: transactions.map((transaction) =>
+            syncTransactionToInternal({ transaction }),
+          ),
+          transactionReceipts: transactionReceipts.map((transactionReceipt) =>
+            syncTransactionReceiptToInternal({ transactionReceipt }),
+          ),
+          traces: traces.map((trace) =>
+            syncTraceToInternal({
+              trace,
+              block: block!,
+              transaction: transactionsByHash.get(trace.transactionHash)!,
+            }),
+          ),
+          cursor: blockNumber,
+        };
+      };
+
+      for await (const interval of mergeGeneratorIntervals(filterGenerators)) {
+        // TODO(kyle) queue per block work while recovering order
+        yield* [];
+      }
+    },
+  };
+}
+
+export async function* mergeGeneratorIntervals(
+  filterGenerators: Map<IntervalWithFilter, AsyncGenerator<Interval>>,
+): AsyncGenerator<Interval> {
+  const results = await Promise.all(
+    filterGenerators.values().map((gen) => gen.next()),
+  );
+
+  let cursor = Math.min(
+    ...filterGenerators.keys().map(({ interval }) => interval[0]),
+  );
+
+  while (results.some((res) => res.done !== true)) {
+    const supremum = Math.min(
+      ...results
+        .map((res) => (res.done ? undefined : res.value[1]))
+        .filter((x) => x !== undefined),
+    );
+
+    const minIndices = Array.from(
+      results
+        .entries()
+        .map(([index, result]) => {
+          if (result.done) return undefined;
+          if (result.value[1] === supremum) return index;
+          return undefined;
+        })
+        .filter((x) => x !== undefined),
+    );
+
+    const resultPromise = Promise.all(
+      minIndices.map((index) =>
+        Array.from(filterGenerators.values())[index]!.next(),
+      ),
+    );
+    if (cursor <= supremum!) {
+      yield [cursor, supremum!];
+      cursor = supremum! + 1;
+    }
+    const nextResults = await resultPromise;
+    for (const [index, result] of nextResults.entries()) {
+      results[minIndices[index]!] = result;
+    }
+  }
 }
